dc408a6b0a0015f479ec5c2bf600c98cadfe386e
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2021-11-15 14:49:46 +0000

    Fix merge issues

--- application/lib/presentation/active_search/manager/active_search_manager.dart
+++ application/lib/presentation/active_search/manager/active_search_manager.dart
@@ -1,9 +1,11 @@
-// import 'package:xayn_architecture/concepts/on_failure.dart';
+import 'package:xayn_architecture/concepts/use_case/handlers/fold.dart';
 import 'package:xayn_architecture/concepts/use_case/use_case_bloc_helper.dart';
+import 'package:xayn_architecture/concepts/use_case/use_case_stream.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/discovery_engine/discovery_engine_results_use_case.dart';
 import 'package:xayn_discovery_app/presentation/active_search/manager/active_search_state.dart';
 import 'package:injectable/injectable.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:xayn_discovery_app/presentation/discovery_engine_mock/manager/discovery_engine_state.dart';
 
 @injectable
 class ActiveSearchManager extends Cubit<ActiveSearchState>
@@ -15,23 +17,31 @@ class ActiveSearchManager extends Cubit<ActiveSearchState>
   }
 
   final DiscoveryEngineResultsUseCase _discoveryEngineResultsUseCase;
+  late final UseCaseValueStream<DiscoveryEngineState> _resultsObserver;
 
   void search(String term) => _discoveryEngineResultsUseCase.search(term);
 
   void _init() {
-    consume(_discoveryEngineResultsUseCase, initialData: null);
-    // .fold(
-    //   onSuccess: (it) => state.copyWith(
-    //     results: it.results,
-    //     isComplete: it.isComplete,
-    //     isLoading: it.isLoading,
-    //     isInErrorState: false,
-    //   ),
-    //   onFailure: HandleFailure((e, s) {
-    //     return state.copyWith(
-    //       isInErrorState: true,
-    //     );
-    //   }),
-    // );
+    _resultsObserver =
+        consume(_discoveryEngineResultsUseCase, initialData: null);
   }
+
+  @override
+  Future<ActiveSearchState?> computeState() async =>
+      fold(_resultsObserver).foldAll((a, errorReport) {
+        if (errorReport.isNotEmpty) {
+          return state.copyWith(
+            isInErrorState: true,
+          );
+        }
+
+        if (a != null) {
+          return state.copyWith(
+            results: a.results,
+            isLoading: a.isLoading,
+            isComplete: a.isComplete,
+            isInErrorState: false,
+          );
+        }
+      });
 }
