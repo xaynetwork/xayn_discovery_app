080de3d78dff2ff546dd9aa72b4860cae63ee254
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-11-17 13:54:56 +0100

    use RaederMode Widget

--- application/lib/presentation/reader_mode/widget/reader_mode.dart
+++ application/lib/presentation/reader_mode/widget/reader_mode.dart
@@ -4,13 +4,19 @@ import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 import 'package:xayn_discovery_app/presentation/reader_mode/manager/reader_mode_manager.dart';
 import 'package:xayn_discovery_app/presentation/reader_mode/manager/reader_mode_state.dart';
-import 'package:xayn_readability/xayn_readability.dart';
+import 'package:xayn_readability/xayn_readability.dart' as readability;
 
 class ReaderMode extends StatefulWidget {
-  final ProcessHtmlResult processHtmlResult;
+  final String title;
+  final String snippet;
+  final Uri imageUri;
+  final readability.ProcessHtmlResult processHtmlResult;
 
   const ReaderMode({
     Key? key,
+    required this.title,
+    required this.snippet,
+    required this.imageUri,
     required this.processHtmlResult,
   }) : super(key: key);
 
@@ -18,26 +24,60 @@ class ReaderMode extends StatefulWidget {
   State<StatefulWidget> createState() => _ReaderModeState();
 }
 
-class _ReaderModeState extends State {
+class _ReaderModeState extends State<ReaderMode> {
   late final ReaderModeManager _readerModeManager;
+  late final readability.ReaderModeController _readerModeController;
 
   @override
   void initState() {
     super.initState();
 
     _readerModeManager = di.get();
+    _readerModeController = readability.ReaderModeController();
+
+    _readerModeManager.handleCardData(
+      title: widget.title,
+      snippet: widget.snippet,
+      imageUri: widget.imageUri,
+      processHtmlResult: widget.processHtmlResult,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _readerModeManager.close();
+    _readerModeController.dispose();
+  }
+
+  @override
+  void didUpdateWidget(ReaderMode oldWidget) {
+    if (oldWidget.title != widget.title ||
+        oldWidget.snippet != widget.snippet ||
+        oldWidget.imageUri != widget.imageUri ||
+        oldWidget.processHtmlResult != widget.processHtmlResult) {
+      _readerModeManager.handleCardData(
+        title: widget.title,
+        snippet: widget.snippet,
+        imageUri: widget.imageUri,
+        processHtmlResult: widget.processHtmlResult,
+      );
+    }
+
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ReaderModeManager, ReaderModeState>(
-        buildWhen: (a, b) => b.html != null,
+        buildWhen: (a, b) => b.uri != null,
         bloc: _readerModeManager,
         builder: (context, state) {
-          final html = state.html!;
+          _readerModeController.loadUri(state.uri!);
 
-          return HtmlWidget(
-            html,
+          return readability.ReaderMode(
+            controller: _readerModeController,
             textStyle: R.styles.appBodyText,
           );
         });
