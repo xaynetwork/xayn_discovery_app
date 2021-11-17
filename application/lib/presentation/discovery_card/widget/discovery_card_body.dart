e40b403f8bc625c6718ecc77fcd2a449fd12566c
Michael <michael.k.malak@gmail.com>
2021-11-17 16:07:17 +0200

    [TB-2989] Add remaining discovery screen card details (#13)
    Add remaining home screen card details
    TB-2989

--- application/lib/presentation/discovery_card/widget/discovery_card_body.dart
+++ application/lib/presentation/discovery_card/widget/discovery_card_body.dart
@@ -1,9 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 import 'package:xayn_readability/xayn_readability.dart';
-
-typedef OnLink = Future<bool> Function(String)?;
 
 class _ResultCardBody extends StatelessWidget {
   final PaletteGenerator? palette;
@@ -23,110 +22,92 @@ class _ResultCardBody extends StatelessWidget {
   Widget build(BuildContext context) {
     throw UnimplementedError();
   }
-
-  Widget _buildWithBackground(Color? color, Widget child) => Container(
-        color: color,
-        padding: EdgeInsets.all(R.dimen.unit2),
-        child: child,
-      );
 }
 
-/// The first or front story page of a discovery card.
-/// This typically contains the title and snippet.
-class DiscoveryCardPrimaryBody extends _ResultCardBody {
-  final String title;
+/// The story page of a discovery card.
+/// This typically contains a single html element that belongs to the reader mode.
+class DiscoveryCardBody extends _ResultCardBody {
   final String snippet;
+  final Widget footer;
 
-  const DiscoveryCardPrimaryBody({
+  const DiscoveryCardBody({
     Key? key,
-    required this.title,
     required this.snippet,
     PaletteGenerator? palette,
+    required this.footer,
   }) : super(key: key, palette: palette);
 
   @override
   Widget build(BuildContext context) {
-    final titleWidget = _buildWithBackground(
-        dominantColor?.withAlpha(0xc0),
-        Text(
-          title,
-          style: R.styles.appScreenHeadline?.copyWith(
-            color: textColor,
-          ),
-        ));
-
-    final spacing = SizedBox(
-      height: R.dimen.unit6,
-    );
-
-    final snippetWidget = _buildWithBackground(
-      dominantColor?.withAlpha(0xc0),
-      Text(
-        snippet,
-        style: R.styles.appBodyText?.copyWith(
-          color: textColor,
-        ),
-      ),
+    final snippetWidget = SnippetWidget(
+      snippet: snippet,
+      backgroundColor: R.colors.snippetBackground,
     );
 
     return Container(
-      color: R.colors.overlayBackground,
       alignment: AlignmentDirectional.bottomCenter,
       padding: EdgeInsets.only(
-        left: R.dimen.unit3,
-        right: R.dimen.unit3,
-        bottom: R.dimen.unit8,
-      ),
+          left: R.dimen.unit3,
+          right: R.dimen.unit3,
+          top: R.dimen.unit8,
+          bottom: R.dimen.unit6),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          titleWidget,
-          spacing,
-          snippetWidget,
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: snippetWidget,
+            ),
+          ),
+          SizedBox(height: R.dimen.unit3),
+          footer,
         ],
       ),
     );
   }
 }
 
-/// The secondary story page of a discovery card.
-/// This typically contains a single html element that belongs to the reader mode.
-class DiscoveryCardSecondaryBody extends _ResultCardBody {
-  final String html;
-  final OnLink? onLink;
-
-  const DiscoveryCardSecondaryBody({
+class SnippetWidget extends StatelessWidget {
+  const SnippetWidget({
     Key? key,
-    required this.html,
-    PaletteGenerator? palette,
-    this.onLink,
-  }) : super(key: key, palette: palette);
+    required this.snippet,
+    required this.backgroundColor,
+  }) : super(key: key);
+
+  final String snippet;
+  final Color backgroundColor;
 
   @override
   Widget build(BuildContext context) {
-    final htmlWidget = SingleChildScrollView(
-      child: _buildWithBackground(
-        dominantColor?.withAlpha(0xc0),
-        HtmlWidget(
-          html,
-          textStyle:
-              R.styles.appBodyText?.copyWith(color: textColor, fontSize: 18),
-          onTapUrl: onLink,
-        ),
-      ),
+    final highlightedTextStyle = R.styles.snippetTextStyle?.copyWith(
+      backgroundColor: backgroundColor,
     );
 
-    return Container(
-      color: R.colors.overlayBackground,
-      alignment: AlignmentDirectional.bottomCenter,
-      padding: EdgeInsets.only(
-        left: R.dimen.unit3,
-        right: R.dimen.unit3,
-        top: R.dimen.unit8,
-        bottom: R.dimen.unit8,
+    final roundedHighlightsTextStyle = highlightedTextStyle?.copyWith(
+        backgroundColor: null,
+        background: Paint()
+          ..color = backgroundColor
+          ..strokeWidth = 5
+          ..strokeJoin = StrokeJoin.round
+          ..style = PaintingStyle.stroke);
+
+    final centeredSnippet =
+        '''<div style="text-align: center">$snippet<div/>''';
+
+    // ref https://github.com/flutter/flutter/issues/29911
+    // Flutter doesn't allow painting stroke with the fill at the same time
+    // As a workaround, we stack both the fill and the rounded stroke highlight
+    return Stack(children: [
+      HtmlWidget(
+        centeredSnippet,
+        textStyle: highlightedTextStyle,
       ),
-      child: htmlWidget,
-    );
+      HtmlWidget(
+        centeredSnippet,
+        textStyle: roundedHighlightsTextStyle,
+      ),
+    ]);
   }
 }
---
+++ application/lib/presentation/discovery_card/widget/favicon_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:xayn_discovery_app/domain/model/discovery_engine/discovery_engine.dart';
+import 'package:xayn_discovery_app/presentation/constants/r.dart';
+import 'package:xayn_discovery_app/presentation/utils/time_ago.dart';
+
+class FaviconBar extends StatelessWidget {
+  const FaviconBar({
+    Key? key,
+    required this.provider,
+    required this.datePublished,
+  }) : super(key: key);
+
+  final WebResourceProvider provider;
+  final DateTime datePublished;
+
+  @override
+  Widget build(BuildContext context) {
+    final favicon = provider.thumbnail == null
+        ? Icon(Icons.web, color: R.colors.icon)
+        : Image.network(
+            provider.thumbnail!,
+            width: R.dimen.iconSize,
+            height: R.dimen.iconSize,
+          );
+
+    return Center(
+      child: Wrap(
+        spacing: R.dimen.unit0_75,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(R.dimen.unit0_5),
+            child: favicon,
+          ),
+          Text(
+            provider.name,
+            style: R.styles.appThumbnailText?.copyWith(color: Colors.white),
+          ),
+          Text(
+            '•',
+            style:
+                R.styles.appThumbnailTextLight?.copyWith(color: Colors.white),
+          ),
+          Text(
+            timeAgo(datePublished, DateFormat.yMMMMd()),
+            style:
+                R.styles.appThumbnailTextLight?.copyWith(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
