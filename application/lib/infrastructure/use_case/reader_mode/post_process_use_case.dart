cd95de04d3649699202fdd9c327a33f162652f5b
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-11-17 14:38:44 +0100

    load reader mode htmlk using Uri.bytes

--- application/lib/infrastructure/use_case/reader_mode/post_process_use_case.dart
+++ application/lib/infrastructure/use_case/reader_mode/post_process_use_case.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:injectable/injectable.dart';
@@ -5,19 +7,20 @@ import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_readability/xayn_readability.dart';
 
 @injectable
-class PostProcessUseCase extends UseCase<CardData, ProcessHtmlResult> {
+class PostProcessUseCase extends UseCase<CardData, Uri> {
   @override
-  Stream<ProcessHtmlResult> transaction(CardData param) async* {
+  Stream<Uri> transaction(CardData param) async* {
     final html = await compute(_processHtml, param);
+    const encoder = Utf8Encoder();
 
-    yield param.processHtmlResult.withOtherContent(html);
+    yield Uri.dataFromBytes(encoder.convert(html));
   }
 }
 
 String _processHtml(final CardData cardData) {
   final document = dom.Document.html(cardData.processHtmlResult.contents!);
   final images = document.querySelectorAll('img');
-  final nodes = document.querySelectorAll('');
+  final nodes = document.querySelectorAll('*');
 
   images.where((it) {
     final src = it.attributes['src'];
