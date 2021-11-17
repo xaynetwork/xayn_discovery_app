105218ce9163de4e7718c4367451e355c2cd139c
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-11-17 14:38:44 +0100

    load reader mode htmlk using Uri.bytes

--- application/lib/presentation/reader_mode/manager/reader_mode_manager.dart
+++ application/lib/presentation/reader_mode/manager/reader_mode_manager.dart
@@ -1,14 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/reader_mode/post_process_use_case.dart';
 import 'package:xayn_discovery_app/presentation/reader_mode/manager/reader_mode_state.dart';
 import 'package:xayn_readability/xayn_readability.dart';
 
+@injectable
 class ReaderModeManager extends Cubit<ReaderModeState>
     with UseCaseBlocHelper<ReaderModeState> {
   final PostProcessUseCase _postProcessUseCase;
 
-  late final UseCaseSink<CardData, ProcessHtmlResult> _postProcessHandler;
+  late final UseCaseSink<CardData, Uri> _postProcessHandler;
 
   ReaderModeManager(this._postProcessUseCase) : super(ReaderModeState.empty()) {
     _initHandlers();
@@ -28,16 +30,12 @@ class ReaderModeManager extends Cubit<ReaderModeState>
       ));
 
   @override
-  Future<ReaderModeState> computeState() async =>
-      fold(_postProcessHandler).foldAll((a, errorReport) {
+  Future<ReaderModeState?> computeState() async =>
+      fold(_postProcessHandler).foldAll((uri, errorReport) {
         if (errorReport.isNotEmpty) {}
 
-        if (a != null) {
-          final html = a.contents;
-
-          if (html != null) {
-            return ReaderModeState(uri: Uri.dataFromString(html));
-          }
+        if (uri != null) {
+          return ReaderModeState(uri: uri);
         }
       });
 
