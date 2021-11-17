ca72a762b861dee0e7dde772a64ce3244e9097a4
Peter Stojanowski <peter.stojanowski@gmail.com>
2021-11-17 11:10:20 +0000

    TB-2874 Active Search screen (#11)

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -53,8 +53,6 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: DiscoveryFeed(),
-    );
+    return const DiscoveryFeed();
   }
 }
