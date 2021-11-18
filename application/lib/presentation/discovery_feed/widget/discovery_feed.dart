9f160a3e3d32f88f6d08026f579b8e267680590e
Michael <michael.k.malak@gmail.com>
2021-11-18 13:57:54 +0200

    [TB-3004] Implement card story animation (#17)
    Add story animation
    Refactor discovery card
    TB-3004

--- application/lib/presentation/discovery_feed/widget/discovery_feed.dart
+++ application/lib/presentation/discovery_feed/widget/discovery_feed.dart
@@ -7,7 +7,6 @@ import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/presentation/active_search/widget/active_search.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/widget/discovery_card.dart';
-import 'package:xayn_discovery_app/presentation/discovery_card/widget/discovery_card_footer.dart';
 import 'package:xayn_discovery_app/presentation/discovery_feed/manager/discovery_feed_manager.dart';
 import 'package:xayn_discovery_app/presentation/discovery_feed/manager/discovery_feed_state.dart';
 import 'package:xayn_discovery_app/presentation/widget/feed_view.dart';
@@ -15,6 +14,8 @@ import 'package:xayn_discovery_app/presentation/widget/button/temp_search_button
 import 'package:xayn_swipe_it/xayn_swipe_it.dart';
 
 enum SwipeOption { like, dislike }
+
+const kSwipeOpenToPosition = 0.35;
 
 /// A widget which displays a list of discovery results.
 class DiscoveryFeed extends StatefulWidget {
@@ -71,68 +72,65 @@ class _DiscoveryFeedState extends State<DiscoveryFeed> {
         return _buildResultCard(document);
       };
 
-  Widget _buildFeedView() {
-    return BlocBuilder<DiscoveryFeedManager, DiscoveryFeedState>(
-      bloc: _discoveryFeedManager,
-      builder: (context, state) {
-        final results = state.results;
+  Widget _buildFeedView() =>
+      BlocBuilder<DiscoveryFeedManager, DiscoveryFeedState>(
+        bloc: _discoveryFeedManager,
+        builder: (context, state) {
+          final results = state.results;
 
-        if (results == null) {
-          return const Center(child: CircularProgressIndicator());
-        }
+          if (results == null) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-        return FeedView(
-          scrollController: _scrollController,
-          itemBuilder: _itemBuilder(results),
-          itemCount: results.length,
-        );
-      },
-    );
-  }
+          return FeedView(
+            scrollController: _scrollController,
+            itemBuilder: _itemBuilder(results),
+            itemCount: results.length,
+          );
+        },
+      );
 
   Widget _buildSwipeWidget({required Widget child}) => Swipe(
         optionsLeft: const [SwipeOption.like],
         optionsRight: const [SwipeOption.dislike],
         onFling: (options) => options.first,
-        opensToPosition: 0.5,
+        opensToPosition: kSwipeOpenToPosition,
         child: child,
-        optionBuilder: (context, option, index, selected) {
-          return SwipeOptionContainer(
-            option: option,
-            color: option == SwipeOption.dislike
-                ? R.colors.swipeBackgroundDelete
-                : R.colors.swipeBackgroundRelevant,
-            child: option == SwipeOption.dislike
-                ? SvgPicture.asset(
-                    R.assets.icons.thumbsDown,
-                    fit: BoxFit.none,
-                    color: R.colors.brightIcon,
-                  )
-                : SvgPicture.asset(
-                    R.assets.icons.thumbsUp,
-                    fit: BoxFit.none,
-                    color: R.colors.brightIcon,
-                  ),
-          );
-        },
+        optionBuilder: (context, option, index, selected) =>
+            SwipeOptionContainer(
+          option: option,
+          color: option == SwipeOption.dislike
+              ? R.colors.swipeBackgroundDelete
+              : R.colors.swipeBackgroundRelevant,
+          child: option == SwipeOption.dislike
+              ? SvgPicture.asset(
+                  R.assets.icons.thumbsDown,
+                  fit: BoxFit.none,
+                  color: R.colors.brightIcon,
+                )
+              : SvgPicture.asset(
+                  R.assets.icons.thumbsUp,
+                  fit: BoxFit.none,
+                  color: R.colors.brightIcon,
+                ),
+        ),
       );
 
-  Widget _buildResultCard(Document document) => Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: R.dimen.unit,
-          vertical: R.dimen.unit0_5,
-        ),
+  Widget _buildResultCard(Document document) {
+    final card = DiscoveryCard(
+      webResource: document.webResource,
+    );
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: R.dimen.unit,
+        vertical: R.dimen.unit0_5,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(R.dimen.unit1_5),
         child: _buildSwipeWidget(
-          child: DiscoveryCard(
-            snippet: document.webResource.snippet,
-            imageUrl: document.webResource.displayUrl.toString(),
-            url: document.webResource.url,
-            footer: DiscoveryCardFooter(
-              title: document.webResource.title,
-              provider: document.webResource.provider,
-              datePublished: document.webResource.datePublished,
-            ),
-          ),
+          child: card,
         ),
-      );
+      ),
+    );
+  }
 }
