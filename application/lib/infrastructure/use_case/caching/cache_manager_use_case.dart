9e792bf1490ae1bf805fd6a99ff06c4602366fe2
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-11-19 10:30:19 +0100

    create cache use case, manager and widget

---
+++ application/lib/infrastructure/use_case/caching/cache_manager_use_case.dart
@@ -0,0 +1,67 @@
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:injectable/injectable.dart';
+import 'package:xayn_architecture/xayn_architecture.dart';
+
+@injectable
+class CacheManagerUseCase extends UseCase<Uri, CacheManagerEvent> {
+  final ImageCacheManager _cacheManager;
+
+  @visibleForTesting
+  CacheManagerUseCase(this._cacheManager);
+
+  @factoryMethod
+  CacheManagerUseCase.withAppImageCacheManager()
+      : _cacheManager = AppImageCacheManager();
+
+  @override
+  Stream<CacheManagerEvent> transaction(Uri param) async* {
+    yield* _cacheManager.getImageFile(param.toString()).asyncMap((it) async {
+      if (it is FileInfo) {
+        return CacheManagerEvent.completed(param, await it.file.readAsBytes());
+      } else if (it is DownloadProgress) {
+        return CacheManagerEvent.progress(param, it.progress ?? .0);
+      }
+
+      throw CacheManagerError();
+    });
+  }
+}
+
+class CacheManagerEvent {
+  final Uri originalUri;
+  final double progress;
+  final Uint8List? bytes;
+
+  const CacheManagerEvent({
+    required this.originalUri,
+    required this.progress,
+    required this.bytes,
+  });
+
+  const CacheManagerEvent.progress(this.originalUri, this.progress)
+      : bytes = null;
+
+  const CacheManagerEvent.completed(this.originalUri, this.bytes)
+      : progress = 1.0;
+}
+
+class CacheManagerError extends Error {}
+
+class AppImageCacheManager extends CacheManager with ImageCacheManager {
+  static const key = 'libAppCachedImageData';
+
+  static final AppImageCacheManager _instance = AppImageCacheManager._();
+  factory AppImageCacheManager() {
+    return _instance;
+  }
+
+  AppImageCacheManager._()
+      : super(Config(
+          key,
+          stalePeriod: const Duration(days: 1),
+          maxNrOfCacheObjects: 200,
+        ));
+}
