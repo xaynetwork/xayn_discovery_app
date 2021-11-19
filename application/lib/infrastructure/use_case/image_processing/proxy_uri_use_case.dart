b0d9f938f346e39cb9d7efde1d711f8d63b9e0be
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-11-19 06:10:36 +0100

    adds more properties for fetcher api

--- application/lib/infrastructure/use_case/image_processing/proxy_uri_use_case.dart
+++ application/lib/infrastructure/use_case/image_processing/proxy_uri_use_case.dart
@@ -11,27 +11,38 @@ class ProxyUriUseCase extends UseCase<FetcherParams, Uri> {
   final String fetcherUrl;
 
   /// Creates a new proxy url use case.
-  ProxyUriUseCase({this.fetcherUrl = Env.imageFetcherUrl});
+  ProxyUriUseCase({required this.fetcherUrl});
+
+  @factoryMethod
+  ProxyUriUseCase.fetcherUrlFromEnv() : fetcherUrl = Env.imageFetcherUrl;
 
   @override
   Stream<Uri> transaction(FetcherParams param) async* {
     final fetcherUri = Uri.parse(fetcherUrl);
-    final queryParameters = {
-      'url': param.uri.toString(),
-      'fit': 'cover',
-    };
-    final hasWidth = param.uri.queryParameters.containsKey('w');
-    final hasHeight = param.uri.queryParameters.containsKey('h');
-    final hasEitherSize = hasWidth || hasHeight;
+    final queryParameters = {'url': param.uri.toString()};
 
-    if (!hasEitherSize) {
-      if (param.width != null) {
-        queryParameters.putIfAbsent('w', () => param.width.toString());
-      }
+    if (param.fit != null) {
+      queryParameters.putIfAbsent('fit', () => param.fit.toString());
+    }
 
-      if (param.height != null) {
-        queryParameters.putIfAbsent('h', () => param.height.toString());
-      }
+    if (param.width != null) {
+      queryParameters.putIfAbsent('w', () => param.width.toString());
+    }
+
+    if (param.height != null) {
+      queryParameters.putIfAbsent('h', () => param.height.toString());
+    }
+
+    if (param.blur != null) {
+      queryParameters.putIfAbsent('blur', () => param.blur.toString());
+    }
+
+    if (param.rotation != null) {
+      queryParameters.putIfAbsent('rot', () => param.rotation.toString());
+    }
+
+    if (param.tint != null) {
+      queryParameters.putIfAbsent('tint', () => param.tint.toString());
     }
 
     yield fetcherUri.replace(path: '/', queryParameters: queryParameters);
@@ -49,10 +60,26 @@ class FetcherParams {
   /// The requested image height
   final int? height;
 
+  /// fit type, eg: cover
+  final String? fit;
+
+  /// blur amount, eg: 5
+  final int? blur;
+
+  /// rotation in degrees
+  final int? rotation;
+
+  /// image tint overlay eg: red
+  final String? tint;
+
   /// Creates new parameters for fetching an image via a proxy.
   const FetcherParams({
     required this.uri,
     this.width,
     this.height,
+    this.fit,
+    this.blur,
+    this.rotation,
+    this.tint,
   });
 }
