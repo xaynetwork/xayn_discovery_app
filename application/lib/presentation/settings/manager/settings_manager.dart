c292c897fbddc1a386c7e63795785391a4ebe14a
Andrii Antonov <pro100svitlo@gmail.com>
2021-11-23 14:10:44 +0100

    Create settings_manager and state

---
+++ application/lib/presentation/settings/manager/settings_manager.dart
@@ -0,0 +1,90 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:xayn_architecture/xayn_architecture.dart';
+import 'package:xayn_discovery_app/domain/model/app_theme.dart';
+import 'package:xayn_discovery_app/domain/model/app_version.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/get_app_theme_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/listen_app_theme_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/save_app_theme_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/app_version/get_app_version_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/use_case_extension.dart';
+import 'package:xayn_discovery_app/presentation/settings/manager/settings_state.dart';
+
+@lazySingleton
+class SettingsScreenManager extends Cubit<SettingsScreenState>
+    with UseCaseBlocHelper<SettingsScreenState> {
+  final GetAppVersionUseCase _getAppVersionUseCase;
+  final GetAppThemeUseCase _getAppThemeUseCase;
+  final SaveAppThemeUseCase _saveAppThemeUseCase;
+  final ListenAppThemeUseCase _listenAppThemeUseCase;
+
+  SettingsScreenManager(
+    this._getAppVersionUseCase,
+    this._getAppThemeUseCase,
+    this._saveAppThemeUseCase,
+    this._listenAppThemeUseCase,
+  ) : super(const SettingsScreenState.initial()) {
+    _init();
+  }
+
+  bool _initDone = false;
+  late AppTheme _theme;
+  late final AppVersion _appVersion;
+  late final UseCaseValueStream<AppTheme> _appThemeHandler;
+
+  void _init() async {
+    // read values
+    _appVersion = (await _getAppVersionUseCase.call(null)).singleValue;
+    _theme = (await _getAppThemeUseCase.call(null)).singleValue;
+
+    // attach listeners
+    _appThemeHandler = consume(_listenAppThemeUseCase, initialData: null);
+
+    _initDone = true;
+
+    scheduleComputeState(() => null);
+  }
+
+  void changeTheme(AppTheme theme) => _saveAppThemeUseCase.call(theme);
+
+  void reportBug() {
+    // todo: handle report bug
+    //ignore: avoid_print
+    print('report bug clicked');
+  }
+
+  void shareApp() {
+    // todo: handle share app url/etc action
+    //ignore: avoid_print
+    print('shareApp clicked');
+  }
+
+  void openUrl(String url) {
+    final uri = Uri.tryParse(url);
+    assert(
+      uri != null && uri.hasAuthority,
+      'Please pass valid url. Current: $url',
+    );
+    // todo: handle open URL
+    //ignore: avoid_print
+    print('openUrl clicked. url: $url');
+  }
+
+  @override
+  Future<SettingsScreenState?> computeState() async {
+    if (!_initDone) return null;
+    SettingsScreenState buildReady() => SettingsScreenState.ready(
+          theme: _theme,
+          appVersion: _appVersion,
+        );
+    return fold(_appThemeHandler).foldAll((final appTheme, _) async {
+      if (appTheme == _theme) {
+        return null;
+      }
+      if (appTheme != null) {
+        _theme = appTheme;
+      }
+      return buildReady();
+    });
+  }
+}
---
+++ application/lib/presentation/settings/manager/settings_state.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:xayn_discovery_app/domain/model/app_theme.dart';
+import 'package:xayn_discovery_app/domain/model/app_version.dart';
+
+part 'settings_state.freezed.dart';
+
+@freezed
+abstract class SettingsScreenState with _$SettingsScreenState {
+  const factory SettingsScreenState.initial() = _Initial;
+
+  const factory SettingsScreenState.ready({
+    required AppTheme theme,
+    required AppVersion appVersion,
+  }) = SettingsScreenStateReady;
+}
