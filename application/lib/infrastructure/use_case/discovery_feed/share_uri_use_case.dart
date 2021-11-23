d358b1b24fe8bf9540c7434ce39109d792046ffe
Michael <michael.k.malak@gmail.com>
2021-11-23 19:20:52 +0200

    Make ShareUriUseCase yield Uri to be used in chain

--- application/lib/infrastructure/use_case/discovery_feed/share_uri_use_case.dart
+++ application/lib/infrastructure/use_case/discovery_feed/share_uri_use_case.dart
@@ -3,10 +3,11 @@ import 'package:share/share.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 
 @injectable
-class ShareUriUseCase extends UseCase<Uri, void> {
+class ShareUriUseCase extends UseCase<Uri, Uri> {
   @override
-  Stream<void> transaction(Uri param) async* {
+  Stream<Uri> transaction(Uri param) async* {
     final url = param.toString();
     Share.share(url);
+    yield param;
   }
 }
