6060bb6d6b0b3eb81a8072befeff6aedbff2c9d2
Andrii Antonov <pro100svitlo@gmail.com>
2021-11-24 16:59:04 +0100

    Add tests for the settings_screen sections

---
+++ application/test/presentation/settings/widget/general_info_section_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:xayn_design/xayn_design.dart';
+import 'package:xayn_discovery_app/presentation/constants/keys.dart';
+import 'package:xayn_discovery_app/presentation/constants/strings.dart';
+import 'package:xayn_discovery_app/presentation/settings/widget/general_info_section.dart';
+
+import '../../app_wrapper.dart';
+
+void main() {
+  testWidgets(
+    'GIVEN section THEN verify all widgets present',
+    (final WidgetTester tester) async {
+      await tester.pumpAppWrapped(SettingsGeneralInfoSection(
+        onAboutPressed: () {},
+        onImprintPressed: () {},
+        onCarbonNeutralPressed: () {},
+        onTermsPressed: () {},
+        onPrivacyPressed: () {},
+      ));
+
+      const kidsAmount = 5;
+      expect(
+        find.text(Strings.settingsSectionTitleGeneralInfo),
+        findsOneWidget,
+      );
+      expect(find.text(Strings.settingsAboutXayn), findsOneWidget);
+      expect(find.text(Strings.settingsCarbonNeutral), findsOneWidget);
+      expect(find.text(Strings.settingsImprint), findsOneWidget);
+      expect(find.text(Strings.settingsPrivacyPolicy), findsOneWidget);
+      expect(find.text(Strings.settingsTermsAndConditions), findsOneWidget);
+
+      expect(find.byType(SettingsSection), findsOneWidget);
+      expect(find.byType(SettingsCard), findsNWidgets(kidsAmount));
+      expect(find.byType(SettingsGroup), findsNWidgets(kidsAmount));
+      expect(find.byType(SettingsTile), findsNWidgets(kidsAmount));
+      expect(find.byType(Text), findsNWidgets(kidsAmount + 1));
+    },
+  );
+  testWidgets(
+    'GIVEN section WHEN clicked btn THEN proper callbacks are called',
+    (final WidgetTester tester) async {
+      final callbacks = {
+        Keys.settingsAboutXayn: false,
+        Keys.settingsImprint: false,
+        Keys.settingsCarbonNeutral: false,
+        Keys.settingsTermsAndConditions: false,
+        Keys.settingsPrivacyPolicy: false,
+      };
+      final widget = SettingsGeneralInfoSection(
+        onAboutPressed: () => callbacks[Keys.settingsAboutXayn] = true,
+        onImprintPressed: () => callbacks[Keys.settingsImprint] = true,
+        onCarbonNeutralPressed: () =>
+            callbacks[Keys.settingsCarbonNeutral] = true,
+        onTermsPressed: () => callbacks[Keys.settingsTermsAndConditions] = true,
+        onPrivacyPressed: () => callbacks[Keys.settingsPrivacyPolicy] = true,
+      );
+      await tester.pumpAppWrapped(widget);
+
+      for (final key in callbacks.keys) {
+        final btnFinder = find.byKey(key);
+        await tester.tap(btnFinder);
+        expect(callbacks[key], isTrue, reason: 'key: $key');
+      }
+    },
+  );
+}
---
+++ application/test/presentation/settings/widget/share_app_section_test.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:xayn_design/xayn_design.dart';
+import 'package:xayn_discovery_app/presentation/constants/keys.dart';
+import 'package:xayn_discovery_app/presentation/constants/strings.dart';
+import 'package:xayn_discovery_app/presentation/settings/widget/share_app_section.dart';
+
+import '../../app_wrapper.dart';
+
+void main() {
+  testWidgets(
+    'GIVEN section THEN verify all widgets present',
+    (final WidgetTester tester) async {
+      await tester.pumpAppWrapped(ShareAppSection(
+        onShareAppPressed: () {},
+      ));
+
+      expect(
+        find.text(Strings.settingsSectionTitleSpreadTheWord),
+        findsOneWidget,
+      );
+      expect(find.text(Strings.settingsShareBtn), findsOneWidget);
+
+      expect(find.byType(SettingsSection), findsOneWidget);
+      expect(find.byType(SettingsCard), findsNothing);
+      expect(find.byType(SettingsGroup), findsNothing);
+      expect(find.byType(SettingsTile), findsNothing);
+      expect(find.byType(AppRaisedButton), findsOneWidget);
+      expect(find.byType(SvgPicture), findsOneWidget);
+      expect(find.byType(Text), findsNWidgets(2));
+    },
+  );
+
+  testWidgets(
+    'GIVEN section WHEN clicked btn THEN proper callbacks are called',
+    (final WidgetTester tester) async {
+      var clicked = false;
+      await tester.pumpAppWrapped(ShareAppSection(
+        onShareAppPressed: () => clicked = true,
+      ));
+
+      final btnFinder = find.byKey(Keys.settingsShareBtn);
+      await tester.tap(btnFinder);
+      expect(clicked, isTrue);
+    },
+  );
+}
