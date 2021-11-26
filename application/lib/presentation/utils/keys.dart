1b8c2199e2cb3e6f03027f4d9623dfdb1e667aa5
Carmine Toriello <carmine.toriello@xayn.com>
2021-11-26 18:44:02 +0100

    Moved onboarding keys into constants folder

--- application/lib/presentation/utils/keys.dart
+++
@@ -1,6 +0,0 @@
-class Keys {
-  static const String onBoardingPageOne = 'onBoardingPageOne';
-  static const String onBoardingPageTwo = 'onBoardingPageTwo';
-  static const String onBoardingPageThree = 'onBoardingPageThree';
-  static const String onBoardingPageTapDetector = 'onBoardingPageTapDetector';
-}
