cc774a22929f9d1722f1a1a98fa9259eda132a12
Andrii Antonov <pro100svitlo@gmail.com>
2021-11-26 09:45:12 +0100

    Add listen_app_theme_use_case.dart with test [fix]

--- application/test/infrastructure/use_case/listen_app_theme_use_case_test.dart
+++ application/test/infrastructure/use_case/listen_app_theme_use_case_test.dart
@@ -6,6 +6,7 @@ import 'package:xayn_architecture/concepts/use_case/test/use_case_test.dart';
 import 'package:xayn_discovery_app/domain/model/app_theme.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/get_app_theme_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/listen_app_theme_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/use_case_extension.dart';
 
 import 'listen_app_theme_use_case_test.mocks.dart';
 
@@ -19,14 +20,14 @@ void main() {
     controller = StreamController();
     useCase = ListenAppThemeUseCase(storage, controller);
   });
-  useCaseTest<ListenAppThemeUseCase, void, AppTheme>(
+  useCaseTest<ListenAppThemeUseCase, None, AppTheme>(
     'WHEN storage emit new value THEN useCase emit it as well',
     build: () => useCase,
     setUp: () {
       controller.add(AppTheme.dark);
       controller.close();
     },
-    input: [null],
+    input: [none],
     expect: [useCaseSuccess(AppTheme.dark)],
   );
 }
