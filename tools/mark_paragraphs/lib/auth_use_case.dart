87b0584e28508800faa621ea16d96f63176bcdb4
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-11-29 14:42:07 +0100

    add isRelevant option

--- tools/mark_paragraphs/lib/auth_use_case.dart
+++ tools/mark_paragraphs/lib/auth_use_case.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 
-class AuthUseCase extends UseCase<String, Map<String, String>> {
+class AuthUseCase extends UseCase<ParagraphData, PersonalizedParagraphData> {
   static User? user;
 
   @override
-  Stream<Map<String, String>> transaction(String param) async* {
+  Stream<PersonalizedParagraphData> transaction(ParagraphData param) async* {
     if (user == null) {
       final credential = await FirebaseAuth.instance.signInAnonymously();
 
@@ -13,12 +13,36 @@ class AuthUseCase extends UseCase<String, Map<String, String>> {
     }
 
     if (user != null) {
-      yield {
-        'userId': user!.uid,
-        'text': param,
-      };
+      yield PersonalizedParagraphData(
+        param.paragraph,
+        userId: user!.uid,
+        isRelevant: param.isRelevant,
+      );
     } else {
       throw Exception('Could not sign in into FireBase');
     }
   }
+}
+
+class ParagraphData {
+  final String paragraph;
+  final bool isRelevant;
+
+  const ParagraphData(
+    this.paragraph, {
+    required this.isRelevant,
+  });
+}
+
+class PersonalizedParagraphData extends ParagraphData {
+  final String userId;
+
+  const PersonalizedParagraphData(
+    String paragraph, {
+    required this.userId,
+    required bool isRelevant,
+  }) : super(
+          paragraph,
+          isRelevant: isRelevant,
+        );
 }
