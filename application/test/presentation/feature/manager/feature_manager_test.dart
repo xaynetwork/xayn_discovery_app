8fdb37be90886697c03def22bc41a0c34b6c3055
Michael <michael.k.malak@gmail.com>
2021-11-29 17:59:45 +0200

    Fix test

--- application/test/presentation/feature/manager/feature_manager_test.dart
+++ application/test/presentation/feature/manager/feature_manager_test.dart
@@ -2,7 +2,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/feature.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/feature/override_feature_use_case.dart';
 import 'package:xayn_discovery_app/presentation/feature/manager/feature_manager.dart';
@@ -16,21 +15,13 @@ void main() {
   late FeatureManagerState initialState;
 
   setUp(() {
-    overrideFeatureUseCase = MockOverrideFeatureUseCase();
-
-    when(overrideFeatureUseCase.transform(any)).thenAnswer(
-      (_) => const Stream.empty(),
-    );
-
     initialState = FeatureManagerState.initial(kInitialFeatureMap);
+    overrideFeatureUseCase = MockOverrideFeatureUseCase();
+    when(overrideFeatureUseCase.transform(any)).thenAnswer(
+      (it) => it.positionalArguments[0],
+    );
     featureManager = FeatureManager(overrideFeatureUseCase);
   });
-
-  setUpUseCaseResponse({required Feature feature, required bool isEnabled}) {
-    when(overrideFeatureUseCase.transform(any)).thenAnswer(
-      (it) => Stream.value(it.positionalArguments[0]),
-    );
-  }
 
   blocTest<FeatureManager, FeatureManagerState>(
     'WHEN manager is created THEN state is initial',
@@ -44,13 +35,17 @@ void main() {
   );
 
   blocTest<FeatureManager, FeatureManagerState>(
-    'Setting featureScreen feature to true',
-    setUp: () =>
-        setUpUseCaseResponse(feature: Feature.featuresScreen, isEnabled: true),
+    'WHEN overrideFeature method called to set featureScreen feature to true THEN expect  featureScreen feature to be equal to true',
+    setUp: () {
+      when(overrideFeatureUseCase.transaction(any)).thenAnswer(
+        (_) => Stream.value({Feature.featuresScreen: true}),
+      );
+    },
     build: () => featureManager,
     act: (manager) => manager.overrideFeature(Feature.featuresScreen, true),
     verify: (manager) {
       verify(overrideFeatureUseCase.transform(any));
+      verify(overrideFeatureUseCase.transaction(any));
       verifyNoMoreInteractions(overrideFeatureUseCase);
       expect(manager.state.featureMap[Feature.featuresScreen], isTrue);
       expect(manager.isEnabled(Feature.featuresScreen), isTrue);
@@ -58,18 +53,20 @@ void main() {
   );
 
   blocTest<FeatureManager, FeatureManagerState>(
-    'Setting featureScreen feature to false',
-    setUp: () => setUpUseCaseResponse(
-      feature: Feature.featuresScreen,
-      isEnabled: false,
-    ),
+    'WHEN overrideFeature method called to set featureScreen feature to false THEN expect  featureScreen feature to be equal to false',
+    setUp: () {
+      when(overrideFeatureUseCase.transaction(any)).thenAnswer(
+        (_) => Stream.value({Feature.featuresScreen: false}),
+      );
+    },
     build: () => featureManager,
     act: (manager) => manager.overrideFeature(Feature.featuresScreen, false),
     verify: (manager) {
       verify(overrideFeatureUseCase.transform(any));
+      verify(overrideFeatureUseCase.transaction(any));
       verifyNoMoreInteractions(overrideFeatureUseCase);
-      // expect(manager.state.featureMap[Feature.featuresScreen], isFalse);
-      // expect(manager.isEnabled(Feature.featuresScreen), isFalse);
+      expect(manager.state.featureMap[Feature.featuresScreen], isFalse);
+      expect(manager.isEnabled(Feature.featuresScreen), isFalse);
     },
   );
 }
