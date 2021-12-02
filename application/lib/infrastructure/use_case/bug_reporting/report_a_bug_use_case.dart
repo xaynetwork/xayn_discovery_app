202f5477c9e6fb0aaba5f1e6661bae0dc6eed34b
Michael <michael.k.malak@gmail.com>
2021-12-02 16:46:10 +0200

    Use Env github token to init Android

--- application/lib/infrastructure/use_case/bug_reporting/report_a_bug_use_case.dart
+++ application/lib/infrastructure/use_case/bug_reporting/report_a_bug_use_case.dart
@@ -1,12 +1,46 @@
+import 'package:flutter/services.dart';
 import 'package:instabug_flutter/Instabug.dart';
+import 'package:xayn_design/xayn_design.dart';
 import 'package:xayn_discovery_app/infrastructure/env/env.dart';
+
+const kInstabugAndroidMethodChannel = 'instabug_android';
+const kInstabugAndroidStartMethod = 'startInstabug';
+const kInstabugTokenParam = 'token';
+const kInstabugInvocationEventParam = 'invocationEvents';
 
 class ReportABugUseCase {
   static init() {
-    /// This method is called for iOS only
-    /// For Android, Instabug is initialized from [CustomFlutterApplication.java]
-    Instabug.start(Env.instabugToken, [InvocationEvent.none]);
+    final token = Env.instabugToken;
+    final invocationEvents = [InvocationEvent.none];
+
+    if (SafePlatform.isAndroid) {
+      _initAndroidWithoutCrashReporting(token, invocationEvents);
+    } else {
+      _initiOS(token, invocationEvents);
+    }
+
+    Instabug.setWelcomeMessageMode(WelcomeMessageMode.disabled);
   }
+
+  static _initAndroidWithoutCrashReporting(
+    String token,
+    List<InvocationEvent> invocationEvents,
+  ) async {
+    const MethodChannel _channel = MethodChannel(kInstabugAndroidMethodChannel);
+    final List<String> invocationEventsStrings =
+        invocationEvents.map((e) => e.toString()).toList(growable: false);
+    final params = {
+      kInstabugTokenParam: token,
+      kInstabugInvocationEventParam: invocationEventsStrings
+    };
+    await _channel.invokeMethod<Object>(kInstabugAndroidStartMethod, params);
+  }
+
+  static _initiOS(
+    String token,
+    List<InvocationEvent> invocationEvents,
+  ) =>
+      Instabug.start(token, invocationEvents);
 
   static showDialog() => Instabug.show();
 }
