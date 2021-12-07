f23ecc7eda9853804fc8c01ac98f9ec664cd81ae
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-12-07 15:09:18 +0100

    split Env into dev and prod versions, use kReleaseMode outside of build_runner this way

--- application/lib/infrastructure/env/env.dart
+++ application/lib/infrastructure/env/env.dart
@@ -3,11 +3,31 @@ import 'package:flutter/foundation.dart';
 
 part 'env.g.dart';
 
-/// Standard Env config.
-@Envify(path: kReleaseMode ? '.env.prod' : '.env.dev')
 abstract class Env {
-  static const String searchApiBaseUrl = _Env.searchApiBaseUrl;
-  static const String searchApiSecretKey = _Env.searchApiSecretKey;
-  static const String imageFetcherUrl = _Env.imageFetcherUrl;
-  static const String instabugToken = _Env.instabugToken;
+  static const String searchApiBaseUrl =
+      kReleaseMode ? _EnvProd.searchApiBaseUrl : _EnvDev.searchApiBaseUrl;
+  static const String searchApiSecretKey =
+      kReleaseMode ? _EnvProd.searchApiSecretKey : _EnvDev.searchApiSecretKey;
+  static const String imageFetcherUrl =
+      kReleaseMode ? _EnvProd.imageFetcherUrl : _EnvDev.imageFetcherUrl;
+  static const String instabugToken =
+      kReleaseMode ? _EnvProd.instabugToken : _EnvDev.instabugToken;
+}
+
+/// Standard Env config.
+@Envify(path: '.env.dev')
+abstract class _EnvDev {
+  static const String searchApiBaseUrl = __EnvDev.searchApiBaseUrl;
+  static const String searchApiSecretKey = __EnvDev.searchApiSecretKey;
+  static const String imageFetcherUrl = __EnvDev.imageFetcherUrl;
+  static const String instabugToken = __EnvDev.instabugToken;
+}
+
+/// Standard Env config.
+@Envify(path: '.env.prod')
+abstract class _EnvProd {
+  static const String searchApiBaseUrl = __EnvProd.searchApiBaseUrl;
+  static const String searchApiSecretKey = __EnvProd.searchApiSecretKey;
+  static const String imageFetcherUrl = __EnvProd.imageFetcherUrl;
+  static const String instabugToken = __EnvProd.instabugToken;
 }
