c807fe4176c583be75f204a12bad81af800596ed
Carmine Toriello <carmine.toriello@xayn.com>
2021-12-09 14:22:45 +0100

    Made initLoggerUseCase a singleton
    Make sure that initLogger method is called only once by checking if transaction method has already been called

--- application/lib/infrastructure/use_case/develop/init_logger_use_case.dart
+++ application/lib/infrastructure/use_case/develop/init_logger_use_case.dart
@@ -5,19 +5,24 @@ import 'package:xayn_discovery_app/presentation/utils/logger/logger.dart';
 
 import 'handlers.dart';
 
-@injectable
+@singleton
 class InitLoggerUseCase extends UseCase<None, None> {
   final FileHandler _fileHandler;
   final LoggerHandler _loggerHandler;
+  bool isTransactionDone;
 
   InitLoggerUseCase(
     this._fileHandler,
     this._loggerHandler,
-  );
+  ) : isTransactionDone = false;
 
   @override
   Stream<None> transaction(param) async* {
     try {
+      if (isTransactionDone) {
+        throw InitLoggerUseCaseException(
+            'Transaction method has already been called once');
+      }
       final directory = await _fileHandler.getAppDirectory();
       final path = directory.path;
 
@@ -26,6 +31,19 @@ class InitLoggerUseCase extends UseCase<None, None> {
       );
     } catch (e) {
       logger.i('Init Logger Use Case: Exception occurred ${e.toString()}');
+    } finally {
+      if (!isTransactionDone) {
+        isTransactionDone = true;
+      }
     }
   }
+}
+
+class InitLoggerUseCaseException extends Error {
+  final String msg;
+
+  InitLoggerUseCaseException(this.msg);
+
+  @override
+  String toString() => msg;
 }
