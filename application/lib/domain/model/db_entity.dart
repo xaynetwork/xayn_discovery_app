08203bfa2f0f0042754dd7524277d01f73caba45
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2021-12-09 08:18:54 +0000

    Revert nonVirtual

--- application/lib/domain/model/db_entity.dart
+++ application/lib/domain/model/db_entity.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
 
 abstract class DbEntity {
-  @nonVirtual
-  final UniqueId id;
-
-  const DbEntity(this.id);
+  UniqueId get id;
 }
