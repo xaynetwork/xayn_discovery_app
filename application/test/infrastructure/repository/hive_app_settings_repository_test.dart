a3bd27deb85e33ea0a1fc83f8df80c763c292320
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2021-12-13 10:15:34 +0000

    Use mock mapper

--- application/test/infrastructure/repository/hive_app_settings_repository_test.dart
+++ application/test/infrastructure/repository/hive_app_settings_repository_test.dart
@@ -2,36 +2,52 @@ import 'dart:typed_data';
 
 import 'package:hive/hive.dart';
 import 'package:hive_crdt/hive_crdt.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:xayn_discovery_app/domain/model/app_settings.dart';
 import 'package:xayn_discovery_app/domain/model/app_theme.dart';
 import 'package:xayn_discovery_app/domain/model/discovery_feed_axis.dart';
 import 'package:xayn_discovery_app/infrastructure/mappers/app_settings_mapper.dart';
-import 'package:xayn_discovery_app/infrastructure/mappers/app_theme_mapper.dart';
-import 'package:xayn_discovery_app/infrastructure/mappers/discovery_feed_axis_mapper.dart';
 import 'package:xayn_discovery_app/infrastructure/repository/hive_app_settings_repository.dart';
 import 'package:xayn_discovery_app/infrastructure/util/box_names.dart';
 
 import '../util/test_utils.dart';
 
+import 'hive_app_settings_repository_test.mocks.dart';
+
+@GenerateMocks([
+  AppSettingsMapper,
+])
 void main() async {
   final box =
       await Hive.openBox<Record>(BoxNames.appSettings, bytes: Uint8List(0));
-  const mapper = AppSettingsMapper(
-    IntToAppThemeMapper(),
-    AppThemeToIntMapper(),
-    IntToDiscoveryFeedAxisMapper(),
-    DiscoveryFeedAxisToIntMapper(),
-  );
-  final repository = HiveAppSettingsRepository(mapper);
+
+  late MockAppSettingsMapper mapper;
+  late HiveAppSettingsRepository repository;
 
   setUp(() {
     initHiveAdapters();
+
+    mapper = MockAppSettingsMapper();
+    repository = HiveAppSettingsRepository(mapper);
   });
 
   group('HiveAppSettingsRepository', () {
     group('getSettings:', () {
       setUp(() async {
+        when(mapper.toMap(any)).thenAnswer((_) => {
+              0: true,
+              1: 1,
+              2: 1,
+            });
+        when(mapper.fromMap(null)).thenAnswer((_) => null);
+        when(mapper.fromMap({
+          0: true,
+          1: 1,
+          2: 1,
+        })).thenAnswer((_) => AppSettings.initial());
+
         final appSettings = AppSettings.initial();
         repository.settings = appSettings;
       });
@@ -77,6 +93,18 @@ void main() async {
         discoveryFeedAxis: DiscoveryFeedAxis.horizontal,
       );
 
+      setUp(() async {
+        final value = AppSettings.initial().copyWith(
+          appTheme: AppTheme.dark,
+          discoveryFeedAxis: DiscoveryFeedAxis.horizontal,
+        );
+        when(mapper.toMap(value)).thenAnswer((_) => {
+              0: false,
+              1: 2,
+              2: 1,
+            });
+      });
+
       tearDown(() async {
         await box.clear();
       });
@@ -92,23 +120,6 @@ void main() async {
               1: 2,
               2: 1,
             }));
-      });
-
-      test('given AppSettings should persist it in Hive box', () async {
-        repository.settings = appSettings.copyWith(
-          isOnboardingDone: true,
-          appTheme: AppTheme.light,
-        );
-
-        expect(box.isNotEmpty, isTrue);
-        expect(
-          box.values.first.value,
-          equals({
-            0: true,
-            1: 1,
-            2: 1,
-          }),
-        );
       });
     });
   });
