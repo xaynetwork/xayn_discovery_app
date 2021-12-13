4b77306ac3f945d7f158ec02ae4c0cb21e2418b2
Carmine Toriello <carmine.toriello@xayn.com>
2021-12-13 16:11:06 +0100

    Implemented bookmark and collection mappers
    Implemented also the tests for the mappers
    Adjusted the AppSettingsMapper for handling mapping errors

--- application/lib/infrastructure/mappers/base_mapper.dart
+++ application/lib/infrastructure/mappers/base_mapper.dart
@@ -6,4 +6,17 @@ abstract class BaseDbEntityMapper<T extends DbEntity> {
   const BaseDbEntityMapper();
   T? fromMap(Map? map);
   DbEntityMap toMap(T entity);
+
+  /// Should be used to throw a [DbEntityMapperException] in case of error
+  /// while mapping an object
+  void throwMapperException();
+}
+
+class DbEntityMapperException implements Exception {
+  final String msg;
+
+  DbEntityMapperException(this.msg);
+
+  @override
+  String toString() => msg;
 }
