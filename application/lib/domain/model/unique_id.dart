59c2e19d3b9926994e7984a33167b8a9c5fedb5b
Carmine Toriello <carmine.toriello@xayn.com>
2021-12-13 16:10:28 +0100

    Added equatable package

--- application/lib/domain/model/unique_id.dart
+++ application/lib/domain/model/unique_id.dart
@@ -1,8 +1,10 @@
+import 'package:equatable/equatable.dart';
 import 'package:uuid/uuid.dart';
 
 /// A class for uniquely identifying another object.
-class UniqueId {
+class UniqueId extends Equatable {
   /// The value used to identify an object.
+
   final String value;
 
   /// Default constructor which initializes the [value] with a random UUID.
@@ -10,4 +12,7 @@ class UniqueId {
 
   /// Creates a [UniqueId] from the passed [uniqueId] used as value.
   const UniqueId.fromTrustedString(String uniqueId) : value = uniqueId;
+
+  @override
+  List<Object?> get props => [value];
 }
