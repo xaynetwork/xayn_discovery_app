e2f4ffb5822bc56d366bbe287cd1dc3e6a30d7b9
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2021-12-15 10:10:33 +0000

    Add missing repository comments

--- application/lib/domain/model/db_entity.dart
+++ application/lib/domain/model/db_entity.dart
@@ -1,7 +1,9 @@
 import 'package:meta/meta.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
 
+/// Abstract interface to be implemented by Hive entities.
 abstract class DbEntity {
+  /// The id of Hive entity. Should not be overriden.
   @nonVirtual
   final UniqueId id;
 
--- application/lib/domain/model/unique_id.dart
+++ application/lib/domain/model/unique_id.dart
@@ -1,9 +1,13 @@
 import 'package:uuid/uuid.dart';
 
+/// A class for uniquely identifying another object.
 class UniqueId {
+  /// The value used to identify an object.
   final String value;
 
+  /// Default constructor which initializes the [value] with a random UUID.
   UniqueId() : value = const Uuid().v4();
 
+  /// Creates a [UniqueId] from the passed [uniqueId] used as value.
   const UniqueId.fromTrustedString(String uniqueId) : value = uniqueId;
 }
