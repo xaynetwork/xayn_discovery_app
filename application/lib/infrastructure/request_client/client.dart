fc7a962ffb14772306839084b705ab581d31ae7f
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-12-20 17:32:44 +0100

    correctly handle image redirects

--- application/lib/infrastructure/request_client/client.dart
+++ application/lib/infrastructure/request_client/client.dart
@@ -1,5 +1,6 @@
 import 'package:http_client/http_client.dart' as http;
 import 'package:injectable/injectable.dart';
+import 'package:xayn_discovery_app/presentation/utils/logger.dart';
 import 'package:xayn_readability/xayn_readability.dart';
 
 const String _kUserAgent =
@@ -20,33 +21,52 @@ class Client implements http.Client {
   Future<http.Response> sendWithRedirectGuard(http.Request request) async {
     final response = await send(request);
 
-    if (const [301, 302, 308].contains(response.statusCode)) {
-      final locations = response.headers['location'] ?? const <String>[];
-      final location =
-          locations.isNotEmpty ? Uri.parse(locations.last) : request.uri;
+    // test if in redirect status code range
+    if (response.statusCode >= 300 && response.statusCode < 400) {
+      final location = _getNextLocation(response, request.uri);
+
+      logger.i(
+          'Redirect detected (${response.statusCode})\nNext location: $location');
 
       // detect if we got 'set-cookie' headers
       // some sites simply return a 302 and expect then to be called
       // again, but with those cookies then set.
       // this is an anti-scraping measure.
       if (response.headers.containsKey('set-cookie')) {
-        final serverCookies = response.headers['set-cookie']!;
+        logger.i('Server cookies detected');
 
-        final cookies = Map.fromEntries(
-          serverCookies
-              .map((it) => it.split(';'))
-              .expand((it) => it)
-              .map((it) => it.split('='))
-              .where((it) => it.length == 2)
-              .map((it) => MapEntry(it.first, it.last)),
-        );
-
-        return await send(request.change(uri: location, cookies: cookies));
+        return await send(request.change(
+          uri: location,
+          cookies: _getCookiesToSet(response),
+        ));
       }
 
       return await send(request.change(uri: location));
     }
 
     return response;
+  }
+
+  Uri _getNextLocation(http.Response response, Uri originalUri) {
+    final redirects = response.redirects
+            ?.map((it) => it.location)
+            .map((it) => it.toString())
+            .toList() ??
+        const <String>[];
+    final locations = response.headers['location'] ?? redirects;
+    return locations.isNotEmpty ? Uri.parse(locations.last) : originalUri;
+  }
+
+  Map<String, String> _getCookiesToSet(http.Response response) {
+    final cookies = response.headers['set-cookie']!;
+
+    return Map.fromEntries(
+      cookies
+          .map((it) => it.split(';'))
+          .expand((it) => it)
+          .map((it) => it.split('='))
+          .where((it) => it.length == 2)
+          .map((it) => MapEntry(it.first, it.last)),
+    );
   }
 }
