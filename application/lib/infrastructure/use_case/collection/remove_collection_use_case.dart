4f98d884aa18a48d038fc9b81bc0d3709071b9ea
Carmine Toriello <carmine.toriello@xayn.com>
2021-12-21 09:29:17 +0100

    Added implementation for moving bookmarks when deleting a collection

--- application/lib/infrastructure/use_case/collection/remove_collection_use_case.dart
+++ application/lib/infrastructure/use_case/collection/remove_collection_use_case.dart
@@ -1,11 +1,12 @@
 import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
-import 'package:xayn_discovery_app/domain/model/collection/collection.dart';
+import 'package:xayn_discovery_app/domain/model/unique_id.dart';
 import 'package:xayn_discovery_app/domain/repository/bookmarks_repository.dart';
 import 'package:xayn_discovery_app/domain/repository/collections_repository.dart';
 
 @injectable
-class RemoveCollectionUseCase extends UseCase<Collection, None> {
+class RemoveCollectionUseCase
+    extends UseCase<RemoveCollectionUseCaseParam, None> {
   final CollectionsRepository _collectionsRepository;
   final BookmarksRepository _bookmarksRepository;
 
@@ -15,9 +16,40 @@ class RemoveCollectionUseCase extends UseCase<Collection, None> {
   );
 
   @override
-  Stream<None> transaction(Collection param) async* {
-    _bookmarksRepository.removeAllByCollectionId(param.id);
-    _collectionsRepository.remove(param);
+  Stream<None> transaction(RemoveCollectionUseCaseParam param) async* {
+    final collection =
+        _collectionsRepository.getById(param.collectionIdToRemove);
+    if (collection == null) {
+      yield none;
+      return;
+    }
+
+    /// Check if the bookmarks must be deleted or moved to a different collection
+    if (param.collectionIdMoveBookmarksTo == null) {
+      _bookmarksRepository.removeAllByCollectionId(param.collectionIdToRemove);
+    } else {
+      /// Move bookmarks to another collection
+      final bookmarks =
+          _bookmarksRepository.getByCollectionId(param.collectionIdToRemove);
+
+      for (var bookmark in bookmarks) {
+        _bookmarksRepository.bookmark = bookmark.copyWith(
+          collectionId: param.collectionIdMoveBookmarksTo!,
+        );
+      }
+    }
+
+    _collectionsRepository.remove(collection);
     yield none;
   }
+}
+
+class RemoveCollectionUseCaseParam {
+  final UniqueId collectionIdToRemove;
+  final UniqueId? collectionIdMoveBookmarksTo;
+
+  RemoveCollectionUseCaseParam({
+    required this.collectionIdToRemove,
+    this.collectionIdMoveBookmarksTo,
+  });
 }
