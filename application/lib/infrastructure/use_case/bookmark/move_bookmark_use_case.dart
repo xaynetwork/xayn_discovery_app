9932ca5cd665609080efe7dd98ad1091f8f539d1
Carmine Toriello <carmine.toriello@xayn.com>
2021-12-21 11:04:18 +0100

    Addressed PR comments regarding bookmark usecases

--- application/lib/infrastructure/use_case/bookmark/move_bookmark_use_case.dart
+++ application/lib/infrastructure/use_case/bookmark/move_bookmark_use_case.dart
@@ -4,6 +4,8 @@ import 'package:xayn_architecture/concepts/use_case/use_case_base.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
 import 'package:xayn_discovery_app/domain/repository/bookmarks_repository.dart';
 import 'package:xayn_discovery_app/domain/repository/collections_repository.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/bookmark/bookmark_exception.dart';
+import 'package:xayn_discovery_app/presentation/utils/logger.dart';
 
 @injectable
 class MoveBookmarkUseCase extends UseCase<MoveBookmarkUseCaseParam, None> {
@@ -19,15 +21,17 @@ class MoveBookmarkUseCase extends UseCase<MoveBookmarkUseCaseParam, None> {
   Stream<None> transaction(MoveBookmarkUseCaseParam param) async* {
     final bookmark = _bookmarksRepository.getById(param.bookmarkId);
     if (bookmark == null) {
-      yield none;
-      return;
+      logger.e(errorMessageMovingNotExistingBookmark);
+      throw BookmarkUseCaseException(errorMessageMovingNotExistingBookmark);
     }
 
     final collection = _collectionsRepository.getById(param.collectionId);
 
     if (collection == null) {
-      yield none;
-      return;
+      logger.e(errorMessageMovingBookmarkToNotExistingCollection);
+      throw BookmarkUseCaseException(
+        errorMessageMovingBookmarkToNotExistingCollection,
+      );
     }
 
     final updatedBookmark = bookmark.copyWith(collectionId: param.collectionId);
