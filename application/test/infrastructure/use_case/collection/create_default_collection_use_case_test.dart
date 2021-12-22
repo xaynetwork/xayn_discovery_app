d4860eee697141aa39ced59d4e19db14c345afcf
Carmine Toriello <carmine.toriello@xayn.com>
2021-12-22 11:40:27 +0100

    Implemented tests for collection usecases

---
+++ application/test/infrastructure/use_case/collection/create_default_collection_use_case_test.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:xayn_architecture/concepts/use_case/test/use_case_test.dart';
+import 'package:xayn_discovery_app/domain/model/collection/collection.dart';
+import 'package:xayn_discovery_app/domain/repository/collections_repository.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/collection/collection_exception.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/collection/create_default_collection_use_case.dart';
+
+import 'create_default_collection_use_case_test.mocks.dart';
+
+@GenerateMocks([CollectionsRepository])
+void main() {
+  late MockCollectionsRepository collectionsRepository;
+  late CreateDefaultCollectionUseCase createDefaultCollectionUseCase;
+  const String defaultCollectionName = 'Read Later';
+  final collection = Collection(
+      id: Collection.readLaterId, name: defaultCollectionName, index: 0);
+
+  setUp(() {
+    collectionsRepository = MockCollectionsRepository();
+    createDefaultCollectionUseCase =
+        CreateDefaultCollectionUseCase(collectionsRepository);
+  });
+
+  group('Rename collection use case', () {
+    useCaseTest(
+      'WHEN the default collection already exists THEN throw an exception',
+      setUp: () =>
+          when(collectionsRepository.getAll()).thenReturn([collection]),
+      build: () => createDefaultCollectionUseCase,
+      input: [defaultCollectionName],
+      verify: (_) {
+        verifyInOrder([
+          collectionsRepository.getAll(),
+        ]);
+        verifyNoMoreInteractions(collectionsRepository);
+      },
+      expect: [
+        useCaseFailure(
+          throwsA(const TypeMatcher<CollectionUseCaseException>()),
+        )
+      ],
+    );
+
+    useCaseTest(
+      'WHEN the default collection doesn\'t exist THEN create the collection, save it and return it',
+      setUp: () => when(collectionsRepository.getAll()).thenReturn([]),
+      build: () => createDefaultCollectionUseCase,
+      input: [defaultCollectionName],
+      verify: (_) {
+        verifyInOrder([
+          collectionsRepository.getAll(),
+          collectionsRepository.collection = collection,
+        ]);
+        verifyNoMoreInteractions(collectionsRepository);
+      },
+      expect: [useCaseSuccess(collection)],
+    );
+  });
+}
---
+++ application/test/infrastructure/use_case/collection/listen_collections_use_case_test.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:xayn_architecture/concepts/use_case/none.dart';
+import 'package:xayn_architecture/concepts/use_case/test/use_case_test.dart';
+import 'package:xayn_discovery_app/domain/model/collection/collection.dart';
+import 'package:xayn_discovery_app/domain/model/repository_event.dart';
+import 'package:xayn_discovery_app/domain/model/unique_id.dart';
+import 'package:xayn_discovery_app/domain/repository/collections_repository.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/collection/listen_collections_use_case.dart';
+
+import 'listen_collections_use_case_test.mocks.dart';
+
+@GenerateMocks([CollectionsRepository])
+void main() {
+  late MockCollectionsRepository collectionsRepository;
+  late ListenCollectionsUseCase listenCollectionsUseCase;
+  final collection1 =
+      Collection(id: UniqueId(), name: 'Collection1 name', index: 1);
+  final collection2 =
+      Collection(id: UniqueId(), name: 'Collection2 name', index: 2);
+
+  setUp(() {
+    collectionsRepository = MockCollectionsRepository();
+    listenCollectionsUseCase = ListenCollectionsUseCase(collectionsRepository);
+  });
+
+  group('Listen collections use case', () {
+    useCaseTest(
+      'WHEN repository emits an event THEN usecase emits the list of collections',
+      setUp: () {
+        when(collectionsRepository.watch()).thenAnswer(
+          (_) => Stream.value(
+            ChangedEvent(
+              id: UniqueId(),
+              newObject: collection1,
+            ),
+          ),
+        );
+        when(collectionsRepository.getAll())
+            .thenReturn([collection1, collection2]);
+      },
+      build: () => listenCollectionsUseCase,
+      input: [none],
+      verify: (_) {
+        verifyInOrder([
+          collectionsRepository.watch(),
+          collectionsRepository.getAll(),
+        ]);
+        verifyNoMoreInteractions(collectionsRepository);
+      },
+      expect: [
+        useCaseSuccess(
+          ListenCollectionsUseCaseOut([collection1, collection2]),
+        )
+      ],
+    );
+  });
+}
