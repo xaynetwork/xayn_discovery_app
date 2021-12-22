d184edf5911d6ae0b7f01bbad84a7ef26303cb43
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2021-12-22 19:10:41 +0100

    attempt to strip title in a naïve way and fix image during transition

--- application/lib/infrastructure/use_case/reader_mode/post_process_use_case.dart
+++ application/lib/infrastructure/use_case/reader_mode/post_process_use_case.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:injectable/injectable.dart';
@@ -7,23 +8,42 @@ import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_readability/xayn_readability.dart';
 
 @injectable
-class PostProcessUseCase extends UseCase<ProcessHtmlResult, Uri> {
+class PostProcessUseCase extends UseCase<PostProcessInput, Uri> {
   @override
-  Stream<Uri> transaction(ProcessHtmlResult param) async* {
-    final contents = param.contents;
+  Stream<Uri> transaction(PostProcessInput param) async* {
+    final contents = param.result.contents;
 
     if (contents != null) {
       yield Uri.dataFromBytes(
         const Utf8Encoder().convert(
-          await compute(_processHtml, contents),
+          await compute(_processHtml(param.title), contents),
         ),
       );
     }
   }
 }
 
-String _processHtml(final String html) {
-  final document = dom.Document.html(html);
+String Function(String) _processHtml(final String title) =>
+    (final String html) {
+      final titleLc = title.toLowerCase();
+      final document = dom.Document.html(html);
+      final everything = document.querySelectorAll('*');
+      final match =
+          everything.firstWhereOrNull((it) => it.text.toLowerCase() == titleLc);
 
-  return document.outerHtml;
+      if (match != null) {
+        match.remove();
+      }
+
+      return document.outerHtml;
+    };
+
+class PostProcessInput {
+  final ProcessHtmlResult result;
+  final String title;
+
+  const PostProcessInput({
+    required this.result,
+    required this.title,
+  });
 }
