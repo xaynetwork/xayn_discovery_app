79abc9e6426db2cf428a295342e1ee7d8c7a4adb
Carmine Toriello <carmine.toriello@xayn.com>
2022-01-03 12:37:14 +0100

    Improved use case tests structure:
     - Created a use_case_mocks.dart file containing all the objects
    to be mocked needed by the tests
     - Changed the folder structure of the tests (usecases) in order to
    be the same of the one we have in the lib folder

--- application/test/infrastructure/use_case/collection/create_collection_use_case_test.dart
+++ application/test/infrastructure/use_case/collection/create_collection_use_case_test.dart
@@ -1,16 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:xayn_architecture/concepts/use_case/test/use_case_test.dart';
 import 'package:xayn_discovery_app/domain/model/collection/collection.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
-import 'package:xayn_discovery_app/domain/repository/collections_repository.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/collection/create_collection_use_case.dart';
-import 'package:xayn_discovery_app/infrastructure/use_case/develop/handlers.dart';
 
-import 'create_collection_use_case_test.mocks.dart';
+import '../use_case_mocks/use_case_mocks.mocks.dart';
 
-@GenerateMocks([CollectionsRepository, UniqueIdHandler])
 void main() {
   late MockCollectionsRepository collectionsRepository;
   late MockUniqueIdHandler uniqueIdHandler;
