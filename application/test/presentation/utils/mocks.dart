f9067139cbfb9effe5ee74f564bc96549490ccf9
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-01-06 11:31:09 +0000

    Fix merge conflicts

--- application/test/presentation/utils/mocks.dart
+++ application/test/presentation/utils/mocks.dart
@@ -1,6 +1,7 @@
 import 'package:mockito/annotations.dart';
 import 'package:xayn_discovery_app/domain/model/discovery_engine/discovery_engine.dart';
 import 'package:xayn_discovery_app/domain/model/discovery_engine/document.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/app_session/save_app_session_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/get_app_theme_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/listen_app_theme_use_case.dart';
 import 'package:xayn_discovery_app/presentation/app/manager/app_manager.dart';
@@ -15,6 +16,7 @@ import 'package:xayn_discovery_app/presentation/settings/manager/settings_manage
   ListenAppThemeUseCase,
   SettingsNavActions,
   SettingsScreenManager,
+  IncrementAppSessionUseCase,
 ])
 class Mocks {
   Mocks._();
