08969591c7b6ce715c1ffd9e6e1ca5b769048849
Andrii Antonov <pro100svitlo@gmail.com>
2022-01-07 16:32:25 +0100

    Update app_toolbar.dart according to the design

--- application/lib/presentation/widget/app_toolbar.dart
+++ application/lib/presentation/widget/app_toolbar.dart
@@ -2,35 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 import 'package:xayn_discovery_app/presentation/constants/strings.dart';
 
-class YourToolbar extends StatelessWidget implements PreferredSizeWidget {
+class AppToolbar extends StatelessWidget implements PreferredSizeWidget {
   final String yourTitle;
 
-  const YourToolbar({
+  const AppToolbar({
     Key? key,
     required this.yourTitle,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final style = R.styles.appHeadlineText!;
-    final text = TextSpan(
-      style: style,
-      children: [
-        const TextSpan(text: Strings.your),
-        const TextSpan(text: ' '),
-        TextSpan(
-          text: yourTitle,
-          style: style.copyWith(color: R.colors.accent),
-        ),
-      ],
-    );
+    final title = '${Strings.your} $yourTitle';
+    final text = Text(title, style: R.styles.appHeadlineText);
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.symmetric(
           horizontal: R.dimen.unit3,
           vertical: R.dimen.unit4,
         ),
-        child: RichText(text: text),
+        child: text,
       ),
     );
   }
