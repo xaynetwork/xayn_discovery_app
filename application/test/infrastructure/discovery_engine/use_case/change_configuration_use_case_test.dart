3a88e467254bac39f2b52bfaa1f72a052a354950
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-01-07 11:46:21 +0100

    add basic use case tests

---
+++ application/test/infrastructure/discovery_engine/use_case/change_configuration_use_case_test.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:xayn_architecture/concepts/use_case/test/use_case_test.dart';
+import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/change_configuration_use_case.dart';
+import 'package:xayn_discovery_engine/discovery_engine.dart' hide Configuration;
+
+import '../../../presentation/utils/utils.dart';
+
+void main() {
+  late MockDiscoveryEngine engine;
+
+  setUp(() async {
+    engine = MockDiscoveryEngine();
+  });
+
+  void _setUpSuccess() => when(engine.changeConfiguration(
+              feedMarket: anyNamed('feedMarket'),
+              maxItemsPerFeedBatch: anyNamed('maxItemsPerFeedBatch')))
+          .thenAnswer(
+        (_) => Future.value(const ClientEventSucceeded()),
+      );
+
+  void _setUpFailure() => when(engine.changeConfiguration(
+              feedMarket: anyNamed('feedMarket'),
+              maxItemsPerFeedBatch: anyNamed('maxItemsPerFeedBatch')))
+          .thenAnswer(
+        (_) => Future.value(const EngineExceptionRaised(
+            EngineExceptionReason.wrongEventInResponse)),
+      );
+
+  group('Change configuration', () {
+    useCaseTest(
+      'WHEN changing the configuration THEN expect a ClientEventSucceeded ',
+      setUp: () => _setUpSuccess(),
+      build: () => ChangeConfigurationUseCase(engine),
+      input: [
+        const Configuration(
+          maxItemsPerFeedBatch: 20,
+          feedMarket: 'test',
+        )
+      ],
+      expect: [useCaseSuccess(const ClientEventSucceeded())],
+    );
+
+    useCaseTest(
+      'WHEN changing the configuration and something went wrong THEN expect a EngineExceptionRaised ',
+      setUp: () => _setUpFailure(),
+      build: () => ChangeConfigurationUseCase(engine),
+      input: [
+        const Configuration(
+          maxItemsPerFeedBatch: 20,
+          feedMarket: 'test',
+        )
+      ],
+      expect: [
+        useCaseSuccess(const EngineExceptionRaised(
+            EngineExceptionReason.wrongEventInResponse))
+      ],
+    );
+  });
+}
