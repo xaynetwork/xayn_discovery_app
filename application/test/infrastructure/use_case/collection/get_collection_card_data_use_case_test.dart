7903fba879f89942b259a92289e73415f523ba62
Carmine Toriello <carmine.toriello@xayn.com>
2022-01-10 17:28:22 +0100

    Implemented collection card manager

---
+++ application/test/infrastructure/use_case/collection/get_collection_card_data_use_case_test.dart
@@ -0,0 +1,100 @@
+import 'dart:typed_data';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:xayn_architecture/xayn_architecture_test.dart';
+import 'package:xayn_discovery_app/domain/model/bookmark/bookmark.dart';
+import 'package:xayn_discovery_app/domain/model/collection/collection.dart';
+import 'package:xayn_discovery_app/domain/model/unique_id.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/collection/collection_exception.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/collection/get_collection_card_data_use_case.dart';
+
+import '../use_case_mocks/use_case_mocks.mocks.dart';
+
+void main() {
+  late MockBookmarksRepository bookmarksRepository;
+  late MockCollectionsRepository collectionsRepository;
+  late GetCollectionCardDataUseCase getCollectionCardDataUseCase;
+  final UniqueId collectionId = UniqueId();
+  final Collection collection = Collection(
+    id: UniqueId(),
+    index: 2,
+    name: 'Collection Test',
+  );
+
+  final bookmarks = [
+    Bookmark(
+      id: UniqueId(),
+      collectionId: collectionId,
+      title: 'Bookmark1 title',
+      image: Uint8List.fromList([1, 2, 3]),
+      providerName: 'Provider name',
+      providerThumbnail: Uint8List.fromList([4, 5, 6]),
+      createdAt: DateTime.now().toUtc().toString(),
+    )
+  ];
+
+  setUp(() {
+    bookmarksRepository = MockBookmarksRepository();
+    collectionsRepository = MockCollectionsRepository();
+    getCollectionCardDataUseCase = GetCollectionCardDataUseCase(
+      bookmarksRepository,
+      collectionsRepository,
+    );
+  });
+
+  group(
+    'Get collection card data use case',
+    () {
+      useCaseTest(
+        'WHEN the collection to retrieve data for doesn\'t exist THEN throw an exception',
+        setUp: () =>
+            when(collectionsRepository.getById(collectionId)).thenReturn(null),
+        build: () => getCollectionCardDataUseCase,
+        input: [collectionId],
+        verify: (_) {
+          verifyInOrder([
+            collectionsRepository.getById(collectionId),
+          ]);
+          verifyNoMoreInteractions(collectionsRepository);
+          verifyNoMoreInteractions(bookmarksRepository);
+        },
+        expect: [
+          useCaseFailure(
+            throwsA(const TypeMatcher<CollectionUseCaseException>()),
+          )
+        ],
+      );
+
+      useCaseTest(
+        'WHEN the id of an existing collection has been given THEN retrieve the card data for it',
+        setUp: () {
+          when(collectionsRepository.getById(collectionId))
+              .thenReturn(collection);
+          when(bookmarksRepository.getByCollectionId(collectionId))
+              .thenReturn(bookmarks);
+        },
+        build: () => getCollectionCardDataUseCase,
+        input: [collectionId],
+        verify: (_) {
+          verifyInOrder([
+            collectionsRepository.getById(collectionId),
+            bookmarksRepository.getByCollectionId(collectionId),
+          ]);
+          verifyNoMoreInteractions(collectionsRepository);
+          verifyNoMoreInteractions(bookmarksRepository);
+        },
+        expect: [
+          useCaseSuccess(
+            GetCollectionCardDataUseCaseOut(
+              numOfItems: bookmarks.length,
+              image: Uint8List.fromList(
+                bookmarks.last.image,
+              ),
+            ),
+          )
+        ],
+      );
+    },
+  );
+}
---
+++ application/test/presentation/collections/manager/collection_card_manager_test.dart
@@ -0,0 +1,91 @@
+import 'dart:typed_data';
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:xayn_discovery_app/domain/model/unique_id.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/collection/collection_exception.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/collection/get_collection_card_data_use_case.dart';
+import 'package:xayn_discovery_app/presentation/collections/manager/collection_card_manager.dart';
+import 'package:xayn_discovery_app/presentation/collections/manager/collection_card_state.dart';
+
+import 'collection_card_manager_test.mocks.dart';
+
+@GenerateMocks([GetCollectionCardDataUseCase])
+void main() {
+  late MockGetCollectionCardDataUseCase getcollectionCardDataUseCase;
+  late CollectionCardManager collectionCardManager;
+  late CollectionCardState initialState;
+  late CollectionCardState populatedState;
+  late UniqueId collectionId;
+  late int numOfItems;
+  late Uint8List image;
+
+  setUp(() {
+    getcollectionCardDataUseCase = MockGetCollectionCardDataUseCase();
+    collectionCardManager = CollectionCardManager(getcollectionCardDataUseCase);
+    collectionId = UniqueId();
+    numOfItems = 4;
+    image = Uint8List.fromList([1, 2, 3]);
+    initialState = CollectionCardState.initial();
+    populatedState = CollectionCardState.populated(
+      numOfItems: numOfItems,
+      image: image,
+    );
+  });
+
+  blocTest<CollectionCardManager, CollectionCardState>(
+    'WHEN manager is created THEN emit initial state ',
+    build: () => collectionCardManager,
+    expect: () => [
+      initialState,
+    ],
+  );
+
+  blocTest<CollectionCardManager, CollectionCardState>(
+    'WHEN retrieveCollectionCardInfo method is called THEN call getCollectionCardDataUseCase and emit state with values ',
+    build: () => collectionCardManager,
+    setUp: () => when(getcollectionCardDataUseCase.singleOutput(collectionId))
+        .thenAnswer(
+      (realInvocation) => Future.value(
+        GetCollectionCardDataUseCaseOut(
+          numOfItems: numOfItems,
+          image: image,
+        ),
+      ),
+    ),
+    act: (manager) => manager.retrieveCollectionCardInfo(collectionId),
+    verify: (manager) {
+      verify(getcollectionCardDataUseCase.singleOutput(collectionId)).called(1);
+      verifyNoMoreInteractions(getcollectionCardDataUseCase);
+    },
+    expect: () => [
+      initialState,
+      populatedState,
+    ],
+  );
+
+  blocTest<CollectionCardManager, CollectionCardState>(
+    'WHEN getCollectionCardDataUseCase throws an exception THEN call emit current state with error message ',
+    build: () => collectionCardManager,
+    setUp: () => when(getcollectionCardDataUseCase.singleOutput(collectionId))
+        .thenAnswer(
+      (realInvocation) => Future.error(
+        CollectionUseCaseException(
+            errorMessageGettingCardDataOfNotExistingCollection),
+      ),
+    ),
+    act: (manager) => manager.retrieveCollectionCardInfo(collectionId),
+    verify: (manager) {
+      verify(getcollectionCardDataUseCase.singleOutput(collectionId)).called(1);
+      verifyNoMoreInteractions(getcollectionCardDataUseCase);
+    },
+    expect: () => [
+      initialState,
+      initialState.copyWith(
+        errorMsg: errorMessageGettingCardDataOfNotExistingCollection,
+      ),
+    ],
+  );
+}
