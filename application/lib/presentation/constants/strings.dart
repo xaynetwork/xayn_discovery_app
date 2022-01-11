36d107837180749f72f2b1940291ed4f05a1db51
Simon Joecks <simon.joecks@xayn.com>
2022-01-11 16:35:50 +0700

    Update docs

--- application/lib/presentation/constants/strings.dart
+++ application/lib/presentation/constants/strings.dart
@@ -9,28 +9,18 @@ typedef CountryNames = Map<String, String>;
 typedef GetCountryNamesCallback = Future<CountryNames> Function(
     AppLanguage appLanguage);
 
-/// The Strings class with constants is now replaced with yaml files under messages.
+/// The Strings class with constants is now replaced with yaml files under translations.
 /// Use them in the format `translations[_locale].i18n.yaml`
 /// The `translations.i18n.yaml` is the default english translation.
 ///
 /// In order to regenerate the translations.i18n.dart files run
 ///
-/// # For continuous generation, best while development
-///     `flutter packages pub run build_runner watch`
-/// # For single shot generation:
-///     `flutter pub run build_runner build --delete-conflicting-outputs ; flutter format lib/messages/`
-///  TODO: IN CASE OF ANY ISSUE (OR FOR MORE DETAILS) - REFERENCE TO THE [run_prepush_checks.sh]
+///     `fastlane update_strings`
 ///
 /// see `https://pub.dev/packages/i18n` for further usage of the i18n package
-/// but for now it makes sense to only use yaml files in the default way, until we
-/// agree on an automatic translation process.
 ///
 /// This String allows to switch translations on demand, by changing the _translation
-/// field, which is done by the [switchTranslations(Locale)] method.
-///
-/// For new languages add the  `messages/translations[_newlang].i18n.yaml` file
-/// and extend the `switchTranslations(Locale)` implementation.
-///
+/// field, which is done by the [switchTranslations(appLanguage)] method.
 class Strings {
   static Translations? _translation;
   static const Translations _defaultTranslation = Translations();
