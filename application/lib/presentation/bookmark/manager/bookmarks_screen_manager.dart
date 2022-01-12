f641107adc7b1b9599aa0dd6d35347dce0d547d7
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-01-12 14:33:43 +0100

    use pipe to watch the bookmark repo

--- application/lib/presentation/bookmark/manager/bookmarks_screen_manager.dart
+++ application/lib/presentation/bookmark/manager/bookmarks_screen_manager.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
-import 'package:xayn_discovery_app/domain/model/bookmark/bookmark.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/bookmark/bookmark_exception.dart';
-import 'package:xayn_discovery_app/infrastructure/use_case/bookmark/get_all_bookmarks_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/bookmark/listen_bookmarks_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/bookmark/move_bookmark_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/bookmark/remove_bookmark_use_case.dart';
@@ -15,13 +13,11 @@ import 'bookmarks_screen_state.dart';
 @injectable
 class BookmarksScreenManager extends Cubit<BookmarksScreenState>
     with UseCaseBlocHelper<BookmarksScreenState> {
-  final GetAllBookmarksUseCase _getAllBookmarksUseCase;
   final ListenBookmarksUseCase _listenBookmarksUseCase;
   final RemoveBookmarkUseCase _removeBookmarkUseCase;
   final MoveBookmarkUseCase _moveBookmarkUseCase;
   final DateTimeHandler _dateTimeHandler;
   BookmarksScreenManager(
-    this._getAllBookmarksUseCase,
     this._listenBookmarksUseCase,
     this._removeBookmarkUseCase,
     this._moveBookmarkUseCase,
@@ -32,10 +28,9 @@ class BookmarksScreenManager extends Cubit<BookmarksScreenState>
     _init();
   }
 
-  late final UseCaseSink<UniqueId, ListenBookmarksUseCaseOut>
+  late final UseCaseSink<ListBookmarksUseCaseIn, ListBookmarksUseCaseOut>
       _listenBookmarksHandler;
 
-  List<Bookmark> _bookmarks = [];
   dynamic _useCaseError;
 
   void _init() {
@@ -43,38 +38,7 @@ class BookmarksScreenManager extends Cubit<BookmarksScreenState>
   }
 
   Future<void> enteringScreen(UniqueId collectionId) async {
-    scheduleComputeState(
-      () async {
-        final useCaseResult = await _callGetBookmarksUseCase(collectionId);
-        _bookmarks =
-            useCaseResult != null ? useCaseResult.bookmarks : _bookmarks;
-        _listenBookmarksHandler(collectionId);
-      },
-    );
-  }
-
-  Future<void> updateBookmarksList(UniqueId? collectionId) async {
-    final useCaseResult = await _callGetBookmarksUseCase(collectionId);
-    if (useCaseResult != null) {
-      scheduleComputeState(
-        () => _bookmarks = useCaseResult.bookmarks,
-      );
-    }
-  }
-
-  Future<GetAllBookmarksUseCaseOut?> _callGetBookmarksUseCase(
-    UniqueId? collectionId,
-  ) async {
-    _useCaseError = null;
-    final useCaseResult = await _getAllBookmarksUseCase
-        .singleOutput(GetAllBookmarksUseCaseIn(
-      collectionId: collectionId,
-    ))
-        .catchError((e, _) {
-      scheduleComputeState(() => _useCaseError = e);
-      return null;
-    });
-    return useCaseResult;
+    _listenBookmarksHandler(ListBookmarksUseCaseIn(collectionId: collectionId));
   }
 
   void moveBookmark({
@@ -99,36 +63,35 @@ class BookmarksScreenManager extends Cubit<BookmarksScreenState>
   }
 
   @override
-  Future<BookmarksScreenState?> computeState() async {
-    String errorMsg;
-    if (_useCaseError != null) {
-      final error = _useCaseError;
-      if (error is BookmarkUseCaseException) {
-        errorMsg = error.msg;
-      } else {
-        errorMsg = error.toString();
-      }
-      return state.copyWith(errorMsg: errorMsg);
-    }
+  Future<BookmarksScreenState?> computeState() async =>
+      fold(_listenBookmarksHandler).foldAll((bookmarkEvent, errorReport) {
+        String errorMsg;
 
-    return fold(_listenBookmarksHandler).foldAll((usecaseOut, errorReport) {
-      if (errorReport.exists(_listenBookmarksHandler)) {
-        final error = errorReport.of(_listenBookmarksHandler)!.error;
+        if (_useCaseError != null) {
+          final error = _useCaseError;
+          if (error is BookmarkUseCaseException) {
+            errorMsg = error.msg;
+          } else {
+            errorMsg = error.toString();
+          }
+          return state.copyWith(errorMsg: errorMsg);
+        }
 
-        errorMsg = error.toString();
+        if (errorReport.exists(_listenBookmarksHandler)) {
+          final error = errorReport.of(_listenBookmarksHandler)!.error;
 
-        return state.copyWith(
-          errorMsg: errorMsg,
-        );
-      }
-      final newTimestamp = _dateTimeHandler.getDateTimeNow();
-      if (usecaseOut != null) {
-        _bookmarks = usecaseOut.bookmarks;
-      }
-      return BookmarksScreenState.populated(
-        _bookmarks,
-        newTimestamp,
-      );
-    });
-  }
+          errorMsg = error.toString();
+
+          return state.copyWith(
+            errorMsg: errorMsg,
+          );
+        }
+
+        if (bookmarkEvent != null) {
+          return BookmarksScreenState.populated(
+            bookmarkEvent.bookmarks,
+            _dateTimeHandler.getDateTimeNow(),
+          );
+        }
+      });
 }
