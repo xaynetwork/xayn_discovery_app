699fd7cb7fd9d1fa9cd70be3632447d7b17dd23a
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-01-17 11:46:56 +0100

    fix tests for Windows

--- application/test/presentation/test_utils/test_dependencies.dart
+++ application/test/presentation/test_utils/test_dependencies.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart' hide test;
 import 'package:injectable/injectable.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 
 import 'test_dependencies.config.dart';
@@ -22,33 +20,6 @@ import 'test_dependencies.config.dart';
 Future<void> configureTestDependencies() async {
   TestWidgetsFlutterBinding.ensureInitialized();
   di.allowReassignment = true;
-  packageInfoMock();
-  await configureDependencies();
+  await configureDependencies(environment: test);
   $initTestGetIt(di);
-}
-
-void packageInfoMock() {
-  // needed for Windows
-  di.registerSingletonAsync<PackageInfo>(() => Future.value(PackageInfo(
-        appName: '',
-        buildNumber: '',
-        packageName: '',
-        version: '',
-        buildSignature: '',
-      )));
-
-  TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
-      .setMockMethodCallHandler(
-          const MethodChannel('dev.fluttercommunity.plus/package_info'),
-          (MethodCall methodCall) async {
-    if (methodCall.method == 'getAll') {
-      return <String, dynamic>{
-        // 'appName': 'ABC',
-        // 'packageName': 'A.B.C', // <--- set initial values here
-        // 'version': '1.0.0', // <--- set initial values here
-        // 'buildNumber': '' // <--- set initial values here
-      };
-    }
-    return null;
-  });
 }
