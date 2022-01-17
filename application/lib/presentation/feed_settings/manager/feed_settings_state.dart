c2ce8f7d88ba151f32c39a0f1bb383849d71ef2e
Andrii Antonov <pro100svitlo@gmail.com>
2022-01-17 17:21:06 +0100

    Add feed_settings_manager.dart with tests

---
+++ application/lib/presentation/feed_settings/manager/feed_settings_state.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:xayn_discovery_app/domain/model/country/country.dart';
+
+part 'feed_settings_state.freezed.dart';
+
+@freezed
+class FeedSettingsState with _$FeedSettingsState {
+  const factory FeedSettingsState.initial() = _Initial;
+
+  const factory FeedSettingsState.ready({
+    required int maxSelectedCountryAmount,
+    required List<Country> selectedCountries,
+    required List<Country> unSelectedCountries,
+  }) = FeedSettingsStateReady;
+}
