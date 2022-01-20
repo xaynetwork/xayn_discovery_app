adb7362936a66d55cd760ad724db1d84ae5fa654
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-01-20 11:10:20 +0100

    make a defaultEnvironment

--- application/lib/infrastructure/di/module/utils.dart
+++ application/lib/infrastructure/di/module/utils.dart
@@ -1,11 +1,12 @@
 import 'package:injectable/injectable.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 
 @module
 abstract class UtilsModule {
   @preResolve
   @lazySingleton
-  @Environment('default')
+  @Environment(defaultEnvironmentName)
   Future<PackageInfo> get packageInfo => PackageInfo.fromPlatform();
 
   @preResolve
