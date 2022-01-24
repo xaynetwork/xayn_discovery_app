91a4356ee6081770f4f54b0cac29042616c4b1df
Andrii Antonov <pro100svitlo@gmail.com>
2022-01-24 11:03:40 +0100

    Add feed_settings repository with implementation and tests

---
+++ application/lib/domain/repository/feed_settings_repository.dart
@@ -0,0 +1,16 @@
+import 'package:xayn_discovery_app/domain/model/feed_settings/feed_settings.dart';
+import 'package:xayn_discovery_app/domain/model/repository_event.dart';
+import 'package:xayn_discovery_app/presentation/feed_settings/feed_settings_screen.dart';
+
+/// Repository interface for storing settings, related to the [FeedSettingsScreen]
+abstract class FeedSettingsRepository {
+  /// The [FeedSettings] setter method.
+  void save(FeedSettings settings);
+
+  /// The [FeedSettings] getter method.
+  FeedSettings get settings;
+
+  /// A stream of [FeedSettings].
+  /// Emits when [FeedSettings] changes.
+  Stream<RepositoryEvent<FeedSettings>> watch();
+}
