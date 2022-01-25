6e0eb7f132e53e9f76798b87984c2805b81d6d61
Carmine Toriello <carmine.toriello@xayn.com>
2022-01-25 11:51:21 +0100

    Implemented custom transition when long pressing a collection card

--- application/lib/presentation/utils/widget/card_widget/card_widget.dart
+++ application/lib/presentation/utils/widget/card_widget/card_widget.dart
@@ -4,7 +4,7 @@ import 'package:xayn_design/xayn_design.dart';
 import 'package:xayn_discovery_app/domain/model/document/document_provider.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 import 'package:xayn_discovery_app/presentation/utils/time_ago.dart';
-import 'package:xayn_discovery_app/presentation/utils/widget/card_data.dart';
+import 'package:xayn_discovery_app/presentation/utils/widget/card_widget/card_data.dart';
 import 'package:xayn_discovery_app/presentation/widget/thumbnail_widget.dart';
 
 class CardWidgetData {
