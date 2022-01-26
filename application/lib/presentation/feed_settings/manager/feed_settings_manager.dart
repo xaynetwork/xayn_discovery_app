6e1d4cc8a157b9cff2c48bd34fa737cfb0bdbbe6
Andrii Antonov <pro100svitlo@gmail.com>
2022-01-26 10:15:38 +0100

    Change logic how the error shown on the feedSettings screen
    - update state
    - update manager
    - update tests

--- application/lib/presentation/feed_settings/manager/feed_settings_manager.dart
+++ application/lib/presentation/feed_settings/manager/feed_settings_manager.dart
@@ -3,10 +3,12 @@ import 'package:injectable/injectable.dart';
 
 import 'package:xayn_architecture/concepts/use_case/none.dart';
 import 'package:xayn_architecture/concepts/use_case/use_case_bloc_helper.dart';
+import 'package:xayn_design/xayn_design.dart';
 import 'package:xayn_discovery_app/domain/model/country/country.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/feed_settings/get_selected_countries_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/feed_settings/get_supported_countries_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/feed_settings/save_selected_countries_use_case.dart';
+import 'package:xayn_discovery_app/presentation/widget/tooltip/messages.dart';
 
 import 'feed_settings_state.dart';
 
@@ -34,6 +36,7 @@ class FeedSettingsManager extends Cubit<FeedSettingsState>
 
   final _allCountries = <Country>{};
   final _selectedCountries = <Country>{};
+  TooltipKey? _errorTooltipKey;
 
   Future<void> init() async {
     final countries = await _getSupportedCountriesUseCase.singleOutput(none);
@@ -44,26 +47,30 @@ class FeedSettingsManager extends Cubit<FeedSettingsState>
     scheduleComputeState(() => _selectedCountries.addAll(selectedMarkets));
   }
 
-  /// return [true], if [country] was added successfully
-  Future<bool> onAddCountryPressed(Country country) async {
+  Future<void> onAddCountryPressed(Country country) async {
     if (_selectedCountries.length == maxSelectedCountryAmount) {
-      return false;
+      scheduleComputeState(
+        () => _errorTooltipKey =
+            TooltipKeys.feedSettingsScreenMaxSelectedCountries,
+      );
+      return;
     }
     _selectedCountries.add(country);
     await _saveSelectedFeedMarketsUseCase.singleOutput(_selectedCountries);
     scheduleComputeState(() {});
-    return true;
   }
 
-  /// return [true], if [country] was removed successfully
-  Future<bool> onRemoveCountryPressed(Country country) async {
+  Future<void> onRemoveCountryPressed(Country country) async {
     if (_selectedCountries.length == 1) {
-      return false;
+      scheduleComputeState(
+        () => _errorTooltipKey =
+            TooltipKeys.feedSettingsScreenMinSelectedCountries,
+      );
+      return;
     }
     _selectedCountries.remove(country);
     await _saveSelectedFeedMarketsUseCase.singleOutput(_selectedCountries);
     scheduleComputeState(() {});
-    return true;
   }
 
   @override
@@ -74,11 +81,15 @@ class FeedSettingsManager extends Cubit<FeedSettingsState>
     final unSelectedCountries = List<Country>.from(_allCountries);
     unSelectedCountries
         .removeWhere((country) => _selectedCountries.contains(country));
-    return FeedSettingsState.ready(
-      maxSelectedCountryAmount: maxSelectedCountryAmount,
-      selectedCountries: _selectedCountries.toList(),
-      unSelectedCountries: unSelectedCountries,
-    );
+    try {
+      return FeedSettingsState.ready(
+          maxSelectedCountryAmount: maxSelectedCountryAmount,
+          selectedCountries: _selectedCountries.toList(),
+          unSelectedCountries: unSelectedCountries,
+          errorKey: _errorTooltipKey);
+    } finally {
+      _errorTooltipKey = null;
+    }
   }
 
   @override
