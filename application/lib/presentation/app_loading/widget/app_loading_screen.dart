407fdccb54c554493b8e3a614dcf024cbd1e6d60
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-01-27 15:22:46 +0000

    WIP

--- application/lib/presentation/app_loading/widget/app_loading_screen.dart
+++ application/lib/presentation/app_loading/widget/app_loading_screen.dart
@@ -1,13 +1,31 @@
+// import 'package:flare_loading/flare_loading.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+// import 'package:lottie/lottie.dart';
+// import 'package:rive/rive.dart';
 
 class AppLoadingScreen extends StatelessWidget {
   const AppLoadingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Lottie.asset('assets/logo_animation.json'),
+    return const Center(
+      child: FlareActor(
+        "assets/animation.flr2d",
+        alignment: Alignment.center,
+        fit: BoxFit.contain,
+        // animation: "idle",
+      ),
+      // FlareLoading(
+      //   name: 'assets/animation.flr2d',
+      //   // startAnimation: 'intro',
+      //   // loopAnimation: 'circle',
+      //   // endAnimation: 'end',
+      //   onSuccess: (_) {},
+      //   onError: (a, b) {},
+      // ),
+      // RiveAnimation.asset('assets/animation.flr2d'),
+      //  Lottie.asset('assets/logo_animation.json'),
     );
   }
 }
