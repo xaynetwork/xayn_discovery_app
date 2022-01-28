803bab7872ea17fb8bcdd0720f25b6eb21afe202
Michael <michael.k.malak@gmail.com>
2022-01-28 13:45:33 +0200

    [TB-3207] [TB-3208] Basic integration of Appsflyer (#110)
    * Integrate AppsFlyer
    * refactor AppDelegate.swift
    * make AppsflyerSdk instance final
    * remove unnecessary _createDefaultCollectionUseCase
    * disable iOS ATTracking
    * disable android tracking

--- application/lib/presentation/app/manager/app_manager.dart
+++ application/lib/presentation/app/manager/app_manager.dart
@@ -5,7 +5,6 @@ import 'package:xayn_discovery_app/domain/model/app_theme.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/app_session/save_app_session_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/get_app_theme_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/app_theme/listen_app_theme_use_case.dart';
-import 'package:xayn_discovery_app/infrastructure/use_case/collection/create_default_collection_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/collection/create_or_get_default_collection_use_case.dart';
 import 'package:xayn_discovery_app/presentation/app/manager/app_state.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
@@ -21,7 +20,6 @@ class AppManager extends Cubit<AppState> with UseCaseBlocHelper<AppState> {
     this._listenAppThemeUseCase,
     this._incrementAppSessionUseCase,
     this._createOrGetDefaultCollectionUseCase,
-    this._createDefaultCollectionUseCase,
   ) : super(AppState.empty()) {
     _init();
   }
@@ -31,7 +29,6 @@ class AppManager extends Cubit<AppState> with UseCaseBlocHelper<AppState> {
   final IncrementAppSessionUseCase _incrementAppSessionUseCase;
   final CreateOrGetDefaultCollectionUseCase
       _createOrGetDefaultCollectionUseCase;
-  final CreateDefaultCollectionUseCase _createDefaultCollectionUseCase;
   late final UseCaseValueStream<AppTheme> _appThemeHandler;
 
   late AppTheme _appTheme;
@@ -44,8 +41,6 @@ class AppManager extends Cubit<AppState> with UseCaseBlocHelper<AppState> {
           .call(R.strings.defaultCollectionNameReadLater);
       _appTheme = await _getAppThemeUseCase.singleOutput(none);
       _appThemeHandler = consume(_listenAppThemeUseCase, initialData: none);
-      _createDefaultCollectionUseCase
-          .singleOutput(R.strings.defaultCollectionNameReadLater);
       _initDone = true;
     });
   }
