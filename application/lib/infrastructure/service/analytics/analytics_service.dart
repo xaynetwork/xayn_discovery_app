7bbe37c102d4a853be551760e47d93250c6645ab
Simon Joecks <simon.joecks@xayn.com>
2022-01-31 16:29:25 +0700

    Safe initialization api

--- application/lib/infrastructure/service/analytics/analytics_service.dart
+++ application/lib/infrastructure/service/analytics/analytics_service.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:xayn_discovery_app/domain/model/analytics/analytics_event.dart';
 import 'package:xayn_discovery_app/infrastructure/env/env.dart';
+import 'package:xayn_discovery_app/infrastructure/util/async_init.dart';
 import 'package:xayn_discovery_app/presentation/utils/logger/logger.dart';
 
 abstract class AnalyticsService {
@@ -12,33 +13,25 @@ abstract class AnalyticsService {
 }
 
 @LazySingleton(as: AnalyticsService)
-class AmplitudeAnalyticsService implements AnalyticsService {
+class AmplitudeAnalyticsService
+    with AsyncInitMixin
+    implements AnalyticsService {
   final Amplitude _amplitude;
-  Future<void>? _ongoingInit;
 
   @visibleForTesting
   AmplitudeAnalyticsService(
       {required Amplitude amplitude, bool initialized = true})
       : _amplitude = amplitude {
     if (!initialized) {
-      _ongoingInit = _init();
+      startInitializing();
     }
   }
 
-  Future<void> _init() async {
+  @override
+  Future<void> init() async {
     await _amplitude.init(Env.amplitudeApiKey);
     await _amplitude.trackingSessionEvents(true);
     await _amplitude.setUseDynamicConfig(true);
-    _ongoingInit = null;
-  }
-
-  Future<void> _checkInit(Future<void> Function() run) async {
-    final ongoingInit = _ongoingInit;
-    if (ongoingInit != null) {
-      await ongoingInit;
-    }
-
-    return run();
   }
 
   @factoryMethod
@@ -46,11 +39,11 @@ class AmplitudeAnalyticsService implements AnalyticsService {
       amplitude: Amplitude.getInstance(), initialized: false);
 
   @override
-  Future<void> flush() => _checkInit(() => _amplitude.uploadEvents());
+  Future<void> flush() => safeRun(() => _amplitude.uploadEvents());
 
   @override
   Future<void> send(AnalyticsEvent event) async {
-    _checkInit(() async {
+    safeRun(() async {
       await _amplitude.logEvent(
         event.type,
         eventProperties: event.properties
---
+++ application/lib/infrastructure/util/async_init.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:async/async.dart';
+import 'package:flutter/cupertino.dart';
+
+/// A helper class to initialize something in [init] that must complete before any
+/// [safeRun] operation runs.
+mixin AsyncInitMixin {
+  static final _operations = <CancelableOperation>[];
+
+  CancelableOperation<bool>? _ongoingInit;
+
+  /// Once [startInitializing] is called [init] needs to complete before
+  /// the safeRun()
+  @mustCallSuper
+  void startInitializing() {
+    _ongoingInit = CancelableOperation.fromFuture(init().then((value) => true),
+        onCancel: () => false);
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      _operations.add(_ongoingInit!);
+    }
+  }
+
+  Future<void> init();
+
+  /// Run an operation after the initialization in [init] is done.
+  @protected
+  @mustCallSuper
+  Future<void> safeRun(FutureOr<void> Function() run) async {
+    final ongoingInit = _ongoingInit;
+    if (ongoingInit != null &&
+        !ongoingInit.isCompleted &&
+        !ongoingInit.isCanceled) {
+      final completed = await ongoingInit.value;
+      if (!completed) {
+        return;
+      }
+    }
+
+    if (ongoingInit != null && ongoingInit.isCanceled) {
+      throw "Operation was already canceled but safeRun has been called regardless. Be sure to never cancel any AsyncInitMixin when it is still used.";
+    }
+
+    return run();
+  }
+
+  /// cancels the ongoing [init] call. Note that the spawned microtask of
+  /// [init] can not be canceled (thus the operations in [init] will be called regardless)
+  /// but the dangling safeRuns that depend on [init] will be terminated.
+  @mustCallSuper
+  Future? cancelInit() {
+    return _ongoingInit?.cancel();
+  }
+
+  @visibleForTesting
+  bool get isCancelled => _ongoingInit?.isCanceled == true;
+
+  @visibleForTesting
+  static Future cancelAll() {
+    final list = _operations.toList();
+    _operations.clear();
+    return Future.wait(list.map((e) => e.cancel()));
+  }
+}
