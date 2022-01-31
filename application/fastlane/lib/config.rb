2f056c6e3b6ed7b1064eedaf8458fa25945f0bfb
Simon Joecks <simon.joecks@xayn.com>
2022-01-31 17:27:42 +0700

    Add beta buildNumber offsets so that it is possible...
    ... to run beta channels next to production channels

--- application/fastlane/lib/config.rb
+++ application/fastlane/lib/config.rb
@@ -1,4 +1,7 @@
 module Config
+  module Keys
+    BUILD_NUMBER_OFFSET = "build_number_offset"
+  end
 
   #release, debug
   BUILD = "debug"
@@ -7,7 +10,7 @@ module Config
   ENV_DEFAULTS = { "SEARCH_API_URL_DEBUG" => "api-gw.xaynet.dev",
                    "SEARCH_API_URL_PRODUCTION" => "api-gw.xayn.com",
                    "IMAGE_FETCHER_URL_DEBUG" => "https://img-fetcher.xaynet.dev",
-                   "IMAGE_FETCHER_URL_PRODUCTION" => "https://img-fetcher.xayn.com" ,
+                   "IMAGE_FETCHER_URL_PRODUCTION" => "https://img-fetcher.xayn.com",
                    "APP_STORE_NUMERICAL_ID_DEBUG" => "1593410545",
                    "APP_STORE_NUMERICAL_ID_PRODUCTION" => "1514123811" }
 
@@ -20,6 +23,15 @@ module Config
   #internal, beta, production
   FLAVOR = "internal"
 
+  # Carefull to change those offsets, they are defining the beta process:
+  # production:       1,     2,     3,  ..., 479, ... |  10480...
+  # beta      :   10001, 10002, 10003, ... 10479, ... |  ^^close beta
+  #
+  # Closing the beta is possible and then production would take over the last
+  # beta buildNumber
+  internalBuildNumberOffset = 0
+  betaBuildNumberOffset = 10000
+
   FLAVOR_MATRIX = {
     "internal" => {
       "ios" => {
@@ -28,6 +40,7 @@ module Config
         "appcenter_target" => "Discovery-App-internal",
         "appcenter_token" => "APPCENTER_IOS_INTERNAL_TOKEN",
         "appcenter_buildfile" => "build/discovery-app.ipa",
+        Keys::BUILD_NUMBER_OFFSET => internalBuildNumberOffset,
       },
       "android" => {
         "id" => "com.xayn.discovery.internal",
@@ -35,6 +48,7 @@ module Config
         "appcenter_target" => "Discovery-App-Android-internal",
         "appcenter_token" => "APPCENTER_ANDROID_INTERNAL_TOKEN",
         "appcenter_buildfile" => "build/app/outputs/flutter-apk/app-release.apk",
+        Keys::BUILD_NUMBER_OFFSET => internalBuildNumberOffset,
       },
     },
     "beta" => {
@@ -45,6 +59,7 @@ module Config
         "appcenter_target" => "Discoveru-App-iOS-beta",
         "appcenter_token" => "APPCENTER_IOS_BETA_TOKEN",
         "appcenter_buildfile" => "build/discovery-app.ipa",
+        Keys::BUILD_NUMBER_OFFSET => betaBuildNumberOffset,
       },
       "android" => {
         "id" => "com.xayn.search",
@@ -52,6 +67,7 @@ module Config
         "appcenter_target" => "Discovery-App-Android-beta",
         "appcenter_token" => "APPCENTER_ANDROID_BETA_TOKEN",
         "appcenter_buildfile" => "build/app/outputs/flutter-apk/app-release.apk",
+        Keys::BUILD_NUMBER_OFFSET => betaBuildNumberOffset,
       },
     },
     "production" => {
