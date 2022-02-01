0803dca08f0e22f0b3768514824d92a6047be5e3
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-01 10:33:58 +0100

    Add purchasable_product.dart with id and tests

---
+++ application/lib/presentation/constants/purchasable_ids.dart
@@ -0,0 +1,7 @@
+import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart';
+
+class PurchasableIds {
+  PurchasableIds._();
+
+  static const PurchasableProductId subscription = 'xayn_subscription';
+}
