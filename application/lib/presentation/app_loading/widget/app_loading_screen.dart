07bd48abb4bc8a4f7a7152e6328a30ffc6e04f6b
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-02-02 21:35:05 +0100

    Clean up

--- application/lib/presentation/app_loading/widget/app_loading_screen.dart
+++ application/lib/presentation/app_loading/widget/app_loading_screen.dart
@@ -1,31 +1,51 @@
-// import 'package:flare_loading/flare_loading.dart';
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-// import 'package:lottie/lottie.dart';
-// import 'package:rive/rive.dart';
+import 'package:lottie/lottie.dart';
+import 'package:xayn_discovery_app/presentation/constants/r.dart';
 
-class AppLoadingScreen extends StatelessWidget {
+class AppLoadingScreen extends StatefulWidget {
   const AppLoadingScreen({Key? key}) : super(key: key);
 
   @override
+  State<AppLoadingScreen> createState() => _AppLoadingScreenState();
+}
+
+class _AppLoadingScreenState extends State<AppLoadingScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+    _controller.addStatusListener(_animationStatusListener);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: FlareActor(
-        "assets/animation.flr2d",
-        alignment: Alignment.center,
-        fit: BoxFit.contain,
-        // animation: "idle",
-      ),
-      // FlareLoading(
-      //   name: 'assets/animation.flr2d',
-      //   // startAnimation: 'intro',
-      //   // loopAnimation: 'circle',
-      //   // endAnimation: 'end',
-      //   onSuccess: (_) {},
-      //   onError: (a, b) {},
-      // ),
-      // RiveAnimation.asset('assets/animation.flr2d'),
-      //  Lottie.asset('assets/logo_animation.json'),
-    );
+    return Scaffold(body: _buildBody());
+  }
+
+  Widget _buildBody() => Center(
+        child: Lottie.asset(
+          R.assets.lottie.splashScreenJson(R.brightness),
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller
+              ..duration = composition.duration
+              ..forward();
+          },
+        ),
+      );
+
+  void _animationStatusListener(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      print('finished');
+    }
   }
 }
