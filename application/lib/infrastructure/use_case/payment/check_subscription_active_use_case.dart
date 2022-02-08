5b87e9d9fa73dea1a37e988a779c0e6d5722bc74
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-08 17:44:13 +0100

    Update check_subscription_active_use_case.dart
    - added AppleVerifyReceiptHelper to it
    - added bool flag isIOs
    - update test with new flows

--- application/lib/infrastructure/use_case/payment/check_subscription_active_use_case.dart
+++ application/lib/infrastructure/use_case/payment/check_subscription_active_use_case.dart
@@ -1,18 +1,34 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
+import 'package:xayn_discovery_app/domain/helper/apple_verify_receipt_helper.dart';
 import 'package:xayn_discovery_app/domain/model/payment/payment_flow_error.dart';
 import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart';
+import 'package:xayn_discovery_app/infrastructure/env/env.dart';
 import 'package:xayn_discovery_app/infrastructure/service/payment/payment_service.dart';
 
 @injectable
 class CheckSubscriptionActiveUseCase
     extends UseCase<PurchasableProductId, bool> {
   final PaymentService _paymentService;
+  final AppleVerifyReceiptHelper _iOsSubscriptionCheckHelper;
+  final bool _isIOs;
 
-  CheckSubscriptionActiveUseCase(this._paymentService);
+  CheckSubscriptionActiveUseCase(
+    this._paymentService,
+    this._iOsSubscriptionCheckHelper,
+  ) : _isIOs = Platform.isIOS;
+
+  @visibleForTesting
+  CheckSubscriptionActiveUseCase.test(
+    this._paymentService,
+    this._iOsSubscriptionCheckHelper, {
+    required bool isIos,
+  }) : _isIOs = isIos;
 
   /// yield [true] if subscription for [PurchasableProduct] with id[param]
   /// is active (restored)
@@ -24,12 +40,51 @@ class CheckSubscriptionActiveUseCase
       throw PaymentFlowError.storeNotAvailable;
     }
 
+    // for android status should be [PurchaseStatus.restored]
+    // but for ios it is [PurchaseStatus.purchased]
     bool checkIfAvailable(PurchaseDetails purchase) =>
-        purchase.status == PurchaseStatus.restored &&
-        purchase.productID == param;
+        purchase.productID == param &&
+        purchase.status == PurchaseStatus.restored;
 
     _paymentService.restorePurchases();
-    final purchases = await _paymentService.purchaseStream.first;
-    yield purchases.where(checkIfAvailable).isNotEmpty;
+
+    // we need timeout here, cos on ios if user has 0 purchases
+    // `purchaseStream.first` will never return
+    final purchases = await _paymentService.purchaseStream.first
+        .timeout(const Duration(milliseconds: 5000), onTimeout: () => []);
+    final filtered = purchases.where(checkIfAvailable);
+
+    if (!_isIOs) {
+      yield filtered.isNotEmpty;
+    } else {
+      if (filtered.isEmpty) {
+        yield false;
+        return;
+      }
+      // for Android this is not critical, to call completePurchase.
+      // but for iOs - it is.
+      // If we will NOT make this call, the number of restored purchase
+      // will grow with every call of `restorePurchases` method.
+      for (final purchase in filtered) {
+        if (purchase.pendingCompletePurchase) {
+          _paymentService.completePurchase(purchase);
+        }
+      }
+
+      final verificationData =
+          filtered.first.verificationData.serverVerificationData;
+
+      final subscriptionExpireDate =
+          await _iOsSubscriptionCheckHelper.getSubscriptionExpireDate(
+        serverVerificationData: verificationData,
+        credentials: Env.appleVerifyReceiptCredentials,
+      );
+      if (subscriptionExpireDate == null) {
+        yield false;
+        return;
+      }
+      final isBeforeToday = DateTime.now().isBefore(subscriptionExpireDate);
+      yield isBeforeToday;
+    }
   }
 }
--- application/test/infrastructure/use_case/payment/check_subscription_active_use_case_test.dart
+++ application/test/infrastructure/use_case/payment/check_subscription_active_use_case_test.dart
@@ -11,23 +11,35 @@ import 'payment_test_data.dart';
 
 void main() {
   late MockPaymentService paymentService;
-  late CheckSubscriptionActiveUseCase useCase;
+  late MockAppleVerifyReceiptHelper verifyReceiptHelper;
+  late CheckSubscriptionActiveUseCase useCaseAndroid;
+  late CheckSubscriptionActiveUseCase useCaseIOs;
   const productId = 'productId';
 
   setUp(() {
     paymentService = MockPaymentService();
+    verifyReceiptHelper = MockAppleVerifyReceiptHelper();
     when(paymentService.isAvailable()).thenAnswer((_) => Future.value(true));
     when(paymentService.restorePurchases()).thenAnswer((_) async => {});
 
-    useCase = CheckSubscriptionActiveUseCase(paymentService);
+    useCaseAndroid = CheckSubscriptionActiveUseCase.test(
+      paymentService,
+      verifyReceiptHelper,
+      isIos: false,
+    );
+    useCaseIOs = CheckSubscriptionActiveUseCase.test(
+      paymentService,
+      verifyReceiptHelper,
+      isIos: true,
+    );
   });
 
   useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
-    'WHEN paymentService not available THEN throw storeNotAvailable error',
+    'for android: WHEN paymentService not available THEN throw storeNotAvailable error',
     setUp: () {
       when(paymentService.isAvailable()).thenAnswer((_) async => false);
     },
-    build: () => useCase,
+    build: () => useCaseAndroid,
     input: {productId},
     expect: [
       useCaseFailure(throwsA(PaymentFlowError.storeNotAvailable)),
@@ -35,15 +47,33 @@ void main() {
     verify: (_) {
       verify(paymentService.isAvailable());
       verifyNoMoreInteractions(paymentService);
+      verifyZeroInteractions(verifyReceiptHelper);
     },
   );
 
   useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
-    'GIVEN purchaseStream with empty list WHEN paymentService is available THEN call restorePurchase and yield false',
+    'for ios: WHEN paymentService not available THEN throw storeNotAvailable error',
+    setUp: () {
+      when(paymentService.isAvailable()).thenAnswer((_) async => false);
+    },
+    build: () => useCaseIOs,
+    input: {productId},
+    expect: [
+      useCaseFailure(throwsA(PaymentFlowError.storeNotAvailable)),
+    ],
+    verify: (_) {
+      verify(paymentService.isAvailable());
+      verifyNoMoreInteractions(paymentService);
+      verifyZeroInteractions(verifyReceiptHelper);
+    },
+  );
+
+  useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+    'for android: GIVEN purchaseStream with empty list WHEN paymentService is available THEN call restorePurchase and yield false',
     setUp: () {
       when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([]));
     },
-    build: () => useCase,
+    build: () => useCaseAndroid,
     input: {productId},
     expect: [useCaseSuccess(false)],
     verify: (_) {
@@ -53,17 +83,37 @@ void main() {
         paymentService.purchaseStream,
       ]);
       verifyNoMoreInteractions(paymentService);
+      verifyZeroInteractions(verifyReceiptHelper);
     },
   );
 
   useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
-    'GIVEN purchaseStream without desired productId WHEN paymentService is available THEN call restorePurchase and yield false',
+    'for ios: GIVEN purchaseStream with empty list WHEN paymentService is available THEN call restorePurchase and yield false',
+    setUp: () {
+      when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([]));
+    },
+    build: () => useCaseIOs,
+    input: {productId},
+    expect: [useCaseSuccess(false)],
+    verify: (_) {
+      verifyInOrder([
+        paymentService.isAvailable(),
+        paymentService.restorePurchases(),
+        paymentService.purchaseStream,
+      ]);
+      verifyNoMoreInteractions(paymentService);
+      verifyZeroInteractions(verifyReceiptHelper);
+    },
+  );
+
+  useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+    'for android: GIVEN purchaseStream without desired productId WHEN paymentService is available THEN call restorePurchase and yield false',
     setUp: () {
       when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([
             createPurchase(PurchaseStatus.pending, id: 'another'),
           ]));
     },
-    build: () => useCase,
+    build: () => useCaseAndroid,
     input: {productId},
     expect: [useCaseSuccess(false)],
     verify: (_) {
@@ -73,6 +123,28 @@ void main() {
         paymentService.purchaseStream,
       ]);
       verifyNoMoreInteractions(paymentService);
+      verifyZeroInteractions(verifyReceiptHelper);
+    },
+  );
+
+  useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+    'for ios: GIVEN purchaseStream without desired productId WHEN paymentService is available THEN call restorePurchase and yield false',
+    setUp: () {
+      when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([
+            createPurchase(PurchaseStatus.pending, id: 'another'),
+          ]));
+    },
+    build: () => useCaseIOs,
+    input: {productId},
+    expect: [useCaseSuccess(false)],
+    verify: (_) {
+      verifyInOrder([
+        paymentService.isAvailable(),
+        paymentService.restorePurchases(),
+        paymentService.purchaseStream,
+      ]);
+      verifyNoMoreInteractions(paymentService);
+      verifyZeroInteractions(verifyReceiptHelper);
     },
   );
 
@@ -84,13 +156,13 @@ void main() {
         .forEach(
       (status) {
         useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
-          'GIVEN status $status WHEN paymentService is available THEN call restorePurchase and yield false',
+          'for android: GIVEN status $status WHEN paymentService is available THEN call restorePurchase and yield false',
           setUp: () {
             when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([
                   createPurchase(PurchaseStatus.pending, id: productId),
                 ]));
           },
-          build: () => useCase,
+          build: () => useCaseAndroid,
           input: {productId},
           expect: [useCaseSuccess(false)],
           verify: (_) {
@@ -100,6 +172,27 @@ void main() {
               paymentService.purchaseStream,
             ]);
             verifyNoMoreInteractions(paymentService);
+            verifyZeroInteractions(verifyReceiptHelper);
+          },
+        );
+        useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+          'for ios: GIVEN status $status WHEN paymentService is available THEN call restorePurchase and yield false',
+          setUp: () {
+            when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([
+                  createPurchase(PurchaseStatus.pending, id: productId),
+                ]));
+          },
+          build: () => useCaseIOs,
+          input: {productId},
+          expect: [useCaseSuccess(false)],
+          verify: (_) {
+            verifyInOrder([
+              paymentService.isAvailable(),
+              paymentService.restorePurchases(),
+              paymentService.purchaseStream,
+            ]);
+            verifyNoMoreInteractions(paymentService);
+            verifyZeroInteractions(verifyReceiptHelper);
           },
         );
       },
@@ -107,13 +200,13 @@ void main() {
   });
 
   useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
-    'GIVEN ${PurchaseStatus.restored} WHEN paymentService is available THEN call restorePurchase and yield true',
+    'for android: GIVEN ${PurchaseStatus.restored} WHEN paymentService is available THEN call restorePurchase and yield true',
     setUp: () {
       when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([
             createPurchase(PurchaseStatus.restored, id: productId),
           ]));
     },
-    build: () => useCase,
+    build: () => useCaseAndroid,
     input: {productId},
     expect: [useCaseSuccess(true)],
     verify: (_) {
@@ -123,6 +216,216 @@ void main() {
         paymentService.purchaseStream,
       ]);
       verifyNoMoreInteractions(paymentService);
+    },
+  );
+
+  group(
+    'for ios: GIVEN ${PurchaseStatus.restored} WHEN paymentService is available',
+    () {
+      setUp(() {
+        when(paymentService.purchaseStream).thenAnswer(
+          (_) => Stream.value([
+            createPurchase(PurchaseStatus.restored, id: productId),
+          ]),
+        );
+        when(
+          verifyReceiptHelper.getSubscriptionExpireDate(
+            serverVerificationData: anyNamed('serverVerificationData'),
+            credentials: anyNamed('credentials'),
+          ),
+        ).thenAnswer((_) async => DateTime.now());
+      });
+      useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+        'GIVEN list of purchases with pendingCompletePurchase == true THEN call completePurchase for all of them',
+        setUp: () {
+          when(paymentService.purchaseStream).thenAnswer((_) => Stream.value([
+                createPurchase(
+                  PurchaseStatus.restored,
+                  id: productId,
+                  pendingCompletePurchase: true,
+                ),
+                createPurchase(
+                  PurchaseStatus.restored,
+                  id: productId,
+                  pendingCompletePurchase: true,
+                ),
+                createPurchase(
+                  PurchaseStatus.restored,
+                  id: productId,
+                  pendingCompletePurchase: true,
+                ),
+              ]));
+        },
+        build: () => useCaseIOs,
+        input: {productId},
+        expect: [useCaseSuccess(false)],
+        verify: (_) {
+          verifyInOrder([
+            paymentService.isAvailable(),
+            paymentService.restorePurchases(),
+            paymentService.purchaseStream,
+            paymentService.completePurchase(any),
+            paymentService.completePurchase(any),
+            paymentService.completePurchase(any),
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: serverVerificationData,
+              credentials: anyNamed('credentials'),
+            ),
+          ]);
+          verifyNoMoreInteractions(paymentService);
+          verifyNoMoreInteractions(verifyReceiptHelper);
+        },
+      );
+
+      useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+        'GIVEN single purchase WHEN verifyReceiptHelper return dateTime > now THEN call restorePurchase and yield true',
+        setUp: () {
+          when(
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: anyNamed('serverVerificationData'),
+              credentials: anyNamed('credentials'),
+            ),
+          ).thenAnswer(
+            (_) async => DateTime.now().add(const Duration(seconds: 1)),
+          );
+        },
+        build: () => useCaseIOs,
+        input: {productId},
+        expect: [useCaseSuccess(true)],
+        verify: (_) {
+          verifyInOrder([
+            paymentService.isAvailable(),
+            paymentService.restorePurchases(),
+            paymentService.purchaseStream,
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: serverVerificationData,
+              credentials: anyNamed('credentials'),
+            ),
+          ]);
+          verifyNoMoreInteractions(paymentService);
+          verifyNoMoreInteractions(verifyReceiptHelper);
+        },
+      );
+
+      useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+        'GIVEN single purchase WHEN verifyReceiptHelper return dateTime == now THEN call restorePurchase and yield false',
+        setUp: () {
+          when(
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: anyNamed('serverVerificationData'),
+              credentials: anyNamed('credentials'),
+            ),
+          ).thenAnswer((_) async => DateTime.now());
+        },
+        build: () => useCaseIOs,
+        input: {productId},
+        expect: [useCaseSuccess(false)],
+        verify: (_) {
+          verifyInOrder([
+            paymentService.isAvailable(),
+            paymentService.restorePurchases(),
+            paymentService.purchaseStream,
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: serverVerificationData,
+              credentials: anyNamed('credentials'),
+            ),
+          ]);
+          verifyNoMoreInteractions(paymentService);
+          verifyNoMoreInteractions(verifyReceiptHelper);
+        },
+      );
+
+      useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+        'GIVEN single purchase WHEN verifyReceiptHelper return dateTime < now THEN call restorePurchase and yield false',
+        setUp: () {
+          when(
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: anyNamed('serverVerificationData'),
+              credentials: anyNamed('credentials'),
+            ),
+          ).thenAnswer(
+            (_) async => DateTime.now().subtract(const Duration(seconds: 1)),
+          );
+        },
+        build: () => useCaseIOs,
+        input: {productId},
+        expect: [useCaseSuccess(false)],
+        verify: (_) {
+          verifyInOrder([
+            paymentService.isAvailable(),
+            paymentService.restorePurchases(),
+            paymentService.purchaseStream,
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: serverVerificationData,
+              credentials: anyNamed('credentials'),
+            ),
+          ]);
+          verifyNoMoreInteractions(paymentService);
+          verifyNoMoreInteractions(verifyReceiptHelper);
+        },
+      );
+
+      useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+        'GIVEN single purchase WHEN verifyReceiptHelper return null THEN call restorePurchase and yield false',
+        setUp: () {
+          when(
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: anyNamed('serverVerificationData'),
+              credentials: anyNamed('credentials'),
+            ),
+          ).thenAnswer((_) async => null);
+        },
+        build: () => useCaseIOs,
+        input: {productId},
+        expect: [useCaseSuccess(false)],
+        verify: (_) {
+          verifyInOrder([
+            paymentService.isAvailable(),
+            paymentService.restorePurchases(),
+            paymentService.purchaseStream,
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: serverVerificationData,
+              credentials: anyNamed('credentials'),
+            ),
+          ]);
+          verifyNoMoreInteractions(paymentService);
+          verifyNoMoreInteractions(verifyReceiptHelper);
+        },
+      );
+
+      useCaseTest<CheckSubscriptionActiveUseCase, PurchasableProductId, bool>(
+        'GIVEN single purchase WHEN verifyReceiptHelper throw PaymentFlowError THEN reThrow that error',
+        setUp: () {
+          when(
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: anyNamed('serverVerificationData'),
+              credentials: anyNamed('credentials'),
+            ),
+          ).thenAnswer(
+            (_) async => throw PaymentFlowError.checkSubscriptionActiveFailed,
+          );
+        },
+        build: () => useCaseIOs,
+        input: {productId},
+        expect: [
+          useCaseFailure(
+            throwsA(PaymentFlowError.checkSubscriptionActiveFailed),
+          )
+        ],
+        verify: (_) {
+          verifyInOrder([
+            paymentService.isAvailable(),
+            paymentService.restorePurchases(),
+            paymentService.purchaseStream,
+            verifyReceiptHelper.getSubscriptionExpireDate(
+              serverVerificationData: serverVerificationData,
+              credentials: anyNamed('credentials'),
+            ),
+          ]);
+          verifyNoMoreInteractions(paymentService);
+          verifyNoMoreInteractions(verifyReceiptHelper);
+        },
+      );
     },
   );
 }
