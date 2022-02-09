b38dfa8bfbc78432ef8db7b731eebbb66e492b7d
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-09 16:50:37 +0100

    Add bugReporting when unknown error happen during aip_error_to_payment_flow_error_mapper.dart

--- application/lib/infrastructure/mappers/aip_error_to_payment_flow_error_mapper.dart
+++ application/lib/infrastructure/mappers/aip_error_to_payment_flow_error_mapper.dart
@@ -2,6 +2,7 @@ import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:injectable/injectable.dart';
 import 'package:xayn_discovery_app/domain/model/payment/payment_flow_error.dart';
 import 'package:xayn_discovery_app/infrastructure/mappers/mapper.dart';
+import 'package:xayn_discovery_app/infrastructure/service/bug_reporting/bug_reporting_service.dart';
 
 const _itemAlreadyOwned = 'BillingResponse.itemAlreadyOwned';
 const _paymentDeclined = 'BillingResponse.error';
@@ -9,6 +10,12 @@ const _paymentDeclined = 'BillingResponse.error';
 @lazySingleton
 class IAPErrorToPaymentFlowErrorMapper
     implements Mapper<IAPError, PaymentFlowError> {
+  final BugReportingService _bugReportingService;
+
+  IAPErrorToPaymentFlowErrorMapper(
+    this._bugReportingService,
+  );
+
   @override
   PaymentFlowError map(IAPError input) {
     switch (input.message) {
@@ -17,6 +24,10 @@ class IAPErrorToPaymentFlowErrorMapper
       case _paymentDeclined:
         return PaymentFlowError.paymentFailed;
       default:
+        _bugReportingService.reportHandledCrash(
+          input.toString(),
+          StackTrace.current,
+        );
         return PaymentFlowError.unknown;
     }
   }
