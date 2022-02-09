4e0bc7f171a349c1ce2b9dcf5e59aa2816f78877
Carmine Toriello <carmine.toriello@xayn.com>
2022-02-09 15:44:37 +0100

    Implemented logic for collection card swipe options

--- application/lib/presentation/collections/collections_screen.dart
+++ application/lib/presentation/collections/collections_screen.dart
@@ -5,6 +5,7 @@ import 'package:xayn_discovery_app/domain/model/collection/collection.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/presentation/bottom_sheet/collection_options/collection_options_menu.dart';
 import 'package:xayn_discovery_app/presentation/bottom_sheet/create_or_rename_collection/widget/create_or_rename_collection_bottom_sheet.dart';
+import 'package:xayn_discovery_app/presentation/bottom_sheet/delete_collection_confirmation/delete_collection_confirmation_bottom_sheet.dart';
 import 'package:xayn_discovery_app/presentation/collections/manager/collection_card_manager.dart';
 import 'package:xayn_discovery_app/presentation/collections/manager/collections_screen_manager.dart';
 import 'package:xayn_discovery_app/presentation/collections/manager/collections_screen_state.dart';
@@ -155,6 +156,7 @@ class _CollectionsScreenState extends State<CollectionsScreen>
           collection,
           () => _showCollectionCardOptions(collection),
         ),
+        onSwipeOptionTap: _onSwipeOptionsTap(collection),
       );
 
   _showAddCollectionBottomSheet() {
@@ -173,4 +175,21 @@ class _CollectionsScreenState extends State<CollectionsScreen>
       ),
     );
   }
+
+  Map<SwipeOptionCollectionCard, VoidCallback> _onSwipeOptionsTap(
+          Collection collection) =>
+      {
+        SwipeOptionCollectionCard.edit: () => showAppBottomSheet(
+              context,
+              builder: (_) => CreateOrRenameCollectionBottomSheet(
+                collection: collection,
+              ),
+            ),
+        SwipeOptionCollectionCard.remove: () => showAppBottomSheet(
+              context,
+              builder: (_) => DeleteCollectionConfirmationBottomSheet(
+                collectionId: collection.id,
+              ),
+            ),
+      };
 }
