381ae6eeabaa0f72930c32dc4a4ef391644c5b04
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-10 12:36:41 +0100

    Create a mapper for payment_flow_error_mapper_to_error_msg_mapper.dart

---
+++ application/test/infrastructure/mapper/payment_flow_error_mapper_to_error_msg_mapper_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:xayn_discovery_app/domain/model/payment/payment_flow_error.dart';
+import 'package:xayn_discovery_app/infrastructure/mappers/payment_flow_error_mapper_to_error_msg_mapper.dart';
+import 'package:xayn_discovery_app/presentation/constants/r.dart';
+
+void main() {
+  final mapper = PaymentFlowErrorToErrorMessageMapper();
+
+  final expectedResults = <String>[
+    R.strings.paymentFlowError.unknown,
+    R.strings.paymentFlowError.storeNotAvailable,
+    R.strings.paymentFlowError.productNotFound,
+    R.strings.paymentFlowError.itemAlreadyOwned,
+    R.strings.paymentFlowError.transactionFailed,
+  ];
+
+  test(
+    'GIVEN paymentErrorFlow WHEN map object THEN receive correct errorMsg',
+    () {
+      final results = PaymentFlowError.values.map(mapper.map).toList();
+      expect(results, equals(expectedResults));
+    },
+  );
+}
