961d559af2436f193e90abc70853435f067bb682
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-02-11 13:32:52 +0100

    address review comment

--- application/lib/presentation/discovery_engine/mixin/change_document_feedback_mixin.dart
+++ application/lib/presentation/discovery_engine/mixin/change_document_feedback_mixin.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/document/document_feedback_context.dart';
 import 'package:xayn_discovery_app/domain/model/document/explicit_document_feedback.dart';
-import 'package:xayn_discovery_app/domain/model/unique_id.dart';
+import 'package:xayn_discovery_app/domain/model/extensions/document_extension.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/change_document_feedback_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/crud_explicit_document_feedback_use_case.dart';
@@ -79,7 +79,7 @@ mixin ChangeDocumentFeedbackMixin<T> on UseCaseBlocHelper<T> {
       await crudExplicitDocumentFeedbackUseCase.singleOutput(
         CrudExplicitDocumentFeedbackUseCaseIn.store(
           ExplicitDocumentFeedback(
-            id: UniqueId.fromTrustedString(document.documentId.toString()),
+            id: document.documentId.uniqueId,
             feedback: feedback,
           ),
         ),
