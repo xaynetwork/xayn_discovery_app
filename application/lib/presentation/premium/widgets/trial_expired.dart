e8d702d41c4e509c853043d4a380f3f8715993cb
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-02-14 13:44:33 +0100

    refactor to make the trial widget usable as a BottomSheet target

--- application/lib/presentation/premium/widgets/trial_expired.dart
+++ application/lib/presentation/premium/widgets/trial_expired.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:xayn_design/xayn_design.dart';
 import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart';
-import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
-import 'package:xayn_discovery_app/presentation/payment/manager/payment_screen_manager.dart';
 
+/// A widget which we use to highlight the 'perks' of subscribing,
+/// includes a number of handlers to cancel, subscribe or enter a promo code.
 class TrialExpired extends StatelessWidget {
+  /// The target product
   final PurchasableProduct _product;
-  late final PaymentScreenManager _paymentScreenManager = di.get();
 
-  TrialExpired({
+  /// Handler for when the subscribe button was tapped.
+  final VoidCallback _onSubscribe;
+
+  /// Only applicable in [TrialExpired.embedded].
+  /// Handler for when the cancel button was tapped.
+  /// Use this to close the bottom sheet when it fires.
+  final VoidCallback? _onCancel;
+
+  /// Handler for when the promo code button was tapped.
+  final VoidCallback _onPromoCode;
+
+  const TrialExpired({
     Key? key,
     required PurchasableProduct product,
+    required VoidCallback onSubscribe,
+    required VoidCallback onPromoCode,
+    VoidCallback? onCancel,
   })  : _product = product,
+        _onSubscribe = onSubscribe,
+        _onPromoCode = onPromoCode,
+        _onCancel = onCancel,
         super(key: key);
 
   @override
@@ -66,40 +83,38 @@ class TrialExpired extends StatelessWidget {
           .map((it) => SettingsCardData.fromTile(it))
           .toList(growable: false));
 
-  Widget _buildSubscribeNow() => AppRaisedButton.text(
-        onPressed: _paymentScreenManager.subscribe,
-        text: R.strings.subscriptionSubscribeNow,
-      );
-
-  Widget _buildSubscriptionOptions() => Row(
+  Widget _buildSubscribeNow() => Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Expanded(
-            child: TextButton(
-              child: Text(
-                R.strings.subscriptionRestore,
-                style: R.styles.appLinkText,
-              ),
-              onPressed: () {
-                // todo: implement restore subscription
-              },
+          if (_onCancel != null)
+            Expanded(
+              child: TextButton(
+                  child: Text(
+                    R.strings.bottomSheetCancel,
+                    style: R.styles.appSecondaryButtonText,
+                  ),
+                  onPressed: _onCancel),
             ),
-          ),
           SizedBox(
             width: R.dimen.unit,
           ),
           Expanded(
-            child: TextButton(
-              child: Text(
-                R.strings.subscriptionPromoCode,
-                style: R.styles.appLinkText,
-              ),
-              onPressed: () {
-                // todo: implement promo code
-              },
+            child: AppRaisedButton.text(
+              onPressed: _onSubscribe,
+              text: R.strings.subscriptionSubscribeNow,
             ),
           ),
         ],
+      );
+
+  Widget _buildSubscriptionOptions() => Center(
+        child: TextButton(
+          child: Text(
+            R.strings.subscriptionPromoCode,
+            style: R.styles.appLinkText,
+          ),
+          onPressed: _onPromoCode,
+        ),
       );
 
   Widget _buildFooter() => Text(
