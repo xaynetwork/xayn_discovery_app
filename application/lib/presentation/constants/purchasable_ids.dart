439d698f99632f89b84df037a0d40ee1ccc33cd8
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-17 15:10:20 +0100

    Update subscription id

--- application/lib/presentation/constants/purchasable_ids.dart
+++ application/lib/presentation/constants/purchasable_ids.dart
@@ -3,5 +3,8 @@ import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart
 class PurchasableIds {
   PurchasableIds._();
 
-  static const PurchasableProductId subscription = 'xayn_subscription';
+  /// Unlimited subscription plan
+  /// created in Feb 2022
+  static const PurchasableProductId subscription =
+      'xayn_subscription_unlimited_feb2022';
 }
