adea65c45d0178f4f70e92e350405be53bf048c4
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-18 15:42:36 +0100

    Migrate purchasable_product_mapper.dart to revenueCat object. Update test

--- application/lib/infrastructure/mappers/purchasable_product_mapper.dart
+++ application/lib/infrastructure/mappers/purchasable_product_mapper.dart
@@ -1,17 +1,16 @@
-import 'package:in_app_purchase/in_app_purchase.dart' hide PurchaseStatus;
 import 'package:injectable/injectable.dart';
+import 'package:purchases_flutter/models/product_wrapper.dart';
 import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart';
 import 'package:xayn_discovery_app/infrastructure/mappers/mapper.dart';
 
 @lazySingleton
-class PurchasableProductMapper
-    implements Mapper<ProductDetails, PurchasableProduct> {
+class PurchasableProductMapper implements Mapper<Product, PurchasableProduct> {
   @override
-  PurchasableProduct map(ProductDetails input) => PurchasableProduct(
-        id: input.id,
+  PurchasableProduct map(Product input) => PurchasableProduct(
+        id: input.identifier,
         title: input.title,
         description: input.description,
-        price: input.price,
+        price: input.priceString,
         status: PurchasableProductStatus.purchasable,
       );
 }
