4a3bc99d8f66a34092c2ceaf324fe1ebb211f7d9
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-18 16:00:21 +0100

    Update get_subscription_details_use_case and test with the RevenueCat objects

--- application/lib/infrastructure/use_case/payment/get_subscription_details_use_case.dart
+++ application/lib/infrastructure/use_case/payment/get_subscription_details_use_case.dart
@@ -1,4 +1,5 @@
 import 'package:injectable/injectable.dart';
+import 'package:purchases_flutter/models/product_wrapper.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/payment/payment_flow_error.dart';
 import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart';
@@ -18,17 +19,12 @@ class GetSubscriptionDetailsUseCase extends UseCase<None, PurchasableProduct> {
 
   @override
   Stream<PurchasableProduct> transaction(None param) async* {
-    final isAvailable = await _paymentService.isAvailable();
-    if (!isAvailable) {
-      throw PaymentFlowError.storeNotAvailable;
-    }
-
     const id = PurchasableIds.subscription;
-    final response = await _paymentService.queryProductDetails({id});
+    final List<Product> products = await _paymentService.getProducts([id]);
 
-    if (response.notFoundIDs.contains(id)) {
+    if (products.isEmpty) {
       throw PaymentFlowError.productNotFound;
     }
-    yield _mapper.map(response.productDetails.first);
+    yield _mapper.map(products.first);
   }
 }
