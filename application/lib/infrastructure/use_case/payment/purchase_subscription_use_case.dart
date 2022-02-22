8f0860d581fea772152edabb9b0fd787ed850f44
Andrii Antonov <pro100svitlo@gmail.com>
2022-02-22 10:22:33 +0100

    Update purchase_subscription_use_case.dart with tests according to the RevenueCat logic

--- application/lib/infrastructure/use_case/payment/purchase_subscription_use_case.dart
+++ application/lib/infrastructure/use_case/payment/purchase_subscription_use_case.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
-import 'package:collection/collection.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
+import 'package:purchases_flutter/object_wrappers.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/payment/payment_flow_error.dart';
 import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart';
@@ -13,7 +13,7 @@ import 'package:xayn_discovery_app/infrastructure/service/payment/payment_servic
 class PurchaseSubscriptionUseCase
     extends UseCase<PurchasableProductId, PurchasableProductStatus> {
   final PaymentService _paymentService;
-  final IAPErrorToPaymentFlowErrorMapper _errorMapper;
+  final PurchasesErrorCodeToPaymentFlowErrorMapper _errorMapper;
 
   PurchaseSubscriptionUseCase(
     this._paymentService,
@@ -22,55 +22,21 @@ class PurchaseSubscriptionUseCase
 
   @override
   Stream<PurchasableProductStatus> transaction(
-      PurchasableProductId param) async* {
-    final isAvailable = await _paymentService.isAvailable();
-    if (!isAvailable) {
-      throw PaymentFlowError.storeNotAvailable;
+    PurchasableProductId param,
+  ) async* {
+    yield PurchasableProductStatus.pending;
+    try {
+      final info = await _paymentService.purchaseProduct(param);
+      yield info.activeSubscriptions.contains(param)
+          ? PurchasableProductStatus.purchased
+          : throw PaymentFlowError.paymentFailed;
+    } on PlatformException catch (e) {
+      var errorCode = PurchasesErrorHelper.getErrorCode(e);
+      if (errorCode == PurchasesErrorCode.purchaseCancelledError) {
+        yield PurchasableProductStatus.canceled;
+        return;
+      }
+      throw _errorMapper.map(errorCode);
     }
-    final response = await _paymentService.queryProductDetails({param});
-
-    if (response.notFoundIDs.contains(param)) {
-      throw PaymentFlowError.productNotFound;
-    }
-
-    final details = response.productDetails.first;
-    _paymentService.buyNonConsumable(
-      purchaseParam: PurchaseParam(productDetails: details),
-    );
-
-    yield* _paymentService.purchaseStream.transform(_getTransformer(param));
   }
-
-  StreamTransformer<List<PurchaseDetails>, PurchasableProductStatus>
-      _getTransformer(PurchasableProductId id) =>
-          StreamTransformer.fromHandlers(
-            handleData: (
-              List<PurchaseDetails> purchases,
-              EventSink<PurchasableProductStatus> sink,
-            ) {
-              final subscription = purchases
-                  .firstWhereOrNull((element) => element.productID == id);
-              if (subscription == null) return;
-              late PurchasableProductStatus status;
-              switch (subscription.status) {
-                case PurchaseStatus.error:
-                  throw _errorMapper.map(subscription.error!);
-                case PurchaseStatus.pending:
-                  status = PurchasableProductStatus.pending;
-                  break;
-                case PurchaseStatus.purchased:
-                  _paymentService.completePurchase(subscription);
-                  status = PurchasableProductStatus.purchased;
-                  break;
-                case PurchaseStatus.restored:
-                  _paymentService.completePurchase(subscription);
-                  status = PurchasableProductStatus.restored;
-                  break;
-                case PurchaseStatus.canceled:
-                  status = PurchasableProductStatus.canceled;
-                  break;
-              }
-              sink.add(status);
-            },
-          );
 }
