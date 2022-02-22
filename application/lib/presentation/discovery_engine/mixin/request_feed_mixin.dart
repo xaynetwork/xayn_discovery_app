542feec4ba9162bb913bf647a7f269c1efaff0a6
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-02-22 16:00:42 +0100

    fix discovery_feed tests

--- application/lib/presentation/discovery_engine/mixin/request_feed_mixin.dart
+++ application/lib/presentation/discovery_engine/mixin/request_feed_mixin.dart
@@ -4,6 +4,7 @@ import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/request_feed_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/request_next_feed_batch_use_case.dart';
+import 'package:xayn_discovery_app/presentation/discovery_engine/mixin/util/use_case_sink_extensions.dart';
 import 'package:xayn_discovery_engine/discovery_engine.dart';
 
 mixin RequestFeedMixin<T> on UseCaseBlocHelper<T> {
@@ -38,6 +39,8 @@ mixin RequestFeedMixin<T> on UseCaseBlocHelper<T> {
     final consumeUseCase = di.get<RequestFeedUseCase>();
 
     _didStartConsuming = true;
-    consume(consumeUseCase, initialData: none);
+
+    consume(consumeUseCase, initialData: none)
+        .autoSubscribe(onError: (e, s) => onError(e, s ?? StackTrace.current));
   }
 }
