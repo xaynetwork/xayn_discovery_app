d95860757995832c10952e5bf1e19e3a972f34be
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-02-22 16:00:42 +0100

    fix discovery_feed tests

--- application/lib/presentation/discovery_engine/mixin/util/use_case_sink_extensions.dart
+++ application/lib/presentation/discovery_engine/mixin/util/use_case_sink_extensions.dart
@@ -3,6 +3,19 @@ import 'package:xayn_architecture/concepts/use_case/use_case_stream.dart';
 
 /// A helper to auto-subscribe to the piped use case handlers inside
 /// Discovery engine mixins.
+extension UseCaseValueStreamExtension<Out> on UseCaseValueStream<Out> {
+  void autoSubscribe({
+    required HandleOnFailure onError,
+    HandleValue<Out>? onValue,
+  }) =>
+      fold(
+        defaultOnError: onError,
+        onValue: onValue ?? (_) {},
+      );
+}
+
+/// A helper to auto-subscribe to the piped use case handlers inside
+/// Discovery engine mixins.
 extension UseCaseSinkExtension<In, Out> on UseCaseSink<In, Out> {
   void autoSubscribe({
     required HandleOnFailure onError,
