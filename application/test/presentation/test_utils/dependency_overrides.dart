8a13251feb5df8685d0e403194c76eed619162a6
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-02-22 11:40:00 +0100

    remove workaround for DocumentFeedbackChange tracking

--- application/test/presentation/test_utils/dependency_overrides.dart
+++ application/test/presentation/test_utils/dependency_overrides.dart
@@ -6,7 +6,6 @@ import 'package:logger/logger.dart';
 import 'package:xayn_architecture/concepts/use_case/none.dart';
 import 'package:xayn_discovery_app/domain/model/analytics/analytics_event.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/app_discovery_engine.dart';
-import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/change_document_feedback_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/request_client/client.dart';
 import 'package:xayn_discovery_app/infrastructure/service/analytics/analytics_service.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/connectivity/connectivity_use_case.dart';
@@ -103,13 +102,6 @@ class TestDiscoveryEngine with AsyncInitMixin implements AppDiscoveryEngine {
 
   @override
   void tempAddEvent(EngineEvent event) => _onEngineEvent.add(event);
-
-  @override
-  DocumentFeedbackChange? resolveChangeDocumentFeedbackParameters(
-      EngineEvent engineEvent) {
-    // TODO: implement resolveChangeDocumentFeedbackParameters
-    return null;
-  }
 
   @override
   Future<void> dispose() async {}
