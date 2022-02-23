4d374c3aaa14458ceceb32e79f9eba9708f22db4
Simon Joecks <simon.joecks@xayn.com>
2022-02-23 10:43:17 +0700

    Document Filter is a filter for either sources or topics

---
+++ application/lib/infrastructure/repository/hive_document_filter_repository.dart
@@ -0,0 +1,24 @@
+import 'package:hive/hive.dart';
+import 'package:hive_crdt/hive_crdt.dart';
+import 'package:injectable/injectable.dart';
+import 'package:xayn_discovery_app/domain/model/document_filter/document_filter.dart';
+import 'package:xayn_discovery_app/domain/repository/document_filter_repository.dart';
+import 'package:xayn_discovery_app/infrastructure/mappers/base_mapper.dart';
+import 'package:xayn_discovery_app/infrastructure/mappers/document_filter_mapper.dart';
+import 'package:xayn_discovery_app/infrastructure/util/box_names.dart';
+
+import 'hive_repository.dart';
+
+@Singleton(as: DocumentFilterRepository)
+class HiveDocumentFilterRepository extends HiveRepository<DocumentFilter>
+    implements DocumentFilterRepository {
+  final DocumentFilterMapper _mapper;
+
+  HiveDocumentFilterRepository(this._mapper);
+
+  @override
+  Box<Record> get box => Hive.box<Record>(BoxNames.documentFilters);
+
+  @override
+  BaseDbEntityMapper<DocumentFilter> get mapper => _mapper;
+}
