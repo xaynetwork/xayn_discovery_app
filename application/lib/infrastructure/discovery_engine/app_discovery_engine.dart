2a617d1e5addaac486629f74ec8d90adde0efecb
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-02-24 15:04:34 +0100

    reduce maxItemsPerFeedBatch to 2

--- application/lib/infrastructure/discovery_engine/app_discovery_engine.dart
+++ application/lib/infrastructure/discovery_engine/app_discovery_engine.dart
@@ -74,7 +74,7 @@ class AppDiscoveryEngine with AsyncInitMixin implements DiscoveryEngine {
       apiBaseUrl: Env.searchApiBaseUrl,
       assetsUrl: Env.aiAssetsUrl,
       applicationDirectoryPath: applicationDocumentsDirectory.path,
-      maxItemsPerFeedBatch: 20,
+      maxItemsPerFeedBatch: 2,
       feedMarkets: markets,
       manifest: await FlutterManifestReader().read(),
     );
