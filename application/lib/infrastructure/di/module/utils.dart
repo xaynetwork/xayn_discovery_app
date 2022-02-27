db66e646384e3d4b12296fb16a0c4ab8f7daaf2f
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-02-27 17:54:19 +0100

    Fix tests

--- application/lib/infrastructure/di/module/utils.dart
+++ application/lib/infrastructure/di/module/utils.dart
@@ -1,9 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
-import 'package:purchases_flutter/purchases_flutter.dart';
-import 'package:xayn_discovery_app/infrastructure/env/env.dart';
-import 'package:xayn_discovery_app/presentation/utils/environment_helper.dart';
 
 @module
 abstract class UtilsModule {
@@ -25,17 +22,4 @@ abstract class UtilsModule {
           buildSignature: '',
         ),
       );
-
-  @preResolve
-  @lazySingleton
-  @test
-  Future<Purchases> preparePurchasesTest() async => Purchases();
-
-  @preResolve
-  @lazySingleton
-  Future<Purchases> preparePurchases() async {
-    Purchases.setDebugLogsEnabled(!EnvironmentHelper.kIsProductionFlavor);
-    await Purchases.setup(Env.revenueCatSdkKey);
-    return Purchases();
-  }
 }
