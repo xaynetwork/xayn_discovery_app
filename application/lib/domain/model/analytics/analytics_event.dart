c4bc5ee14843d6395bb828c4a04f19ed66f99e7a
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-03-01 11:48:50 +0100

    pass "cores" as a setOnce

--- application/lib/domain/model/analytics/analytics_event.dart
+++ application/lib/domain/model/analytics/analytics_event.dart
@@ -1,9 +1,3 @@
-import 'package:system_info2/system_info2.dart';
-
-const String _kCoresEntry = 'cores';
-const String _kCoresSocketEntry = 'socket';
-const String _kCoresVendorEntry = 'vendor';
-const String _kCoresArchEntry = 'arch';
 const String _kTimestampEntry = 'timeStamp';
 
 abstract class AnalyticsEvent {
@@ -13,15 +7,6 @@ abstract class AnalyticsEvent {
   AnalyticsEvent(this.type, {Map<String, dynamic>? properties})
       : properties = {
           _kTimestampEntry: DateTime.now().toUtc().toIso8601String(),
-          _kCoresEntry: SysInfo.cores
-              .map(
-                (it) => {
-                  _kCoresSocketEntry: it.socket,
-                  _kCoresVendorEntry: it.vendor,
-                  _kCoresArchEntry: it.architecture.name,
-                },
-              )
-              .toList(growable: false),
           if (properties != null) ...properties,
         };
 }
