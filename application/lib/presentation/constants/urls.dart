52f30eb022d6383ac440c41005670097c844673e
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-03-07 12:43:23 +0100

    Update privacy policy link to beta

--- application/lib/presentation/constants/urls.dart
+++ application/lib/presentation/constants/urls.dart
@@ -2,7 +2,7 @@ class Urls {
   Urls._();
 
   static const termsAndConditions = 'https://beta.xayn.com/terms-of-use';
-  static const privacyPolicy = 'https://about.xayn.com/privacy-policy';
+  static const privacyPolicy = 'https://beta.xayn.com/privacy-policy';
   static const imprint = 'https://about.xayn.com/imprint';
   static const aboutXayn = 'https://about.xayn.com/about-us';
   static const carbonNeutral = 'https://xayn.com/product/sustainability';
