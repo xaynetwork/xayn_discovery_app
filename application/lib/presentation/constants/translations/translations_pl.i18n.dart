fb5d5a04d0d8f7765862d0c39df911dbcc612506
Simon Joecks <simon.joecks@gmail.com>
2022-03-07 14:19:22 +0700

    Update Strings and add dutch, french, spanish and polish

---
+++ application/lib/presentation/constants/translations/translations_pl.i18n.dart
@@ -0,0 +1,332 @@
+// GENERATED FILE, do not edit!
+// ignore_for_file: unused_element, unused_field
+import 'package:i18n/i18n.dart' as i18n;
+import 'translations.i18n.dart';
+
+String get _languageCode => 'pl';
+String _plural(int count,
+        {String? zero,
+        String? one,
+        String? two,
+        String? few,
+        String? many,
+        String? other}) =>
+    i18n.plural(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+String _ordinal(int count,
+        {String? zero,
+        String? one,
+        String? two,
+        String? few,
+        String? many,
+        String? other}) =>
+    i18n.ordinal(
+      count,
+      _languageCode,
+      zero: zero,
+      one: one,
+      two: two,
+      few: few,
+      many: many,
+      other: other,
+    );
+String _cardinal(
+  int count, {
+  String? zero,
+  String? one,
+  String? two,
+  String? few,
+  String? many,
+  String? other,
+}) =>
+    i18n.cardinal(
+      count,
+      _languageCode,
+      zero: zero,
+      one: one,
+      two: two,
+      few: few,
+      many: many,
+      other: other,
+    );
+
+class TranslationsPl extends Translations {
+  const TranslationsPl();
+  String get locale => "pl";
+  String get languageCode => "pl";
+  String get your => """Twoje""";
+  String get activeSearchSearchHint => """Wyszukiwanie""";
+  String get settingsTitle => """Twoje Ustawienia""";
+  String get settingsSectionTitleAppTheme => """Twój motyw aplikacji""";
+  String get settingsSectionScrollDirection => """Kierunek discovery feed""";
+  String get settingsSectionTitleGeneralInfo => """Informacje ogólne""";
+  String get settingsSectionTitleHelpImprove => """Pomóż nam ulepszyć!""";
+  String get settingsSectionTitleSpreadTheWord => """Podziel się""";
+  String get settingsAppThemeSystem => """Domyślny""";
+  String get settingsAppThemeLight => """Tryb jasny""";
+  String get settingsAppThemeDark => """Tryb ciemny""";
+  String get settingsScrollDirectionVertical => """Pionowy""";
+  String get settingsScrollDirectionHorizontal => """Poziomy""";
+  String get settingsAboutXayn => """O Xayn""";
+  String get settingsCarbonNeutral =>
+      """Jesteśmy neutralni pod względem emisji dwutlenku węgla!""";
+  String get settingsImprint => """Dane firmy""";
+  String get settingsPrivacyPolicy => """Polityka prywatności""";
+  String get settingsTermsAndConditions => """Warunki użytkowania""";
+  String get settingsHaveFoundBug => """Znalazłeś błąd?""";
+  String get settingsShareBtn => """Podziel się z przyjaciółmi""";
+  String get settingsVersion => """Wersja:""";
+  String get settingsBuild => """Kompilacja:""";
+  String get minAgo => """minuta temu""";
+  String get momentsAgo => """chwilę temu""";
+  String get hourAgo => """godzina temu""";
+  String get hoursAgo => """godzin temu""";
+  String get dayAgo => """dzień temu""";
+  String get daysAgo => """dni temu""";
+  String get readingTimeUnitSingular => """minuta""";
+  String get readingTimeUnitPlural => """minuty""";
+  String get readingTimeSuffix => """pozostało""";
+  String get cannotLoadUrlError =>
+      """Nie można załadować obrazu z adresem url: """;
+  String get personalAreaTitle => """Twoja przestrzeń""";
+  String get personalAreaCollections => """Wasze kolekcje""";
+  String get personalAreaHomeFeed => """Ustawienia kanałów""";
+  String get personalAreaSettings => """Ustawienia aplikacji""";
+  String get comingSoon => """Wkrótce""";
+  String get feedSettingsScreenTitle => """Twój kanał odkryć""";
+  String get feedSettingsScreenContryListHint =>
+      """Wybierz kraje, z których chcesz otrzymywać wiadomości (maks. %s)""";
+  String get feedSettingsScreenActiveCountryListSubtitle => """Kraje aktywne""";
+  String get feedSettingsScreenInActiveCountryListSubtitle =>
+      """Kraje nieaktywne""";
+  String get feedSettingsScreenMinSelectedCountriesError =>
+      """Wybierz co najmniej jeden kraj""";
+  String get feedSettingsScreenMaxSelectedCountriesError =>
+      """Już wybrałeś %s krajów""";
+  String get bottomSheetCancel => """Anuluj""";
+  String get langNameFrench => """Francuski""";
+  String get langNameDutch => """Holenderski""";
+  String get langNameEnglish => """Angielski""";
+  String get langNameGerman => """Niemiecki""";
+  String get langNamePolish => """Polski""";
+  String get langNameSpanish => """Hiszpański""";
+  String get bottomSheetApply => """Zastosuj""";
+  String get bottomSheetCreate => """Utwórz""";
+  String get bottomSheetCreateCollectionHeader => """Utwórz nową kolekcję""";
+  String get bottomSheetCreateCollectionTextFieldHint => """Nazwa kolekcji""";
+  String get errorMsgTryingToCreateAgainDefaultCollection =>
+      """Znowu próbujesz utworzyć domyślną kolekcję""";
+  String get errorMsgTryingToRemoveDefaultCollection =>
+      """Nie możesz usunąć domyślnej kolekcji""";
+  String get articles => """artykuły""";
+  String get article => """artykuł""";
+  String get collectionsScreenTitle => """Twoje kolekcje""";
+  String get defaultCollectionNameReadLater => """Przeczytaj później""";
+  String get bookmarkSnackBarSavedTo => """Zapisano do %s""";
+  String get bottomSheetSaveTo => """Zapisz do""";
+  String get readerModeBylinePrefix => """Autor""";
+  String get errorMsgCollectionDoesntExist => """Kolekcja nie istnieje""";
+  String get errorMsgBookmarkDoesntExist => """Zakładka nie istnieje""";
+  String get errorMsgCollectionNameAlreadyUsed =>
+      """Ta nazwa jest już używana""";
+  String get errorMsgCollectionNameIsInvalid =>
+      """Nazwa musi zawierać mniej niż %s znaków""";
+  String get bottomSheetRename => """Przemianować""";
+  String get bottomSheetDelete => """Usunąć""";
+  String get bottomSheetDeleteAll => """Usuń wszystko""";
+  String get bottomSheetMoveBookmarks => """Ruszaj się""";
+  String get bottomSheetDeleteCollectionHeader => """Jesteś pewny ?""";
+  String get bottomSheetDeleteCollectionWithBookmarksBody =>
+      """Jeśli usuniesz tę kolekcję, każdy artykuł w tym folderze również zostanie usunięty. Czy chcesz kontynuować, czy chcesz wcześniej przenieść wszystkie artykuły?""";
+  String get bottomSheetRenameCollectionHeader => """Zmień nazwę kolekcji""";
+  String get settingsXaynPremium => """Xayn Premium""";
+  String get settingsSectionSubscription => """Subskrypcja""";
+  String get trialBannerSubscribeNow => """Zapisz się teraz""";
+  String get bookmarkScreenNoArticles => """Brak artykułów""";
+  String get trialBannerEndsIn => """Twój okres próbny kończy się za %s dni""";
+  String get trialBannerEndsTomorrow =>
+      """Twój okres próbny kończy się jutro""";
+  String get trialBannerEndsToday => """Twój okres próbny kończy się dzisiaj""";
+  String get trialBannerExpired => """Twój okres próbny wygasł""";
+  String get subscriptionHeader =>
+      """Uzyskaj nieograniczoną zawartość za jedyne""";
+  String get subscriptionPricingError => """Nie można pobrać szczegółów cen""";
+  String get subscriptionPerk1 => """Spersonalizowany strumień""";
+  String get subscriptionPerk2 => """Bez reklam""";
+  String get subscriptionPerk3 => """Bez śledzenia""";
+  String get subscriptionPerk4 =>
+      """Neutralny pod względem emisji dwutlenku węgla""";
+  String get subscriptionPerk5 => """Anuluj w dowolnym momencie""";
+  String get subscriptionSubscribeNow => """Zapisz się teraz""";
+  String get subscriptionRestore => """Przywróć subskrypcję""";
+  String get subscriptionPromoCode => """Mam kod promocyjny""";
+  String get subscriptionDisclaimer =>
+      """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a mi neque. Mauris non volutpat nisi, w iaculis purus. Aenean id semper nibh. Donec finibus elitar ac massa ornare luctus. Etiam vitae dolor at lorem venenatis tincidunt. Sed mattis euismod justo vitae placerat. Pellentesque laoreet sed mi ac laoreet. Mauris mauris mi, commodo vitae enim non, ultrices pellentesque tellus. Pellentesque non tempor nulla, imperdiet metus. Nullam ac congue tellus. Sed w sollicitudin orci. W ac fringilla augue, w hendrerit erat. Aliquam lectus mauris, pellentesque eget ante eget, tristique interdum sem.""";
+  String get settingsSubscribedToHeader => """Subskrybujesz""";
+  String get doneButtonTitle => """Gotowy""";
+  String get subscriptionRenewsMonthlyText =>
+      """Subskrypcja odnawia się co miesiąc w dniu: %s""";
+  String get promoCodeValidUntilText =>
+      """Twój kod promocyjny jest ważny do: %s""";
+  String get subscriptionPlatformInfoApple =>
+      """Jako użytkownik wersji beta dajemy Ci premię za darmo! Możesz zarządzać swoimi subskrypcjami w __Apple App Store__.""";
+  String get subscriptionPlatformInfoGoogle =>
+      """Jako użytkownik wersji beta dajemy Ci premię za darmo! Możesz zarządzać swoimi subskrypcjami w __Sklepie Google Play__.""";
+  PaymentFlowErrorTranslationsPl get paymentFlowError =>
+      PaymentFlowErrorTranslationsPl(this);
+  String get bottomSheetDeleteCollectionWithNoItemsBody =>
+      """Czy na pewno chcesz usunąć tę kolekcję?""";
+  String get bottomSheetMoveSingleBookmark => """Przejdź do innej kolekcji""";
+  String get readerModeSettingsErrorChangesNotApplied =>
+      """Nie udało się zastosować zmian""";
+  String get errorGenericHeaderSomethingWentWrong => """Coś poszło nie tak""";
+  String get errorGenericBodyPleaseTryAgainLater =>
+      """Spróbuj ponownie później.""";
+  String get errorClose => """Blisko""";
+}
+
+class PaymentFlowErrorTranslationsPl extends PaymentFlowErrorTranslations {
+  final TranslationsPl _parent;
+  const PaymentFlowErrorTranslationsPl(this._parent) : super(_parent);
+  String get unknown => """Wystąpił nieznany błąd""";
+  String get storeNotAvailable => """Sklep jest w tej chwili niedostępny""";
+  String get productNotFound => """Produkt nie znaleziony :(""";
+  String get itemAlreadyOwned => """Zostałeś już zasubskrybowany""";
+  String get transactionFailed => """Transakcja płatnicza nie powiodła się""";
+}
+
+Map<String, String> get translationsPlMap => {
+      """your""": """Twoje""",
+      """activeSearchSearchHint""": """Wyszukiwanie""",
+      """settingsTitle""": """Twoje Ustawienia""",
+      """settingsSectionTitleAppTheme""": """Twój motyw aplikacji""",
+      """settingsSectionScrollDirection""": """Kierunek discovery feed""",
+      """settingsSectionTitleGeneralInfo""": """Informacje ogólne""",
+      """settingsSectionTitleHelpImprove""": """Pomóż nam ulepszyć!""",
+      """settingsSectionTitleSpreadTheWord""": """Podziel się""",
+      """settingsAppThemeSystem""": """Domyślny""",
+      """settingsAppThemeLight""": """Tryb jasny""",
+      """settingsAppThemeDark""": """Tryb ciemny""",
+      """settingsScrollDirectionVertical""": """Pionowy""",
+      """settingsScrollDirectionHorizontal""": """Poziomy""",
+      """settingsAboutXayn""": """O Xayn""",
+      """settingsCarbonNeutral""":
+          """Jesteśmy neutralni pod względem emisji dwutlenku węgla!""",
+      """settingsImprint""": """Dane firmy""",
+      """settingsPrivacyPolicy""": """Polityka prywatności""",
+      """settingsTermsAndConditions""": """Warunki użytkowania""",
+      """settingsHaveFoundBug""": """Znalazłeś błąd?""",
+      """settingsShareBtn""": """Podziel się z przyjaciółmi""",
+      """settingsVersion""": """Wersja:""",
+      """settingsBuild""": """Kompilacja:""",
+      """minAgo""": """minuta temu""",
+      """momentsAgo""": """chwilę temu""",
+      """hourAgo""": """godzina temu""",
+      """hoursAgo""": """godzin temu""",
+      """dayAgo""": """dzień temu""",
+      """daysAgo""": """dni temu""",
+      """readingTimeUnitSingular""": """minuta""",
+      """readingTimeUnitPlural""": """minuty""",
+      """readingTimeSuffix""": """pozostało""",
+      """cannotLoadUrlError""":
+          """Nie można załadować obrazu z adresem url: """,
+      """personalAreaTitle""": """Twoja przestrzeń""",
+      """personalAreaCollections""": """Wasze kolekcje""",
+      """personalAreaHomeFeed""": """Ustawienia kanałów""",
+      """personalAreaSettings""": """Ustawienia aplikacji""",
+      """comingSoon""": """Wkrótce""",
+      """feedSettingsScreenTitle""": """Twój kanał odkryć""",
+      """feedSettingsScreenContryListHint""":
+          """Wybierz kraje, z których chcesz otrzymywać wiadomości (maks. %s)""",
+      """feedSettingsScreenActiveCountryListSubtitle""": """Kraje aktywne""",
+      """feedSettingsScreenInActiveCountryListSubtitle""":
+          """Kraje nieaktywne""",
+      """feedSettingsScreenMinSelectedCountriesError""":
+          """Wybierz co najmniej jeden kraj""",
+      """feedSettingsScreenMaxSelectedCountriesError""":
+          """Już wybrałeś %s krajów""",
+      """bottomSheetCancel""": """Anuluj""",
+      """langNameFrench""": """Francuski""",
+      """langNameDutch""": """Holenderski""",
+      """langNameEnglish""": """Angielski""",
+      """langNameGerman""": """Niemiecki""",
+      """langNamePolish""": """Polski""",
+      """langNameSpanish""": """Hiszpański""",
+      """bottomSheetApply""": """Zastosuj""",
+      """bottomSheetCreate""": """Utwórz""",
+      """bottomSheetCreateCollectionHeader""": """Utwórz nową kolekcję""",
+      """bottomSheetCreateCollectionTextFieldHint""": """Nazwa kolekcji""",
+      """errorMsgTryingToCreateAgainDefaultCollection""":
+          """Znowu próbujesz utworzyć domyślną kolekcję""",
+      """errorMsgTryingToRemoveDefaultCollection""":
+          """Nie możesz usunąć domyślnej kolekcji""",
+      """articles""": """artykuły""",
+      """article""": """artykuł""",
+      """collectionsScreenTitle""": """Twoje kolekcje""",
+      """defaultCollectionNameReadLater""": """Przeczytaj później""",
+      """bookmarkSnackBarSavedTo""": """Zapisano do %s""",
+      """bottomSheetSaveTo""": """Zapisz do""",
+      """readerModeBylinePrefix""": """Autor""",
+      """errorMsgCollectionDoesntExist""": """Kolekcja nie istnieje""",
+      """errorMsgBookmarkDoesntExist""": """Zakładka nie istnieje""",
+      """errorMsgCollectionNameAlreadyUsed""": """Ta nazwa jest już używana""",
+      """errorMsgCollectionNameIsInvalid""":
+          """Nazwa musi zawierać mniej niż %s znaków""",
+      """bottomSheetRename""": """Przemianować""",
+      """bottomSheetDelete""": """Usunąć""",
+      """bottomSheetDeleteAll""": """Usuń wszystko""",
+      """bottomSheetMoveBookmarks""": """Ruszaj się""",
+      """bottomSheetDeleteCollectionHeader""": """Jesteś pewny ?""",
+      """bottomSheetDeleteCollectionWithBookmarksBody""":
+          """Jeśli usuniesz tę kolekcję, każdy artykuł w tym folderze również zostanie usunięty. Czy chcesz kontynuować, czy chcesz wcześniej przenieść wszystkie artykuły?""",
+      """bottomSheetRenameCollectionHeader""": """Zmień nazwę kolekcji""",
+      """settingsXaynPremium""": """Xayn Premium""",
+      """settingsSectionSubscription""": """Subskrypcja""",
+      """trialBannerSubscribeNow""": """Zapisz się teraz""",
+      """bookmarkScreenNoArticles""": """Brak artykułów""",
+      """trialBannerEndsIn""": """Twój okres próbny kończy się za %s dni""",
+      """trialBannerEndsTomorrow""": """Twój okres próbny kończy się jutro""",
+      """trialBannerEndsToday""": """Twój okres próbny kończy się dzisiaj""",
+      """trialBannerExpired""": """Twój okres próbny wygasł""",
+      """subscriptionHeader""":
+          """Uzyskaj nieograniczoną zawartość za jedyne""",
+      """subscriptionPricingError""": """Nie można pobrać szczegółów cen""",
+      """subscriptionPerk1""": """Spersonalizowany strumień""",
+      """subscriptionPerk2""": """Bez reklam""",
+      """subscriptionPerk3""": """Bez śledzenia""",
+      """subscriptionPerk4""":
+          """Neutralny pod względem emisji dwutlenku węgla""",
+      """subscriptionPerk5""": """Anuluj w dowolnym momencie""",
+      """subscriptionSubscribeNow""": """Zapisz się teraz""",
+      """subscriptionRestore""": """Przywróć subskrypcję""",
+      """subscriptionPromoCode""": """Mam kod promocyjny""",
+      """subscriptionDisclaimer""":
+          """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a mi neque. Mauris non volutpat nisi, w iaculis purus. Aenean id semper nibh. Donec finibus elitar ac massa ornare luctus. Etiam vitae dolor at lorem venenatis tincidunt. Sed mattis euismod justo vitae placerat. Pellentesque laoreet sed mi ac laoreet. Mauris mauris mi, commodo vitae enim non, ultrices pellentesque tellus. Pellentesque non tempor nulla, imperdiet metus. Nullam ac congue tellus. Sed w sollicitudin orci. W ac fringilla augue, w hendrerit erat. Aliquam lectus mauris, pellentesque eget ante eget, tristique interdum sem.""",
+      """settingsSubscribedToHeader""": """Subskrybujesz""",
+      """doneButtonTitle""": """Gotowy""",
+      """subscriptionRenewsMonthlyText""":
+          """Subskrypcja odnawia się co miesiąc w dniu: %s""",
+      """promoCodeValidUntilText""":
+          """Twój kod promocyjny jest ważny do: %s""",
+      """subscriptionPlatformInfoApple""":
+          """Jako użytkownik wersji beta dajemy Ci premię za darmo! Możesz zarządzać swoimi subskrypcjami w __Apple App Store__.""",
+      """subscriptionPlatformInfoGoogle""":
+          """Jako użytkownik wersji beta dajemy Ci premię za darmo! Możesz zarządzać swoimi subskrypcjami w __Sklepie Google Play__.""",
+      """paymentFlowError.unknown""": """Wystąpił nieznany błąd""",
+      """paymentFlowError.storeNotAvailable""":
+          """Sklep jest w tej chwili niedostępny""",
+      """paymentFlowError.productNotFound""": """Produkt nie znaleziony :(""",
+      """paymentFlowError.itemAlreadyOwned""":
+          """Zostałeś już zasubskrybowany""",
+      """paymentFlowError.transactionFailed""":
+          """Transakcja płatnicza nie powiodła się""",
+      """bottomSheetDeleteCollectionWithNoItemsBody""":
+          """Czy na pewno chcesz usunąć tę kolekcję?""",
+      """bottomSheetMoveSingleBookmark""": """Przejdź do innej kolekcji""",
+      """readerModeSettingsErrorChangesNotApplied""":
+          """Nie udało się zastosować zmian""",
+      """errorGenericHeaderSomethingWentWrong""": """Coś poszło nie tak""",
+      """errorGenericBodyPleaseTryAgainLater""":
+          """Spróbuj ponownie później.""",
+      """errorClose""": """Blisko""",
+    };
