7e075839af73c4a653915cf038f994dd4c99eb27
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-03-09 13:57:42 +0100

    move code into extract paragraphs use case

--- application/lib/presentation/text_to_speech/manager/text_to_speech_manager.dart
+++ application/lib/presentation/text_to_speech/manager/text_to_speech_manager.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:html/dom.dart' as dom;
 import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/feature.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/discovery_feed/extract_paragraphs_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/discovery_feed/text_to_speech_use_case.dart';
 import 'package:xayn_discovery_app/presentation/feature/manager/feature_manager.dart';
 import 'package:xayn_discovery_app/presentation/text_to_speech/manager/text_to_speech_state.dart';
@@ -13,37 +13,35 @@ class TextToSpeechManager extends Cubit<TextToSpeechState>
     with UseCaseBlocHelper<TextToSpeechState> {
   final FeatureManager _featureManager;
   final TextToSpeechUseCase _textToSpeechUseCase;
+  final ExtractParagraphsUseCase _extractParagraphsUseCase;
   late final UseCaseSink<Utterance, Duration> _textToSpeechSink =
       pipe(_textToSpeechUseCase);
 
   TextToSpeechManager(
     this._textToSpeechUseCase,
+    this._extractParagraphsUseCase,
     this._featureManager,
   ) : super(TextToSpeechState.silent());
 
-  static List<String> extractParagraphs(final String contents) {
-    final element = dom.DocumentFragment.html(contents);
-
-    return element
-        .querySelectorAll('p')
-        .map((it) => it.text)
-        .toList(growable: false);
-  }
-
   void handleStart({
-    required List<String> paragraphs,
+    required String html,
     required String languageCode,
     Uri? uri,
-  }) =>
-      _featureManager.isEnabled(Feature.textToSpeech)
-          ? _textToSpeechSink(
-              Utterance(
-                languageCode: languageCode,
-                paragraphs: paragraphs,
-                uri: uri,
-              ),
-            )
-          : null;
+  }) async {
+    final isFeatureEnabled = _featureManager.isEnabled(Feature.textToSpeech);
+
+    if (!isFeatureEnabled) return;
+
+    final paragraphs = await _extractParagraphsUseCase.singleOutput(html);
+
+    _textToSpeechSink(
+      Utterance(
+        languageCode: languageCode,
+        paragraphs: paragraphs,
+        uri: uri,
+      ),
+    );
+  }
 
   @override
   Future<void> close() async {
