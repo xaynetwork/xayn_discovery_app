40ddb9c11bd7043893822eb1ae5f29b2a4dc8de1
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-03-09 10:51:54 +0100

    use feature flag for enabling tts

--- application/lib/presentation/text_to_speech/manager/text_to_speech_manager.dart
+++ application/lib/presentation/text_to_speech/manager/text_to_speech_manager.dart
@@ -1,30 +1,39 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
+import 'package:xayn_discovery_app/domain/model/feature.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/discovery_feed/text_to_speech_use_case.dart';
+import 'package:xayn_discovery_app/presentation/feature/manager/feature_manager.dart';
 import 'package:xayn_discovery_app/presentation/text_to_speech/manager/text_to_speech_state.dart';
 import 'package:xayn_discovery_app/presentation/utils/logger.dart';
 
 @injectable
 class TextToSpeechManager extends Cubit<TextToSpeechState>
     with UseCaseBlocHelper<TextToSpeechState> {
+  final FeatureManager _featureManager;
   final TextToSpeechUseCase _textToSpeechUseCase;
   late final UseCaseSink<Utterance, Duration> _textToSpeechSink =
       pipe(_textToSpeechUseCase);
 
-  TextToSpeechManager(this._textToSpeechUseCase)
-      : super(TextToSpeechState.silent());
+  TextToSpeechManager(
+    this._textToSpeechUseCase,
+    this._featureManager,
+  ) : super(TextToSpeechState.silent());
 
   void handleStart({
     required List<String> paragraphs,
     required String languageCode,
     Uri? uri,
   }) =>
-      _textToSpeechSink(Utterance(
-        languageCode: languageCode,
-        paragraphs: paragraphs,
-        uri: uri,
-      ));
+      _featureManager.isEnabled(Feature.textToSpeech)
+          ? _textToSpeechSink(
+              Utterance(
+                languageCode: languageCode,
+                paragraphs: paragraphs,
+                uri: uri,
+              ),
+            )
+          : null;
 
   @override
   Future<void> close() {
