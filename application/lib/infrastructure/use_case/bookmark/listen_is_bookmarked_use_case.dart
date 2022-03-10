ae6fd9f6422b4b9ea9e4cd52f7e614388662670f
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-03-10 12:30:38 +0100

    fix starting event

--- application/lib/infrastructure/use_case/bookmark/listen_is_bookmarked_use_case.dart
+++ application/lib/infrastructure/use_case/bookmark/listen_is_bookmarked_use_case.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:injectable/injectable.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
 import 'package:xayn_discovery_app/domain/repository/bookmarks_repository.dart';
@@ -15,11 +14,15 @@ class ListenIsBookmarkedUseCase extends UseCase<UniqueId, bool> {
 
   @override
   Stream<bool> transaction(UniqueId param) async* {
+    // initial event
+    yield _bookmarksRepository.getById(param) != null;
+
+    // changes & deletes are from now on watched
     yield* _bookmarksRepository
         .watch()
         .where((event) => event.id == param)
         .map((_) => _bookmarksRepository.getById(param))
-        .startWith(_bookmarksRepository.getById(param))
+        .distinct()
         .map((event) => event != null);
   }
 }
