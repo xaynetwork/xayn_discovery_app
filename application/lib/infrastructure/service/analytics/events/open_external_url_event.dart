dc3975699ee6487e16a4a1bed0eb6339db502604
Michael <michael.k.malak@gmail.com>
2022-03-14 16:31:39 +0200

    [TB-3488] Add extra param to openExternalUrl event (#281)

--- application/lib/infrastructure/service/analytics/events/open_external_url_event.dart
+++ application/lib/infrastructure/service/analytics/events/open_external_url_event.dart
@@ -2,15 +2,24 @@ import 'package:xayn_discovery_app/domain/model/analytics/analytics_event.dart';
 
 const String _kEventType = 'openExternalUrl';
 const String _kParamUrl = 'url';
+const String _kViewMode = 'currentView';
 
 /// An [AnalyticsEvent] which tracks when an external url is opened
 class OpenExternalUrlEvent extends AnalyticsEvent {
   OpenExternalUrlEvent({
     required String url,
+    required CurrentView currentView,
   }) : super(
           _kEventType,
           properties: {
             _kParamUrl: url,
+            _kViewMode: currentView.name,
           },
         );
+}
+
+enum CurrentView {
+  story,
+  reader,
+  settings,
 }
--- application/test/presentation/settings/settings_screen_test.dart
+++ application/test/presentation/settings/settings_screen_test.dart
@@ -8,6 +8,7 @@ import 'package:xayn_design/xayn_design_test.dart';
 import 'package:xayn_discovery_app/domain/model/app_theme.dart';
 import 'package:xayn_discovery_app/domain/model/app_version.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
+import 'package:xayn_discovery_app/infrastructure/service/analytics/events/open_external_url_event.dart';
 import 'package:xayn_discovery_app/presentation/constants/keys.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 import 'package:xayn_discovery_app/presentation/constants/urls.dart';
@@ -170,7 +171,7 @@ void main() {
         manager.stream,
 
         // actual click happened here
-        manager.openExternalUrl(url),
+        manager.openExternalUrl(url, CurrentView.settings),
       ]);
       verifyNoMoreInteractions(manager);
     }
