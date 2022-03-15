8b7b50d80f968bb3f961a4d1898f0d8269eb6f83
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-03-15 21:51:55 +0100

    fix obsolete import

--- application/lib/presentation/discovery_engine/mixin/engine_events_mixin.dart
+++ application/lib/presentation/discovery_engine/mixin/engine_events_mixin.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/engine_events_use_case.dart';
