a9c67c8815c0f3245838846ddfdc32b62b6ba5d4
Simon Joecks <simon.joecks@gmail.com>
2022-03-15 14:39:23 +0700

    Add tooltip message as entry to edit sources.

--- application/lib/presentation/discovery_card/widget/on_bookmark_changed_mixin.dart
+++ application/lib/presentation/discovery_card/widget/on_bookmark_changed_mixin.dart
@@ -1,10 +1,13 @@
 import 'package:xayn_discovery_app/presentation/discovery_card/manager/discovery_card_state.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/widget/discovery_card_base.dart';
 import 'package:xayn_discovery_app/presentation/widget/tooltip/bookmark_messages.dart';
+import 'package:xayn_discovery_app/presentation/widget/tooltip/document_filter_messages.dart';
+import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
 
 mixin OnBookmarkChangedMixin<T extends DiscoveryCardBase>
     on DiscoveryCardBaseState<T> {
   late bool _didShowBookmarkTooltip;
+  late bool _didShowSourceHandleTooltip;
 
   @override
   void initState() {
@@ -26,6 +29,22 @@ mixin OnBookmarkChangedMixin<T extends DiscoveryCardBase>
       );
     }
 
+    if (state.explicitDocumentUserReaction == UserReaction.negative &&
+        !_didShowSourceHandleTooltip) {
+      showTooltip(
+        DocumentFilterKeys.documentFilter,
+        parameters: [
+          // TODO pass arguments to personalize the bottom sheet
+          // context,
+          // widget.document,
+          // state.processedDocument?.getProvider(widget.document.resource),
+          // (tooltipKey) => showTooltip(tooltipKey),
+        ],
+      );
+    }
+
     _didShowBookmarkTooltip = state.isBookmarkToggled;
+    _didShowSourceHandleTooltip =
+        state.explicitDocumentUserReaction == UserReaction.negative;
   }
 }
