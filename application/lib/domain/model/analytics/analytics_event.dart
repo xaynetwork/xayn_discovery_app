405de4a3bccf5edd4fd3aac131824400038b351e
michaelkmalak <michael.k.malak@gmail.com>
2022-03-17 18:33:53 +0200

    add dayOfWeek to analytics events

--- application/lib/domain/model/analytics/analytics_event.dart
+++ application/lib/domain/model/analytics/analytics_event.dart
@@ -1,6 +1,9 @@
+import 'package:intl/intl.dart';
 import 'package:xayn_discovery_app/presentation/utils/map_utils.dart';
 
 const String _kTimestampEntry = 'timeStamp';
+const String _kDay = 'dayOfWeek';
+const String _defaultLocale = 'en_US';
 
 abstract class AnalyticsEvent {
   final String type;
@@ -9,6 +12,7 @@ abstract class AnalyticsEvent {
   AnalyticsEvent(this.type, {Map<String, dynamic>? properties})
       : properties = {
           _kTimestampEntry: DateTime.now().toUtc().toIso8601String(),
+          _kDay: DateFormat.EEEE(_defaultLocale).format(DateTime.now()),
           if (properties != null) ...properties,
         }.toSerializableMap();
 }
