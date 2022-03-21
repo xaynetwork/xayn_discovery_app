17e09407b919ed96eac67a980d1d8d0b35299155
Andrii Antonov <pro100svitlo@gmail.com>
2022-03-21 16:46:31 +0100

    Add SetCollectionAndBookmarksChangesIdentityParam to the app_manager.dart

--- application/test/presentation/app/manager/app_manager_test.dart
+++ application/test/presentation/app/manager/app_manager_test.dart
@@ -19,6 +19,8 @@ void main() {
   late MockAppSettingsRepository appSettingsRepository;
   late MockRenameDefaultCollectionUseCase renameDefaultCollectionUseCase;
   late MockSetInitialIdentityParamsUseCase setInitialIdentityParamsUseCase;
+  late MockSetCollectionAndBookmarksChangesIdentityParam
+      setCollectionAndBookmarksChangesIdentityParam;
   late Collection mockDefaultCollection;
 
   setUp(() {
@@ -30,6 +32,8 @@ void main() {
         MockCreateOrGetDefaultCollectionUseCase();
     renameDefaultCollectionUseCase = MockRenameDefaultCollectionUseCase();
     setInitialIdentityParamsUseCase = MockSetInitialIdentityParamsUseCase();
+    setCollectionAndBookmarksChangesIdentityParam =
+        MockSetCollectionAndBookmarksChangesIdentityParam();
     appSettingsRepository = MockAppSettingsRepository();
 
     when(appSettingsRepository.settings).thenReturn(AppSettings.initial());
@@ -50,6 +54,9 @@ void main() {
     when(setInitialIdentityParamsUseCase.call(none)).thenAnswer(
       (_) async => const [UseCaseResult.success(none)],
     );
+    when(setCollectionAndBookmarksChangesIdentityParam.call(none)).thenAnswer(
+      (_) async => const [UseCaseResult.success(none)],
+    );
   });
 
   AppManager create() => AppManager(
@@ -58,6 +65,7 @@ void main() {
         createOrGetDefaultCollectionUseCase,
         renameDefaultCollectionUseCase,
         setInitialIdentityParamsUseCase,
+        setCollectionAndBookmarksChangesIdentityParam,
         appSettingsRepository,
       );
 
