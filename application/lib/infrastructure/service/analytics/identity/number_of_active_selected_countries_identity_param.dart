a43c3ae38bcbf64f574eb122e0b8c4c099cdad89
Andrii Antonov <pro100svitlo@gmail.com>
2022-03-21 16:46:14 +0100

    Add set_collection_and_bookmark_changes_identity_param_use_case.dart

--- application/lib/infrastructure/service/analytics/identity/number_of_active_selected_countries_identity_param.dart
+++ application/lib/infrastructure/service/analytics/identity/number_of_active_selected_countries_identity_param.dart
@@ -1,8 +1,9 @@
 import 'package:xayn_discovery_app/infrastructure/service/analytics/identity/base/identity_param.dart';
 
 class NumberOfActiveSelectedCountriesIdentityParam extends IdentityParam {
-  const NumberOfActiveSelectedCountriesIdentityParam(int numberOfSelectedCountries)
-      : super(
+  const NumberOfActiveSelectedCountriesIdentityParam(
+    int numberOfSelectedCountries,
+  ) : super(
           IdentityKeys.numberOfActiveSelectedCountries,
           numberOfSelectedCountries,
         );
