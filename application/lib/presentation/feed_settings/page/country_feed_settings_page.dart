4f32cf0dad3a6c0df0483c05f51038045bfc306c
Simon Joecks <simon.joecks@gmail.com>
2022-03-22 16:48:13 +0700

    TB-3234: Prepare FeedSettings Screen to hold multiple pages

--- application/lib/presentation/feed_settings/page/country_feed_settings_page.dart
+++ application/lib/presentation/feed_settings/page/country_feed_settings_page.dart
@@ -1,19 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:xayn_design/xayn_design.dart';
 import 'package:xayn_discovery_app/domain/model/country/country.dart';
+import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/infrastructure/util/string_extensions.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
+import 'package:xayn_discovery_app/presentation/error/mixin/error_handling_mixin.dart';
+import 'package:xayn_discovery_app/presentation/feed_settings/manager/country_feed_settings_manager.dart';
+import 'package:xayn_discovery_app/presentation/feed_settings/manager/country_feed_settings_state.dart';
 import 'package:xayn_discovery_app/presentation/feed_settings/widget/country_item.dart';
+import 'package:xayn_discovery_app/presentation/widget/tooltip/messages.dart';
 
 typedef OnCountryPressed = Function(Country country);
 
-class CountryFeedSettingsPage extends StatelessWidget {
+class CountryFeedSettingsPage extends StatefulWidget {
+  const CountryFeedSettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<CountryFeedSettingsPage> createState() =>
+      _CountryFeedSettingsPageState();
+}
+
+class _CountryFeedSettingsPageState extends State<CountryFeedSettingsPage>
+    with TooltipStateMixin, ErrorHandlingMixin {
+  late final CountryFeedSettingsManager _manager = di.get();
+
+  @override
+  void initState() {
+    _manager.init();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget buildReadyState(ready) {
+      registerTooltip(
+        key: TooltipKeys.feedSettingsScreenMaxSelectedCountries,
+        params: TooltipParams(
+          label: R.strings.feedSettingsScreenMaxSelectedCountriesError
+              .format(ready.maxSelectedCountryAmount.toString()),
+          builder: (_) => CustomizedTextualNotification(
+            labelTextStyle: R.styles.tooltipHighlightTextStyle,
+          ),
+        ),
+      );
+
+      return SelectCountries(
+        maxSelectedCountryAmount: ready.maxSelectedCountryAmount,
+        selectedCountries: ready.selectedCountries,
+        unSelectedCountries: ready.unSelectedCountries,
+        onAddCountryPressed: _manager.onAddCountryPressed,
+        onRemoveCountryPressed: _manager.onRemoveCountryPressed,
+      );
+    }
+
+    return BlocConsumer<CountryFeedSettingsManager, CountryFeedSettingsState>(
+      bloc: _manager,
+      builder: (_, state) => state.map(
+        initial: (state) => const Center(),
+        ready: buildReadyState,
+      ),
+      listener: (_, state) => state.whenOrNull(
+        ready: (_, __, ___, error) => handleError(error, showTooltip),
+      ),
+    );
+  }
+}
+
+class SelectCountries extends StatelessWidget {
   final int maxSelectedCountryAmount;
   final List<Country> selectedCountries;
   final List<Country> unSelectedCountries;
   final OnCountryPressed onAddCountryPressed;
   final OnCountryPressed onRemoveCountryPressed;
 
-  CountryFeedSettingsPage({
+  SelectCountries({
     required this.maxSelectedCountryAmount,
     required this.selectedCountries,
     required this.unSelectedCountries,
