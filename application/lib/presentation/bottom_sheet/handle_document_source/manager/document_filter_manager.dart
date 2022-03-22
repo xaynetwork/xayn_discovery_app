0d25e0e35d3342f54139d1119ae0812020046410
Simon Joecks <simon.joecks@gmail.com>
2022-03-22 11:08:29 +0700

    Create Apply usecase to apply changes document filter changes to db and engine

--- application/lib/presentation/bottom_sheet/handle_document_source/manager/document_filter_manager.dart
+++ application/lib/presentation/bottom_sheet/handle_document_source/manager/document_filter_manager.dart
@@ -3,6 +3,8 @@ import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/document_filter/document_filter.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/crud/db_entity_crud_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/document_filter/apply_document_filter_in.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/document_filter/apply_document_filter_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/document_filter/crud_document_filter_use_case.dart';
 import 'package:xayn_discovery_app/presentation/bottom_sheet/handle_document_source/manager/document_filter_state.dart';
 import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
@@ -11,10 +13,12 @@ import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
 class DocumentFilterManager extends Cubit<DocumentFilterState>
     with UseCaseBlocHelper<DocumentFilterState> {
   final CrudDocumentFilterUseCase _documentFilterUseCase;
+  final ApplyDocumentFilterUseCase _applyDocumentFilterUseCase;
   final Document _document;
 
   DocumentFilterManager(
     this._documentFilterUseCase,
+    this._applyDocumentFilterUseCase,
 
     /// must be passed with di.get(param1 : document)
     @factoryParam this._document,
@@ -64,6 +68,8 @@ class DocumentFilterManager extends Cubit<DocumentFilterState>
   }
 
   void onApplyChangesPressed() {
-    _documentFilterUseCase.applyChanges(_pendingChanges);
+    _applyDocumentFilterUseCase.singleOutput(
+        ApplyDocumentFilterIn.applyChangesToDbAndEngine(
+            changes: _pendingChanges));
   }
 }
