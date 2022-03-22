e0e1bb47b879d6707587b2d492aff0a814f9353a
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-03-22 12:58:08 +0100

    fix initial screen-type markets not being stored

--- application/lib/infrastructure/use_case/feed_settings/save_initial_feed_market_use_case.dart
+++ application/lib/infrastructure/use_case/feed_settings/save_initial_feed_market_use_case.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
+import 'package:xayn_discovery_app/domain/model/feed/feed_type_markets.dart';
 import 'package:xayn_discovery_app/domain/model/feed_market/feed_market.dart';
 import 'package:xayn_discovery_app/domain/repository/feed_settings_repository.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/feed_type_markets/save_feed_type_markets_use_case.dart';
 
 @injectable
 class SaveInitialFeedMarketUseCase
     extends UseCase<SaveDefaultFeedMarketInput, None> {
   final FeedSettingsRepository _repository;
-
+  final SaveFeedTypeMarketsUseCase _saveFeedTypeMarketsUseCase;
   SaveInitialFeedMarketUseCase(
     this._repository,
+    this._saveFeedTypeMarketsUseCase,
   );
 
   @override
@@ -20,6 +23,9 @@ class SaveInitialFeedMarketUseCase
       final market = _getMarket(param);
       final updatedSettings = settings.copyWith(feedMarkets: {market});
       _repository.save(updatedSettings);
+      // also save local-screen markets in this case
+      _saveFeedTypeMarketsUseCase(FeedTypeMarkets.forFeed({market}));
+      _saveFeedTypeMarketsUseCase(FeedTypeMarkets.forSearch({market}));
     }
     yield none;
   }
