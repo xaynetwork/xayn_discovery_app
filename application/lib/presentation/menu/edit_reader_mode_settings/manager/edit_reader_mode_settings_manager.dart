e28bf8b33dc50aab22fd1375741ff1650d6b858e
michaelkmalak <michael.k.malak@gmail.com>
2022-03-22 12:39:09 +0200

    track editing reader mode settings menu

--- application/lib/presentation/menu/edit_reader_mode_settings/manager/edit_reader_mode_settings_manager.dart
+++ application/lib/presentation/menu/edit_reader_mode_settings/manager/edit_reader_mode_settings_manager.dart
@@ -5,6 +5,10 @@ import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_backgrou
 import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_font_size.dart';
 import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_font_style.dart';
 import 'package:xayn_discovery_app/domain/repository/reader_mode_settings_repository.dart';
+import 'package:xayn_discovery_app/infrastructure/service/analytics/events/reader_mode_background_color_changed_event.dart';
+import 'package:xayn_discovery_app/infrastructure/service/analytics/events/reader_mode_font_size_changed_event.dart';
+import 'package:xayn_discovery_app/infrastructure/service/analytics/events/reader_mode_font_style_changed_event.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/analytics/send_analytics_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/reader_mode_settings/save_reader_mode_background_color_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/reader_mode_settings/save_reader_mode_font_size_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/reader_mode_settings/save_reader_mode_font_style_use_case.dart';
@@ -18,6 +22,7 @@ class EditReaderModeSettingsManager extends Cubit<EditReaderModeSettingsState>
       _saveReaderModeBackgroundColorUseCase;
   final SaveReaderModeFontSizeUseCase _saveReaderModeFontSizeUseCase;
   final SaveReaderModeFontStyleUseCase _saveReaderModeFontStyleUseCase;
+  final SendAnalyticsUseCase _sendAnalyticsUseCase;
 
   late final UseCaseSink<ReaderModeBackgroundColor, ReaderModeBackgroundColor>
       _saveBackgroundColorHandler = pipe(_saveReaderModeBackgroundColorUseCase);
@@ -30,6 +35,7 @@ class EditReaderModeSettingsManager extends Cubit<EditReaderModeSettingsState>
     this._saveReaderModeFontSizeUseCase,
     this._saveReaderModeBackgroundColorUseCase,
     this._saveReaderModeFontStyleUseCase,
+    this._sendAnalyticsUseCase,
     ReaderModeSettingsRepository readerModeSettingsRepository,
   ) : super(EditReaderModeSettingsState(
           readerModeFontStyle: readerModeSettingsRepository.settings.fontStyle,
@@ -38,26 +44,40 @@ class EditReaderModeSettingsManager extends Cubit<EditReaderModeSettingsState>
               readerModeSettingsRepository.settings.backgroundColor,
         ));
 
-  void onDarkBackgroundColorPressed(ReaderModeBackgroundDarkColor darkColor) =>
-      _saveBackgroundColorHandler(
-        state.readerModeBackgroundColor.copyWith(
-          dark: darkColor,
-        ),
-      );
+  void onDarkBackgroundColorPressed(ReaderModeBackgroundDarkColor darkColor) {
+    final backgroundColor = state.readerModeBackgroundColor.copyWith(
+      dark: darkColor,
+    );
+    _saveBackgroundColorHandler(backgroundColor);
+    _sendAnalyticsUseCase(
+      ReaderModeBackgroundColorChanged(backgroundColor: backgroundColor),
+    );
+  }
 
   void onLightBackgroundColorPressed(
-          ReaderModeBackgroundLightColor lightColor) =>
-      _saveBackgroundColorHandler(
-        state.readerModeBackgroundColor.copyWith(
-          light: lightColor,
-        ),
-      );
+      ReaderModeBackgroundLightColor lightColor) {
+    final backgroundColor = state.readerModeBackgroundColor.copyWith(
+      light: lightColor,
+    );
+    _saveBackgroundColorHandler(backgroundColor);
+    _sendAnalyticsUseCase(
+      ReaderModeBackgroundColorChanged(backgroundColor: backgroundColor),
+    );
+  }
 
-  void onFontSizePressed(ReaderModeFontSize fontSize) =>
-      _saveFontSizeHandler(fontSize);
+  void onFontSizePressed(ReaderModeFontSize fontSize) {
+    _saveFontSizeHandler(fontSize);
+    _sendAnalyticsUseCase(
+      ReaderModeFontSizeChanged(fontSize: fontSize),
+    );
+  }
 
-  void onFontStylePressed(ReaderModeFontStyle fontStyle) =>
-      _saveFontStyleHandler(fontStyle);
+  void onFontStylePressed(ReaderModeFontStyle fontStyle) {
+    _saveFontStyleHandler(fontStyle);
+    _sendAnalyticsUseCase(
+      ReaderModeFontStyleChanged(fontStyle: fontStyle),
+    );
+  }
 
   @override
   Future<EditReaderModeSettingsState?> computeState() async => fold3(
