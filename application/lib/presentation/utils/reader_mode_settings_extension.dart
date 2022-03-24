2b3ec60c3aa3366cd88d28e54b5f52f7cb68e707
Andrii Antonov <pro100svitlo@gmail.com>
2022-03-24 14:09:22 +0100

    Remove reader_mode_font_size.dart

--- application/lib/presentation/utils/reader_mode_settings_extension.dart
+++ application/lib/presentation/utils/reader_mode_settings_extension.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_background_color.dart';
-import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_font_size.dart';
 import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_font_style.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 
@@ -34,30 +33,6 @@ extension ReaderModeBackgroundDarkColorExtension
         return R.colors.readerModeBlackBackgroundColor;
       case ReaderModeBackgroundDarkColor.dark:
         return R.colors.readerModeDarkBackgroundColor;
-    }
-  }
-}
-
-extension ReaderModeFontSizeExtension on ReaderModeFontSize {
-  String get svgPath {
-    switch (this) {
-      case ReaderModeFontSize.small:
-        return R.assets.icons.textSmallFont;
-      case ReaderModeFontSize.medium:
-        return R.assets.icons.textMediumFont;
-      case ReaderModeFontSize.large:
-        return R.assets.icons.textLargeFont;
-    }
-  }
-
-  TextStyle get textStyle {
-    switch (this) {
-      case ReaderModeFontSize.small:
-        return R.styles.sHighDensityStyle;
-      case ReaderModeFontSize.medium:
-        return R.styles.mHighDensityStyle;
-      case ReaderModeFontSize.large:
-        return R.styles.lHighDensityStyle;
     }
   }
 }
