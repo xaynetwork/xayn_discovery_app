c4917118d14fa34f673be145589f157a87be5e56
Andrii Antonov <pro100svitlo@gmail.com>
2022-03-24 14:04:35 +0100

    TB-3494: dynamic change font size in reader mode
    - Replace usage of readerModeFontSize with param
    - update usecases and tests
    - update ui and managers

--- application/lib/domain/model/reader_mode/reader_mode_settings.dart
+++ application/lib/domain/model/reader_mode/reader_mode_settings.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:xayn_discovery_app/domain/model/db_entity.dart';
 import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_background_color.dart';
-import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_font_size.dart';
+import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_font_size_param.dart';
 import 'package:xayn_discovery_app/domain/model/reader_mode/reader_mode_font_style.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
 
@@ -11,27 +11,27 @@ part 'reader_mode_settings.freezed.dart';
 class ReaderModeSettings extends DbEntity with _$ReaderModeSettings {
   factory ReaderModeSettings._({
     required ReaderModeBackgroundColor backgroundColor,
-    required ReaderModeFontSize fontSize,
+    required ReaderModeFontSizeParam fontSizeParam,
     required ReaderModeFontStyle fontStyle,
     required UniqueId id,
   }) = _ReaderModeSettings;
 
   factory ReaderModeSettings.global({
     required ReaderModeBackgroundColor backgroundColor,
-    required ReaderModeFontSize fontSize,
+    required ReaderModeFontSizeParam fontSizeParam,
     required ReaderModeFontStyle fontStyle,
   }) =>
       ReaderModeSettings._(
         id: ReaderModeSettings.globalId,
         backgroundColor: backgroundColor,
-        fontSize: fontSize,
+        fontSizeParam: fontSizeParam,
         fontStyle: fontStyle,
       );
 
   factory ReaderModeSettings.initial() => ReaderModeSettings._(
         id: ReaderModeSettings.globalId,
         backgroundColor: ReaderModeBackgroundColor.initial(),
-        fontSize: ReaderModeFontSize.medium,
+        fontSizeParam: ReaderModeFontSizeParams.defaultValue,
         fontStyle: ReaderModeFontStyle.sans,
       );
 
