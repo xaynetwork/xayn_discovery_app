ace3222eea7ae2f0f0727db50b5e9b46c8927f95
Simon Joecks <simon.joecks@gmail.com>
2022-03-30 09:23:02 +0700

    TB-3578: Use a short change log for store releases

--- application/fastlane/lib/git.rb
+++ application/fastlane/lib/git.rb
@@ -1,8 +1,54 @@
+class String
+  def truncate(max)
+    max = max -1
+    length > max ? "#{self[0...max]}…" : self
+  end
+end
+
+
 # Returns a change log, based on the git history with no more than 50 lines
 def gitChangeLog(version)
   Dir.chdir("../") do
     gitlog = sh "git log | head -n50"
     return "Version: #{version}\n\n#{gitlog}"
+  end
+end
+
+PURE_SEMANTIC_VERSION = /^\d.\d{1,3}.\d{1,3}$/
+JIRA_TICKETS = /([A-Z]{2}-[0-9]{1,4})/
+
+def gitShortChangeLog(version, maxSize=-1, addDate=true)
+  Dir.chdir("../") do
+    log = sh 'git log  --pretty="%(describe:tags=true) %as %s" --first-parent --since="1 week ago"'
+    issues = log.scan(JIRA_TICKETS)|[]
+    lines = log.split("\n")
+
+    newLog = ""
+    lastVersion = version
+    lastDate = ""
+    lastLine = ""
+    for l in lines do
+      # %(describe:tags=true) %as %s => tag date title
+      line = l.scan(/(.*?) (.*?) (.*)/)[0]
+      if PURE_SEMANTIC_VERSION.match?(line[0]) && lastVersion != line[0]
+         lastVersion = line[0]
+         newLog += "\n#{lastVersion}\n"
+      end
+      if addDate
+        if lastDate  != line[1]
+           lastDate = line[1]
+           newLog += "\n#{lastDate}\n"
+        end
+      end
+      if lastLine  != line[2]
+         lastLine = line[2]
+         newLog += "- #{lastLine.truncate(50)}\n"
+      end
+    end
+
+    issueString = issues.reduce("") {|a,i| "#{a} #{i[0]}"}
+    completeLog =  "Issues (last 7 days): #{issueString}\n\n#{version}\n#{newLog}"
+    return maxSize > 0 ? completeLog.truncate(maxSize) : completeLog
   end
 end
 
