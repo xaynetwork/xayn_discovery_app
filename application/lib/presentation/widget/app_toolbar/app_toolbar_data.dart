2f99ff3029b9b464f957dd027bed0507c1ad8d88
Carmine Toriello <carmine.toriello@xayn.com>
2022-03-30 15:51:10 +0200

    Show plus and gear icons in the new personal area toolbar

--- application/lib/presentation/widget/app_toolbar/app_toolbar_data.dart
+++ application/lib/presentation/widget/app_toolbar/app_toolbar_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:xayn_discovery_app/presentation/widget/app_toolbar/model/app_toolbar_icon_model.dart';
 
 part 'app_toolbar_data.freezed.dart';
 
@@ -15,4 +16,10 @@ class AppToolbarData with _$AppToolbarData {
     VoidCallback? onPressed,
     Key? iconkey,
   }) = _AppToolbarDataWithTrailingIcon;
+
+  const factory AppToolbarData.withTwoTrailingIcons({
+    @Assert('iconModels.length == 2', 'a list with two AppToolbarIconModel must be passed')
+        required List<AppToolbarIconModel> iconModels,
+    required String title,
+  }) = _AppToolbarDataWithTwoTrailingIcons;
 }
