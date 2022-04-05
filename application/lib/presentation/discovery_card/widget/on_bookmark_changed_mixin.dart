65b14b0463a37757ddb6f597c61f4155409bc718
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-04-05 16:36:54 +0200

    refactor into onw mixin and fix tests

--- application/lib/presentation/discovery_card/widget/on_bookmark_changed_mixin.dart
+++ application/lib/presentation/discovery_card/widget/on_bookmark_changed_mixin.dart
@@ -1,19 +1,13 @@
 import 'dart:async';
 
-import 'package:flutter/widgets.dart';
 import 'package:rxdart/rxdart.dart';
 
-import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/manager/discovery_card_state.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/widget/discovery_card_base.dart';
-import 'package:xayn_discovery_app/presentation/feature/manager/feature_manager.dart';
 import 'package:xayn_discovery_app/presentation/widget/tooltip/bookmark_messages.dart';
-import 'package:xayn_discovery_app/presentation/widget/tooltip/document_filter_messages.dart';
-import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
 
 mixin OnBookmarkChangedMixin<T extends DiscoveryCardBase>
     on DiscoveryCardBaseState<T> {
-  late final FeatureManager _featureManager = di.get();
   late final StreamSubscription<Iterable<BookmarkStatus>>
       _bookmarkTooltipSubscription;
 
@@ -52,30 +46,5 @@ mixin OnBookmarkChangedMixin<T extends DiscoveryCardBase>
     _bookmarkTooltipSubscription.cancel();
 
     super.dispose();
-  }
-
-  @override
-  void didUpdateWidget(T oldWidget) {
-    if (_featureManager.isDocumentFilterEnabled) {
-      // we only want to show the reaction tooltip when the reaction
-      // value changed from *any* to negative.
-      final didBecomeNegative =
-          oldWidget.document.userReaction != UserReaction.negative &&
-              widget.document.userReaction == UserReaction.negative;
-
-      if (didBecomeNegative) {
-        // since we are inside of a build phase here, we cannot call
-        // showTooltip sync, or it complains.
-        WidgetsBinding.instance!.endOfFrame.whenComplete(() => showTooltip(
-              DocumentFilterKeys.documentFilter,
-              parameters: [
-                context,
-                widget.document,
-              ],
-            ));
-      }
-    }
-
-    super.didUpdateWidget(oldWidget);
   }
 }
