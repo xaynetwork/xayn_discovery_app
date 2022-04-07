65903e149dd7fcbb6f27f1b24380d871750c1f23
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-04-07 18:37:46 +0200

    transition to reader mode

--- application/lib/presentation/images/widget/shaders/traversal/traversal_painter.dart
+++ application/lib/presentation/images/widget/shaders/traversal/traversal_painter.dart
@@ -4,13 +4,10 @@ import 'package:flutter/rendering.dart';
 import 'package:xayn_discovery_app/presentation/images/widget/shaders/base_painter.dart';
 
 class TraversalPainter extends BaseAnimationPainter {
-  final bool rendersOnlyOnce;
-
   TraversalPainter({
     required ui.Image image,
     required Color shadowColor,
     required double animationValue,
-    this.rendersOnlyOnce = false,
   }) : super(
           image: image,
           shadowColor: shadowColor,
@@ -19,7 +16,6 @@ class TraversalPainter extends BaseAnimationPainter {
 
   @override
   void paintMedia(ui.Canvas canvas, ui.Image image, ui.Size size, Rect rect) {
-    // scale >= 1.0
     final scale = image.height / rect.height;
     final dx = image.width - rect.width;
     final src = Rect.fromLTWH(
@@ -31,8 +27,4 @@ class TraversalPainter extends BaseAnimationPainter {
 
     canvas.drawImageRect(image, src, rect, Paint());
   }
-
-  @override
-  bool shouldRepaint(TraversalPainter oldDelegate) =>
-      rendersOnlyOnce ? false : super.shouldRepaint(oldDelegate);
 }
