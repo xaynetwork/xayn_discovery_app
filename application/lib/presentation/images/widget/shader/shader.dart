3bb8f275168a2728551de9d4bf9fe475cdc13b40
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-04-14 09:26:31 +0200

    adds docs

--- application/lib/presentation/images/widget/shader/shader.dart
+++ application/lib/presentation/images/widget/shader/shader.dart
@@ -7,11 +7,16 @@ import 'package:xayn_discovery_app/presentation/images/widget/shader/static/stat
 import 'package:xayn_discovery_app/presentation/images/widget/shader/panning/panning_shader.dart';
 import 'package:xayn_discovery_app/presentation/images/widget/shader/zoom/zoom_shader.dart';
 
+/// A list of all possible shaders which can be targeted in the UI.
 enum ShaderType { static, pan, zoom }
 
+/// A factory which creates [ShaderBuilder]s.
 class ShaderFactory {
   ShaderFactory._();
 
+  /// For a given [ShaderType], returns a [ShaderBuilder] method.
+  /// When [transitionToIdle] is true, the shader will animate the current
+  /// animation status to static. If false, it will play in a continuous loop instead.
   static BaseStaticShader Function({
     Key? key,
     required Uint8List bytes,
