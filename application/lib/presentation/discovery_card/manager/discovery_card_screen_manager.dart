d5d1567120836a74646aadc984b855275453488c
michaelkmalak <michael.k.malak@gmail.com>
2022-04-19 12:37:07 +0200

    TB-3671: add missing nav button to bookmarked article

--- application/lib/presentation/discovery_card/manager/discovery_card_screen_manager.dart
+++ application/lib/presentation/discovery_card/manager/discovery_card_screen_manager.dart
@@ -3,6 +3,8 @@ import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/model/error/error_object.dart';
 import 'package:xayn_discovery_app/domain/model/unique_id.dart';
+import 'package:xayn_discovery_app/infrastructure/service/analytics/events/reader_mode_settings_menu_displayed_event.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/analytics/send_analytics_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/document/get_document_use_case.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/manager/discovery_card_screen_state.dart';
 import 'package:xayn_discovery_app/presentation/utils/logger/logger.dart';
@@ -19,10 +21,12 @@ class DiscoveryCardScreenManager extends Cubit<DiscoveryCardScreenState>
     @factoryParam UniqueId? documentId,
     this._getDocumentUseCase,
     this._navActions,
+    this._sendAnalyticsUseCase,
   ) : super(DiscoveryCardScreenState.initial()) {
     _getDocumentHandler(documentId!);
   }
 
+  final SendAnalyticsUseCase _sendAnalyticsUseCase;
   final GetDocumentUseCase _getDocumentUseCase;
   final DiscoveryCardScreenManagerNavActions _navActions;
   late final _getDocumentHandler = pipe(_getDocumentUseCase);
@@ -50,4 +54,11 @@ class DiscoveryCardScreenManager extends Cubit<DiscoveryCardScreenState>
   void onBackPressed() {
     _navActions.onBackPressed();
   }
+
+  void onReaderModeMenuDisplayed({required bool isVisible}) =>
+      _sendAnalyticsUseCase(
+        ReaderModeSettingsMenuDisplayedEvent(
+          isVisible: isVisible,
+        ),
+      );
 }
