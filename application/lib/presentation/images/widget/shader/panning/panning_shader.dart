0ef4e0903c52bb921c70e88a064f1259b5c6f854
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-04-20 13:38:19 +0200

    TB-3712: Use linear curve and make pan move slightly faster

--- application/lib/presentation/images/widget/shader/panning/panning_shader.dart
+++ application/lib/presentation/images/widget/shader/panning/panning_shader.dart
@@ -5,7 +5,7 @@ import 'package:xayn_discovery_app/presentation/images/widget/cached_image.dart'
 import 'package:xayn_discovery_app/presentation/images/widget/shader/base_shader.dart';
 import 'package:xayn_discovery_app/presentation/images/widget/shader/panning/panning_painter.dart';
 
-const int _kPixelDuration = 35;
+const double _kPixelDuration = .065;
 
 class PanningShader extends BaseAnimationShader {
   const PanningShader({
@@ -68,8 +68,7 @@ class _PanningShaderState extends BaseAnimationShaderState<PanningShader> {
       final overflow = srcImage.width - width;
 
       if (overflow > .0) {
-        updateDuration(
-            Duration(milliseconds: overflow.toInt() * _kPixelDuration));
+        updateDuration(Duration(milliseconds: overflow ~/ _kPixelDuration));
       }
     }
 
