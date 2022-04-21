fc49d18c96d8d272311753d79892f2fa8a71b1f7
Andrii Antonov <pro100svitlo@gmail.com>
2022-04-21 12:47:23 +0200

    TB-3607: Add onboarding_status_mapper

---
+++ application/lib/infrastructure/mappers/onboarding_status_mapper.dart
@@ -0,0 +1,56 @@
+import 'package:injectable/injectable.dart';
+import 'package:xayn_discovery_app/domain/model/onboarding/onboarding_status.dart';
+import 'package:xayn_discovery_app/infrastructure/mappers/base_mapper.dart';
+import 'package:xayn_discovery_app/infrastructure/mappers/mapper.dart';
+
+@singleton
+class OnboardingStatusToDbEntityMapMapper
+    extends Mapper<OnboardingStatus, DbEntityMap> {
+  @override
+  DbEntityMap map(OnboardingStatus input) => {
+        OnboardingStatusFields.homeVerticalSwipeDone:
+            input.homeVerticalSwipeDone,
+        OnboardingStatusFields.homeSideSwipeDone: input.homeSideSwipeDone,
+        OnboardingStatusFields.homeManageBookmarksDone:
+            input.homeManageBookmarksDone,
+        OnboardingStatusFields.collectionsManageDone:
+            input.collectionsManageDone,
+      };
+}
+
+@singleton
+class DbEntityMapToOnboardingStatusMapper
+    extends Mapper<DbEntityMap?, OnboardingStatus> {
+  @override
+  OnboardingStatus map(Map? input) {
+    if (input == null) return const OnboardingStatus.initial();
+
+    final homeVerticalSwipeDone =
+        input[OnboardingStatusFields.homeVerticalSwipeDone] ??
+            defaultHomeVerticalSwipe;
+    final homeSideSwipeDone = input[OnboardingStatusFields.homeSideSwipeDone] ??
+        defaultHomeSideSwipeDone;
+    final homeManageBookmarksDone =
+        input[OnboardingStatusFields.homeManageBookmarksDone] ??
+            defaultHomeManageBookmarksDone;
+    final collectionsManageDone =
+        input[OnboardingStatusFields.collectionsManageDone] ??
+            defaultCollectionsManageDone;
+
+    return OnboardingStatus(
+      homeVerticalSwipeDone: homeVerticalSwipeDone,
+      homeSideSwipeDone: homeSideSwipeDone,
+      homeManageBookmarksDone: homeManageBookmarksDone,
+      collectionsManageDone: collectionsManageDone,
+    );
+  }
+}
+
+abstract class OnboardingStatusFields {
+  OnboardingStatusFields._();
+
+  static const homeVerticalSwipeDone = 0;
+  static const homeSideSwipeDone = 1;
+  static const homeManageBookmarksDone = 2;
+  static const collectionsManageDone = 3;
+}
