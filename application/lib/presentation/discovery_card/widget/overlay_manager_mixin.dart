8bbf63307aff2c8de1bbe5d509e4406dacaaecbb
Andrii Antonov <pro100svitlo@gmail.com>
2022-04-26 10:36:37 +0200

    Update overlay_data and overlay_mixin to support bottomSheets

--- application/lib/presentation/discovery_card/widget/overlay_manager_mixin.dart
+++ application/lib/presentation/discovery_card/widget/overlay_manager_mixin.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/widget/overlay_data.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/widget/overlay_manager.dart';
 
 mixin OverlayManagerMixin<T> on Cubit<T> {
-  late final _overlayManager = OverlayManager<T>();
+  late var _overlayManager = OverlayManager<T>();
   StreamSubscription? _subscription;
+
+  @visibleForTesting
+  void setOverlayManager(OverlayManager<T> manager) {
+    _overlayManager = manager;
+  }
 
   OverlayManager<T> get overlayManager {
     _subscription ??= stream.listen((event) {
