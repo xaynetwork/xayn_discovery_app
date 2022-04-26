5d9e1fa78f781c4b21eb7ed5e9858ccc78045820
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-04-26 18:00:27 +0200

    point to new design ref and fix width and height

--- application/lib/presentation/widget/illustration_container.dart
+++ application/lib/presentation/widget/illustration_container.dart
@@ -5,25 +5,31 @@ class IllustrationContainer extends StatelessWidget {
   final Widget child;
   final String assetName;
   final VoidCallback? onFinished;
+  final double? width;
+  final double? height;
 
   const IllustrationContainer.asset(
     this.assetName, {
     Key? key,
     required this.child,
     this.onFinished,
+    this.width,
+    this.height,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final illustration = Illustration.asset(
       assetName,
+      width: width,
+      height: height,
       onFinished: onFinished,
     );
 
     return Column(
       children: [
-        illustration,
-        Expanded(child: child),
+        Expanded(child: illustration),
+        child,
       ],
     );
   }
