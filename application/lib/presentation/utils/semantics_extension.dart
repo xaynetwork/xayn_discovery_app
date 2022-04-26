f06ec5e811b2fd491ff9354ea0e08acc34889f76
vadim.popov <vadim.popov@xayn.com>
2022-04-26 14:45:01 +0200

    TB-3641: excluded semantics in SemanticsExtension

--- application/lib/presentation/utils/semantics_extension.dart
+++ application/lib/presentation/utils/semantics_extension.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 extension SemanticsExtension on Widget {
   Widget withSemanticsLabel(
     String semanticsLabel, {
-    bool excludeChildren = true,
+    bool excludeChildren = false,
   }) =>
       Semantics(
         label: semanticsLabel,
