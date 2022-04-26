0349cf8d6e535d2859da04ce1bc396fddec4b84a
vadim.popov <vadim.popov@xayn.com>
2022-04-26 20:30:49 +0200

    TB-3641: debugging semantics

--- application/lib/presentation/utils/semantics_extension.dart
+++ application/lib/presentation/utils/semantics_extension.dart
@@ -3,12 +3,11 @@ import 'package:flutter/widgets.dart';
 extension SemanticsExtension on Widget {
   Widget withSemanticsLabel(
     String semanticsLabel, {
-    bool excludeChildren = false,
+    bool excludeChildren = true,
   }) =>
       Semantics(
         label: semanticsLabel,
         enabled: true,
-        button: true,
         child: excludeChildren ? ExcludeSemantics(child: this) : this,
       );
 }
