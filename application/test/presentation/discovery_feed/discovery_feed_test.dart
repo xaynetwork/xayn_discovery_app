31ade74d7ee9cd1660668735d2d7fc8230fd6a07
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-04-26 16:37:43 +0200

    Fix tests

--- application/test/presentation/discovery_feed/discovery_feed_test.dart
+++ application/test/presentation/discovery_feed/discovery_feed_test.dart
@@ -163,7 +163,7 @@ void main() async {
         isFullScreen: false,
         isInErrorState: false,
         didReachEnd: false,
-        subscriptionStatus: subscriptionStatusInitial,
+        subscriptionStatus: null,
       ),
     ],
     verify: (manager) {
@@ -277,7 +277,7 @@ void main() async {
           isInErrorState: false,
           shouldUpdateNavBar: false,
           didReachEnd: false,
-          subscriptionStatus: subscriptionStatusInitial,
+          subscriptionStatus: null,
           readerModeBackgroundColor: ReaderModeBackgroundColor(
             dark: ReaderModeBackgroundDarkColor.dark,
             light: ReaderModeBackgroundLightColor.white,
