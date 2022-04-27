f494c52433d4593bc2bf949e89d9054cdb097ffb
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-04-27 15:41:09 +0200

    add more illustrations

--- application/lib/presentation/widget/animation_player_container.dart
+++ application/lib/presentation/widget/animation_player_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/widgets.dart';
 import 'package:xayn_discovery_app/presentation/widget/animation_player.dart';
 
+const double _kWidth = 200.0;
+const double _kHeight = 200.0;
+
 class AnimationPlayerContainer extends StatelessWidget {
   final Widget child;
   final String assetName;
   final VoidCallback? onFinished;
-  final double? width;
-  final double? height;
+  final double width;
+  final double height;
 
   const AnimationPlayerContainer.asset(
     this.assetName, {
     Key? key,
     required this.child,
     this.onFinished,
-    this.width,
-    this.height,
+    this.width = _kWidth,
+    this.height = _kHeight,
   }) : super(key: key);
 
   @override
