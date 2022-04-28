a52cc3dcc3a2a96006a4fc49baf01c492246e789
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-04-28 17:53:04 +0200

    use pause/resume subscriptions solution

--- application/lib/presentation/discovery_engine/mixin/close_feed_documents_mixin.dart
+++ application/lib/presentation/discovery_engine/mixin/close_feed_documents_mixin.dart
@@ -6,10 +6,11 @@ import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/close_feed_documents_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/crud_explicit_document_feedback_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/crud/db_entity_crud_use_case.dart';
+import 'package:xayn_discovery_app/presentation/discovery_engine/mixin/singleton_subscription_observer.dart';
 import 'package:xayn_discovery_app/presentation/discovery_engine/mixin/util/use_case_sink_extensions.dart';
 import 'package:xayn_discovery_engine/discovery_engine.dart';
 
-mixin CloseFeedDocumentsMixin<T> on UseCaseBlocHelper<T> {
+mixin CloseFeedDocumentsMixin<T> on SingletonSubscriptionObserver<T> {
   final Set<DocumentId> _closedDocuments = <DocumentId>{};
   UseCaseSink<Set<DocumentId>, EngineEvent>? _useCaseSink;
 
--- application/lib/presentation/discovery_engine/mixin/singleton_subscription_observer.dart
+++ application/lib/presentation/discovery_engine/mixin/singleton_subscription_observer.dart
@@ -1,22 +1,17 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 
 mixin SingletonSubscriptionObserver<T> on UseCaseBlocHelper<T> {
-  final PublishSubject<bool> _onSubscriptionActivity = PublishSubject<bool>();
-
-  Stream<bool>? _onHasActiveListeners;
-  Stream<bool> get onHasActiveListeners => _onHasActiveListeners ??=
-      _onSubscriptionActivity.distinct().where((it) => it);
-
-  Stream<bool>? _onHasNoActiveListeners;
-  Stream<bool> get onHasNoActiveListeners => _onHasNoActiveListeners ??=
-      _onSubscriptionActivity.distinct().where((it) => !it);
-
   @override
-  Stream<T> get stream => super
-      .stream
-      .doOnListen(() => _onSubscriptionActivity.add(true))
-      .doOnCancel(() => _onSubscriptionActivity.add(false));
+  Stream<T> get stream =>
+      super.stream.doOnListen(onListen).doOnCancel(onCancel);
+
+  @mustCallSuper
+  void onListen() => resumeAllSubscriptions();
+
+  @mustCallSuper
+  void onCancel() => pauseAllSubscriptions();
 }
