3bd7f631cbdb8103126167fb9f27a5b2d9cd9970
Carmine Toriello <carmine.toriello@xayn.com>
2022-05-05 15:51:59 +0200

    TB-3670 Parse engine event error messages
    When the engine returns a failing event, parse the content
    of it and show the error message contained in it by using
    a bottom sheet

---
+++ application/lib/presentation/base_discovery/utils/engine_error_messages.dart
@@ -0,0 +1,30 @@
+import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
+
+/// Class containing the error messages that can be found in
+/// an engine event when an error occurs
+class EngineErrorMessages {
+  EngineErrorMessages._();
+
+  static const String timeoutError = 'TimedOut';
+  static const String dnsError = 'dns error';
+
+  /// Use the following when there is no match with anyone else error message
+  static const String unknownError = 'Unknown';
+}
+
+mixin EngineErrorMessagesMixin {
+  /// Checks which error message the engine event contains
+  /// and returns it
+  String getEngineEventErrorMessage(EngineEvent event) {
+    final eventString = event.toString().toLowerCase();
+    final timeoutError = EngineErrorMessages.timeoutError.toLowerCase();
+    final dnsError = EngineErrorMessages.dnsError.toLowerCase();
+    if (eventString.contains(timeoutError)) {
+      return EngineErrorMessages.timeoutError;
+    }
+    if (eventString.contains(dnsError)) {
+      return EngineErrorMessages.dnsError;
+    }
+    return EngineErrorMessages.unknownError;
+  }
+}
