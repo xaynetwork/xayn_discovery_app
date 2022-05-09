08e900b6175a4c082079fde98d6b253882bb4d3f
michaelkmalak <michael.k.malak@gmail.com>
2022-05-09 13:27:02 +0200

    refactor bottom sheets in payment screens

--- application/lib/presentation/payment/payment_screen.dart
+++ application/lib/presentation/payment/payment_screen.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:xayn_design/xayn_design.dart';
-import 'package:xayn_discovery_app/domain/model/payment/payment_flow_error.dart';
 import 'package:xayn_discovery_app/domain/model/payment/purchasable_product.dart';
 import 'package:xayn_discovery_app/infrastructure/di/di_config.dart';
-import 'package:xayn_discovery_app/presentation/bottom_sheet/error/generic_error_bottom_sheet.dart';
-import 'package:xayn_discovery_app/presentation/bottom_sheet/error/no_active_subscription_found_error_bottom_sheet.dart';
-import 'package:xayn_discovery_app/presentation/bottom_sheet/error/payment_failed_error_bottom_sheet.dart';
 import 'package:xayn_discovery_app/presentation/constants/r.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/widget/overlay_manager.dart';
 import 'package:xayn_discovery_app/presentation/discovery_card/widget/overlay_mixin.dart';
 import 'package:xayn_discovery_app/presentation/payment/manager/payment_screen_manager.dart';
 import 'package:xayn_discovery_app/presentation/payment/manager/payment_screen_state.dart';
 import 'package:xayn_discovery_app/presentation/premium/widgets/trial_expired.dart';
-import 'package:xayn_discovery_app/presentation/utils/error_code_extensions.dart';
 
 class PaymentScreen extends StatefulWidget {
   const PaymentScreen({Key? key}) : super(key: key);
@@ -44,11 +38,10 @@ class _PaymentScreenState extends State<PaymentScreen>
     required PaymentScreenState state,
     required BuildContext context,
   }) =>
-      state.whenOrNull(ready: (product, error) {
+      state.whenOrNull(ready: (product) {
         if (product.status.isPurchased || product.status.isRestored) {
           manager.onDismiss();
         }
-        if (error != null) _handleError(context, error);
         return null;
       });
 
@@ -84,30 +77,6 @@ class _PaymentScreenState extends State<PaymentScreen>
     );
     return Scaffold(
       body: SingleChildScrollView(child: content),
-    );
-  }
-
-  void _handleError(BuildContext context, PaymentFlowError error) {
-    if (error == PaymentFlowError.itemAlreadyOwned) {
-      manager.onDismiss();
-      return;
-    }
-
-    late BottomSheetBase body;
-    if (error == PaymentFlowError.paymentFailed) {
-      body = PaymentFailedErrorBottomSheet();
-    } else if (error == PaymentFlowError.noActiveSubscriptionFound) {
-      body = NoActiveSubscriptionFoundErrorBottomSheet();
-    } else {
-      body = GenericErrorBottomSheet(
-        errorCode: error.errorCode,
-      );
-    }
-
-    showAppBottomSheet(
-      context,
-      builder: (_) => body,
-      allowStacking: true,
     );
   }
 }
