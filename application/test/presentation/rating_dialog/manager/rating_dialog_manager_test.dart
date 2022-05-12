6b2583baa3d2c0dc1ac6e13cbe2f935cd89b0ea3
Simon Joecks <simon.joecks@gmail.com>
2022-05-12 10:40:44 +0700

    Added rating dialog manager tests

--- application/test/presentation/rating_dialog/manager/rating_dialog_manager_test.dart
+++ application/test/presentation/rating_dialog/manager/rating_dialog_manager_test.dart
@@ -36,11 +36,66 @@ void main() {
   });
 
   test(
-      "After the third session we will show the dialog when clicking on bookmarked",
+      "After the third session we will show the RatingDialog when completing bookmark flow",
       () async {
     _createManager(numberOfSessions: 3);
 
     expect(await manager.completedBookmarking(), true);
     verify(appReview.requestReview());
+  });
+
+  test(
+      "After the third session we will show the RatingDialog completing share to a friend",
+      () async {
+    _createManager(numberOfSessions: 3);
+
+    expect(await manager.shareWithFriendsCompleted(), true);
+    verify(appReview.requestReview());
+  });
+
+  test(
+      "After the third session we will show the RatingDialog completing share a Document",
+      () async {
+    _createManager(numberOfSessions: 3);
+
+    expect(await manager.shareWithFriendsCompleted(), true);
+    verify(appReview.requestReview());
+  });
+
+  test(
+      "After the any number larger than 3 session we will show the RatingDialog completing any action",
+      () async {
+    _createManager(numberOfSessions: 5);
+
+    expect(await manager.shareWithFriendsCompleted(), true);
+    verify(appReview.requestReview());
+  });
+
+  test(
+      "After the any number below than 3 sessions we will NOT show the RatingDialog",
+      () async {
+    _createManager(numberOfSessions: 2);
+
+    expect(await manager.completedBookmarking(), false);
+    verifyNever(appReview.requestReview());
+  });
+
+  test("After showing the dialog once, we never show it again.", () async {
+    _createManager(numberOfSessions: 3);
+    expect(await manager.completedBookmarking(), true);
+    verify(appReview.requestReview());
+    reset(appReview);
+
+    expect(await manager.completedBookmarking(), false);
+    verifyNever(appReview.requestReview());
+    expect(appStatusRepository.appStatus.ratingDialogAlreadyVisible, true);
+  });
+
+  test("When the rating dialog was visible before, never show it again.",
+      () async {
+    _createManager(numberOfSessions: 3, ratingAlreadyVisible: true);
+
+    expect(await manager.shareWithFriendsCompleted(), false);
+    verifyNever(appReview.requestReview());
   });
 }
