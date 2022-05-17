55503401e4af59747ebfcc9399937b1cb66749ae
Andrii Antonov <pro100svitlo@gmail.com>
2022-05-17 15:31:11 +0200

    Make feature.dart enum be enhanced

--- application/lib/domain/model/feature.dart
+++ application/lib/domain/model/feature.dart
@@ -1,10 +1,22 @@
+import 'package:xayn_discovery_app/presentation/utils/environment_helper.dart';
+
 enum Feature {
-  featuresScreen,
-  payment,
-  readerModeSettings,
-  discoveryEngineReportOverlay,
-  ratingDialog,
-  tts,
+  featuresScreen('Michael',
+      EnvironmentHelper.kIsDebug || EnvironmentHelper.kIsInternalFlavor),
+  payment('Peter', EnvironmentHelper.kAppId == 'com.xayn.discovery'),
+  readerModeSettings('Michael', true),
+  discoveryEngineReportOverlay('Simon', false),
+  ratingDialog('Simon',
+      EnvironmentHelper.kIsDebug || EnvironmentHelper.kIsInternalFlavor),
+  tts('Frank', false);
+
+  final String author;
+  final bool defaultValue;
+
+  const Feature(
+    this.author,
+    this.defaultValue,
+  );
 }
 
 typedef FeatureMap = Map<Feature, bool>;
