e0478bc08419e50022c2396dbfac0bab0bb11031
Andrii Antonov <pro100svitlo@gmail.com>
2022-05-19 16:19:49 +0200

    Add feature owner enum :)

--- application/lib/domain/model/feature.dart
+++ application/lib/domain/model/feature.dart
@@ -1,24 +1,33 @@
 import 'package:xayn_discovery_app/presentation/utils/environment_helper.dart';
 
 enum Feature {
-  featuresScreen('Michael',
+  featuresScreen(Owner.Michael,
       EnvironmentHelper.kIsDebug || EnvironmentHelper.kIsInternalFlavor),
-  payment('Peter', EnvironmentHelper.kAppId == 'com.xayn.discovery'),
-  readerModeSettings('Michael', true),
-  discoveryEngineReportOverlay('Simon', false),
-  ratingDialog('Simon',
+  payment(Owner.Peter, EnvironmentHelper.kAppId == 'com.xayn.discovery'),
+  readerModeSettings(Owner.Michael, true),
+  discoveryEngineReportOverlay(Owner.Simon, false),
+  ratingDialog(Owner.Simon,
       EnvironmentHelper.kIsDebug || EnvironmentHelper.kIsInternalFlavor),
-  tts('Frank', false, 'Enables text-to-speech function for articles');
+  tts(Owner.Frank, false, 'Enables text-to-speech function for articles');
 
-  final String owner;
+  final Owner owner;
   final String? description;
   final bool defaultValue;
 
   const Feature(
     this.owner,
-    this.defaultValue,
-    [this.description,]
-  );
+    this.defaultValue, [
+    this.description,
+  ]);
 }
 
 typedef FeatureMap = Map<Feature, bool>;
+
+// ignore_for_file: constant_identifier_names
+enum Owner {
+  Carmine,
+  Frank,
+  Michael,
+  Peter,
+  Simon,
+}
