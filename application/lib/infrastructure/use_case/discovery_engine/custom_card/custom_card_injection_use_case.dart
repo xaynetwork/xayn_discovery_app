077506a881f81b7d451158c9b2eb4e250b3c0c9d
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-05-20 11:00:58 +0200

    add docs for upcoming survey logic

--- application/lib/infrastructure/use_case/discovery_engine/custom_card/custom_card_injection_use_case.dart
+++ application/lib/infrastructure/use_case/discovery_engine/custom_card/custom_card_injection_use_case.dart
@@ -7,13 +7,14 @@ import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
 class CustomCardInjectionUseCase extends UseCase<Set<Document>, Set<Card>> {
   @override
   Stream<Set<Card>> transaction(Set<Document> param) async* {
+    // todo This use case should act upon the logic, which triggers whenever
+    // we should show the survey.
+    // When the trigger occurs, simply follow the code below:
+    const shouldShowSurvey = 1 == 2; // make this dynamic!
     final transformed = param.map(Card.document).toSet();
 
-    if (transformed.length >= 2) {
-      final customCardInjectedList = transformed.toList()
-        ..insert(2, const Card.other(CardType.survey));
-
-      yield customCardInjectedList.toSet();
+    if (shouldShowSurvey) {
+      yield {const Card.other(CardType.survey), ...transformed};
     } else {
       yield transformed;
     }
