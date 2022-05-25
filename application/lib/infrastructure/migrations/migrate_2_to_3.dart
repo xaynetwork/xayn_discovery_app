b0cd88b34b7f0c9a355f14ad509c7b1cf156193d
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-05-25 11:46:33 +0200

    PR comments

--- application/lib/infrastructure/migrations/migrate_2_to_3.dart
+++ application/lib/infrastructure/migrations/migrate_2_to_3.dart
@@ -1,10 +1,7 @@
-import 'package:hive/hive.dart';
-import 'package:hive_crdt/hive_crdt.dart';
 import 'package:xayn_discovery_app/domain/model/app_status.dart';
 import 'package:xayn_discovery_app/infrastructure/mappers/app_status_mapper.dart';
 import 'package:xayn_discovery_app/infrastructure/migrations/base_migration.dart';
 import 'package:xayn_discovery_app/infrastructure/migrations/single_value_migration_repository.dart';
-import 'package:xayn_discovery_app/infrastructure/util/hive_db.dart';
 
 /// This Migration sets [isBetaUser] to true for users that used the app before.
 // ignore: camel_case_types
@@ -21,16 +18,16 @@ class Migration_2_To_3 extends BaseDbMigration {
   Future<int> runMigration(int fromVersion) async {
     assert(fromVersion == 2);
 
-    // Check if the user has any feed data stored in the database.
-    // If so, we assume it was a beta user.
-    final recordBox = HiveCrdt(Hive.box<Record>('feed'), HiveDB.nodeId);
-    final isBetaUser = !recordBox.isEmpty;
     final repository = SingleValueMigrationRepository(
       box:
           'appStatus', // important hardcoded string to avoid accidental refactors
       key: AppStatus.globalId,
       transform: (map) {
-        map[AppStatusFields.isBetaUser] = isBetaUser;
+        // Check if the user has any previous sessions.
+        // If so, we assume it was a beta user.
+        final numberOfSessions =
+            map[AppStatusFields.numberOfSessions] as int? ?? 0;
+        map[AppStatusFields.isBetaUser] = numberOfSessions > 0;
         return map;
       },
     );
