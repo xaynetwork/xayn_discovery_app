8a4300153fa35c89a6de36c8978bdcae364144cc
Carmine Toriello <carmine.toriello@xayn.com>
2022-06-02 12:40:49 +0200

    Renamed use case for handling survey banner shown scenario

--- application/lib/infrastructure/use_case/survey_banner/handle_survey_banner_shown_use_case.dart
+++ application/lib/infrastructure/use_case/survey_banner/handle_survey_banner_shown_use_case.dart
@@ -3,23 +3,28 @@ import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_app/domain/repository/app_status_repository.dart';
 
 @injectable
-class IncrementSurveyShownUseCase extends UseCase<None, None> {
+class HandleSurveyBannerShownUseCase extends UseCase<None, None> {
   final AppStatusRepository _repository;
 
-  IncrementSurveyShownUseCase(this._repository);
+  HandleSurveyBannerShownUseCase(this._repository);
 
   @override
   Stream<None> transaction(param) async* {
+    /// Get the current status of the data
     final appStatus = _repository.appStatus;
     final cta = appStatus.cta;
     final surveyBannerData = appStatus.cta.surveyBannerData;
+
+    /// Update the status of the data
     final updatedSurveyBannerData = surveyBannerData.copyWith(
         numberOfTimesShown: surveyBannerData.numberOfTimesShown + 1);
     final updatedCta = cta.copyWith(surveyBannerData: updatedSurveyBannerData);
     final updatedAppStatus = appStatus.copyWith(
       cta: updatedCta,
     );
+
     _repository.save(updatedAppStatus);
+
     yield none;
   }
 }
