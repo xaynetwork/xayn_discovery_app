0befa84aba3e1be9bfcb14cbaef3895f220e28cb
Carmine Toriello <carmine.toriello@xayn.com>
2022-06-02 12:40:32 +0200

    Implemented use case for handling survey banner clicked scenario
    Updated SurveyBannerData object

---
+++ application/lib/infrastructure/use_case/survey_banner/handle_survey_banner_clicked_use_case.dart
@@ -0,0 +1,26 @@
+import 'package:injectable/injectable.dart';
+import 'package:xayn_architecture/concepts/use_case/none.dart';
+import 'package:xayn_architecture/concepts/use_case/use_case_base.dart';
+import 'package:xayn_discovery_app/domain/repository/app_status_repository.dart';
+
+@injectable
+class HandleSurveyBannerClickedUseCase extends UseCase<None, None> {
+  final AppStatusRepository appStatusRepository;
+
+  HandleSurveyBannerClickedUseCase(this.appStatusRepository);
+  @override
+  Stream<None> transaction(None param) async* {
+    /// Get the current status of the data
+    final appStatus = appStatusRepository.appStatus;
+    final cta = appStatus.cta;
+    final surveyBannerData = cta.surveyBannerData;
+
+    /// Update the status of the data
+    final updatedCta =
+        cta.copyWith(surveyBannerData: surveyBannerData.clicked());
+    final updatedAppStatus = appStatus.copyWith(cta: updatedCta);
+    appStatusRepository.save(updatedAppStatus);
+
+    yield none;
+  }
+}
