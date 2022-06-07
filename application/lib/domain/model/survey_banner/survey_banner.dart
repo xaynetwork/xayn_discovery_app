db84e47ba08b1bfd5e92e43539d2c80a59ef2709
Carmine Toriello <carmine.toriello@xayn.com>
2022-06-07 11:01:01 +0200

    TB-3849 Introduced new paramter in SurveyBanner object

--- application/lib/domain/model/survey_banner/survey_banner.dart
+++ application/lib/domain/model/survey_banner/survey_banner.dart
@@ -5,34 +5,42 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 class SurveyBanner extends Equatable {
   final int numberOfTimesShown;
   final bool hasSurveyBannerBeenClicked;
+  final int lastSessionNumberWhenShown;
 
   const SurveyBanner({
     required this.numberOfTimesShown,
     required this.hasSurveyBannerBeenClicked,
+    required this.lastSessionNumberWhenShown,
   });
 
   const SurveyBanner.initial()
       : numberOfTimesShown = 0,
-        hasSurveyBannerBeenClicked = false;
+        hasSurveyBannerBeenClicked = false,
+        lastSessionNumberWhenShown = 0;
 
   SurveyBanner copyWith({
     int? numberOfTimesShown,
     bool? hasSurveyBannerBeenClicked,
+    int? lastSessionNumberWhenShown,
   }) =>
       SurveyBanner(
         numberOfTimesShown: numberOfTimesShown ?? this.numberOfTimesShown,
         hasSurveyBannerBeenClicked:
             hasSurveyBannerBeenClicked ?? this.hasSurveyBannerBeenClicked,
+        lastSessionNumberWhenShown:
+            lastSessionNumberWhenShown ?? this.lastSessionNumberWhenShown,
       );
 
-  SurveyBanner clicked() => SurveyBanner(
+  SurveyBanner clicked({required int sessionNumber}) => SurveyBanner(
         numberOfTimesShown: numberOfTimesShown,
         hasSurveyBannerBeenClicked: true,
+        lastSessionNumberWhenShown: sessionNumber,
       );
 
   @override
   List<Object?> get props => [
         numberOfTimesShown,
         hasSurveyBannerBeenClicked,
+        lastSessionNumberWhenShown,
       ];
 }
