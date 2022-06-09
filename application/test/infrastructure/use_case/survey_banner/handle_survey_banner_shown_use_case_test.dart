cb8ff82709a6b4fd406ec76fc525579a992a9535
Carmine Toriello <carmine.toriello@xayn.com>
2022-06-09 10:18:22 +0200

    Implemented tests for survey banner use cases

---
+++ application/test/infrastructure/use_case/survey_banner/handle_survey_banner_shown_use_case_test.dart
@@ -0,0 +1,47 @@
+import 'package:mockito/mockito.dart';
+import 'package:xayn_architecture/concepts/use_case/none.dart';
+import 'package:xayn_architecture/xayn_architecture_test.dart';
+import 'package:xayn_discovery_app/domain/model/app_status.dart';
+import 'package:xayn_discovery_app/domain/model/cta/cta.dart';
+import 'package:xayn_discovery_app/domain/model/survey_banner/survey_banner.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/survey_banner/handle_survey_banner_shown_use_case.dart';
+
+import '../../../test_utils/utils.dart';
+
+void main() {
+  late MockAppStatusRepository appStatusRepository;
+  late HandleSurveyBannerShownUseCase handleSurveyBannerShownUseCase;
+
+  appStatusRepository = MockAppStatusRepository();
+  handleSurveyBannerShownUseCase =
+      HandleSurveyBannerShownUseCase(appStatusRepository);
+
+  final initialAppStatus = AppStatus.initial();
+
+  const initialSurveyBanner = SurveyBanner.initial();
+
+  when(appStatusRepository.appStatus).thenReturn(initialAppStatus);
+
+  late AppStatus updatedAppStatus;
+
+  useCaseTest(
+    'WHEN called THEN update the survey banner object into the db with increased number of time shown',
+    setUp: () {
+      final updatedSurveyBanner = initialSurveyBanner.copyWith(
+          numberOfTimesShown: initialSurveyBanner.numberOfTimesShown + 1);
+
+      updatedAppStatus = initialAppStatus.copyWith(
+          cta: CTA(
+        surveyBanner: updatedSurveyBanner,
+      ));
+    },
+    build: () => handleSurveyBannerShownUseCase,
+    input: [none],
+    verify: (_) {
+      verify(appStatusRepository.save(updatedAppStatus)).called(1);
+    },
+    expect: [
+      useCaseSuccess(none),
+    ],
+  );
+}
