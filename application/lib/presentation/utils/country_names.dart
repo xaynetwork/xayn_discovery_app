c04d9dd945fa69199e136c38d4e218e048821761
Simon Joecks <simon.joecks@gmail.com>
2022-06-09 10:25:51 +0700

    Simplify the use of displayable language, so we don't require that many translations

--- application/lib/presentation/utils/country_names.dart
+++ application/lib/presentation/utils/country_names.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_localized_countries/flutter_localized_countries.dart';
-import 'package:xayn_discovery_app/infrastructure/util/discovery_engine_markets.dart';
 import 'package:xayn_discovery_app/presentation/constants/app_language.dart';
-import 'package:xayn_discovery_app/presentation/utils/app_locale.dart';
 
 /// Key is country code, value is localized country name.
 typedef CountryNames = Map<String, String>;
@@ -14,26 +12,4 @@ Future<CountryNames> getCountryNames(AppLanguage appLanguage) async {
   final locale = appLanguage.flutterLocale;
   final names = await const CountryNamesLocalizationsDelegate().load(locale);
   return names.data;
-}
-
-extension CountryNameExtension on CountryNames {
-  String countryNameForLocale(
-    AppLocale locale,
-  ) {
-    final showLanguageCode = isCountryMultilingual(locale.countryCode);
-    final countryCode = locale.countryCode;
-    String resolvedCountryName;
-
-    if (countryCode == null) {
-      // fallback value
-      resolvedCountryName = locale.toLanguageTag();
-    } else {
-      final maybeCountryName = this[countryCode];
-      resolvedCountryName = maybeCountryName ?? locale.toLanguageTag();
-    }
-
-    return showLanguageCode
-        ? '$resolvedCountryName ( ${locale.languageCode} )'
-        : resolvedCountryName;
-  }
 }
