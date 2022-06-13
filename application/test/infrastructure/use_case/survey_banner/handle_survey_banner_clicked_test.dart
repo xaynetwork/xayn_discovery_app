53f113f1d73185dc9ded625037c9c012d3c5f3cf
Carmine Toriello <carmine.toriello@xayn.com>
2022-06-13 17:53:01 +0200

    TB-3881 Added link to survey banner

--- application/test/infrastructure/use_case/survey_banner/handle_survey_banner_clicked_test.dart
+++ application/test/infrastructure/use_case/survey_banner/handle_survey_banner_clicked_test.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:xayn_architecture/concepts/use_case/none.dart';
 import 'package:xayn_architecture/concepts/use_case/test/use_case_test.dart';
 import 'package:xayn_discovery_app/domain/model/app_status.dart';
 import 'package:xayn_discovery_app/domain/model/cta/cta.dart';
+import 'package:xayn_discovery_app/domain/model/payment/subscription_status.dart';
 import 'package:xayn_discovery_app/domain/model/survey_banner/survey_banner.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/survey_banner/handle_survey_banner_clicked_use_case.dart';
 
@@ -10,38 +12,78 @@ import '../../../test_utils/utils.dart';
 
 void main() {
   late MockAppStatusRepository appStatusRepository;
+  late MockGetSubscriptionStatusUseCase getSubscriptionStatusUseCase;
+  late MockUrlOpener urlOpener;
   late HandleSurveyBannerClickedUseCase handleSurveyBannerClickedUseCase;
 
   appStatusRepository = MockAppStatusRepository();
-  handleSurveyBannerClickedUseCase =
-      HandleSurveyBannerClickedUseCase(appStatusRepository);
+  getSubscriptionStatusUseCase = MockGetSubscriptionStatusUseCase();
+  urlOpener = MockUrlOpener();
+  handleSurveyBannerClickedUseCase = HandleSurveyBannerClickedUseCase(
+    appStatusRepository,
+    urlOpener,
+    getSubscriptionStatusUseCase,
+  );
 
   final initialAppStatus = AppStatus.initial();
 
   const initialSurveyBanner = SurveyBanner.initial();
 
+  final clickedSurveyBanner =
+      initialSurveyBanner.copyWith(hasSurveyBannerBeenClicked: true);
+
+  final appStatusSurveyBannerClicked = initialAppStatus.copyWith(
+    cta: CTA(
+      surveyBanner: clickedSurveyBanner,
+    ),
+  );
+
   when(appStatusRepository.appStatus).thenReturn(initialAppStatus);
 
-  late AppStatus updatedAppStatus;
-
-  useCaseTest(
-    'WHEN called THEN update the survey banner object into the db with clicked flag set to true',
-    setUp: () {
-      final updatedSurveyBanner =
-          initialSurveyBanner.copyWith(hasSurveyBannerBeenClicked: true);
-
-      updatedAppStatus = initialAppStatus.copyWith(
-          cta: CTA(
-        surveyBanner: updatedSurveyBanner,
-      ));
+  group(
+    'WHEN user is subscribed',
+    () {
+      useCaseTest(
+        'WHEN use case is called THEN update the survey banner object into the db with clicked flag set to true and open the survey url for subscribed user',
+        setUp: () {
+          when(getSubscriptionStatusUseCase.singleOutput(none)).thenAnswer(
+              (_) async =>
+                  SubscriptionStatus.initial().copyWith(isBetaUser: true));
+        },
+        build: () => handleSurveyBannerClickedUseCase,
+        input: [none],
+        verify: (_) {
+          verify(appStatusRepository.save(appStatusSurveyBannerClicked))
+              .called(1);
+          verify(urlOpener.openUrl(subscribedUserSurveyUrl)).called(1);
+        },
+        expect: [
+          useCaseSuccess(none),
+        ],
+      );
     },
-    build: () => handleSurveyBannerClickedUseCase,
-    input: [none],
-    verify: (_) {
-      verify(appStatusRepository.save(updatedAppStatus)).called(1);
+  );
+
+  group(
+    'WHEN user is NOT subscribed',
+    () {
+      useCaseTest(
+        'WHEN use case is called THEN update the survey banner object into the db with clicked flag set to true and open the survey url for not subscribed user',
+        setUp: () {
+          when(getSubscriptionStatusUseCase.singleOutput(none))
+              .thenAnswer((_) async => SubscriptionStatus.initial());
+        },
+        build: () => handleSurveyBannerClickedUseCase,
+        input: [none],
+        verify: (_) {
+          verify(appStatusRepository.save(appStatusSurveyBannerClicked))
+              .called(1);
+          verify(urlOpener.openUrl(notSubscribedUserSurveyUrl)).called(1);
+        },
+        expect: [
+          useCaseSuccess(none),
+        ],
+      );
     },
-    expect: [
-      useCaseSuccess(none),
-    ],
   );
 }
