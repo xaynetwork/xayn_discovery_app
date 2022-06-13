6fa5398371ba09b858ee77010d561a34e18d3e30
Vadim <49361237+vadpopov@users.noreply.github.com>
2022-06-13 16:10:22 +0200

    Tb 3862 add semantics (#519)
    * + adding selected semantics to like button
    
    * TB-3862
    
    + added flag to semantics label
    - removed selected label
    
    * TB-3862: _add_semantics_
    
    + added semantics flags for nav_bar_items.dart
    
    * TB-3862: _add_semantics_
    
    * TB-3862: _add_semantics_

--- application/lib/presentation/navigation/widget/nav_bar_items.dart
+++ application/lib/presentation/navigation/widget/nav_bar_items.dart
@@ -34,7 +34,7 @@ NavBarItemIconButton buildNavBarItemLike({
       isHighlighted: false,
       onPressed: onPressed,
       key: Keys.navBarItemLike,
-      semanticsLabel: Keys.navBarItemLike.valueKey,
+      semanticsLabel: '${Keys.navBarItemLike.valueKey} = $isLiked',
     );
 
 NavBarItemIconButton buildNavBarItemDisLike({
@@ -48,7 +48,7 @@ NavBarItemIconButton buildNavBarItemDisLike({
       isHighlighted: false,
       onPressed: onPressed,
       key: Keys.navBarItemDisLike,
-      semanticsLabel: Keys.navBarItemDisLike.valueKey,
+      semanticsLabel: '${Keys.navBarItemDisLike.valueKey} = $isDisLiked',
     );
 
 NavBarItemIconButton buildNavBarItemShare({
@@ -142,7 +142,7 @@ NavBarItemIconButton buildNavBarItemBookmark({
       onPressed: onPressed,
       onLongPressed: onLongPressed,
       key: Keys.navBarItemBookmark,
-      semanticsLabel: Keys.navBarItemBookmark.valueKey,
+      semanticsLabel: '${Keys.navBarItemBookmark.valueKey} = ${ bookmarkStatus == BookmarkStatus.bookmarked}',
     );
 
 const configIdSearch = NavBarConfigId('active_search');
