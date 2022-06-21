5542225f8cbdc1a5d896b475d7a1bc3ae3e84e04
DESKTOP-68EULOT\Igindo-Frank <frank@igindo.com>
2022-06-21 08:35:23 +0200

    fix test

--- application/test/presentation/feed_settings/page/source/manager/sources_manager_test.dart
+++ application/test/presentation/feed_settings/page/source/manager/sources_manager_test.dart
@@ -151,6 +151,7 @@ void main() {
           trustedSources: defaultTrustedSources,
           jointExcludedSources: defaultExcludedSources,
           jointTrustedSources: defaultTrustedSources,
+          sourcesSearchTerm: 'new',
         )),
   );
 
