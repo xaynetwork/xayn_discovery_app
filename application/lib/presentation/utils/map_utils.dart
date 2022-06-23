3209ab3b2eac316a83c98f0cc028f78325b36416
michaelkmalak <michael.k.malak@gmail.com>
2022-06-23 15:41:45 +0200

    TB-3957: flatten analytics json properties

--- application/lib/presentation/utils/map_utils.dart
+++ application/lib/presentation/utils/map_utils.dart
@@ -1,10 +1,14 @@
+import 'package:flat/flat.dart';
+
 extension MapExtension on Map<String, dynamic> {
   /// This ensures that unserializable objects like i.e. [UniqueId] can
   /// tracked in services like [AnalyticsService]
-  Map<String, dynamic> toSerializableMap() => map((key, value) {
-        final serializableValue = value is Map<String, dynamic>
-            ? value.toSerializableMap()
-            : value.toString();
-        return MapEntry(key, serializableValue);
-      });
+  Map<String, dynamic> toSerializableMap() => flatten(
+        map((key, value) {
+          final serializableValue = value is Map<String, dynamic>
+              ? value.toSerializableMap()
+              : value.toString();
+          return MapEntry(key, serializableValue);
+        }),
+      );
 }
