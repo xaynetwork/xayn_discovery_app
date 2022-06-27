ab4007692d90eb4b240a237a4548e10a0923b2b5
Carmine Toriello <carminetoriello@Carmines-MacBook-Pro.local>
2022-06-27 17:38:18 +0200

    TB-3958 Created discovery card header menu widget
    Updated ref to the xayn design repo where the
    app menu widget has been changed

---
+++ application/lib/presentation/discovery_card/widget/discovery_card_header_menu.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:xayn_design/xayn_design.dart';
+import 'package:xayn_discovery_app/presentation/constants/r.dart';
+
+class DiscoveryCardHeaderMenu extends StatelessWidget {
+  const DiscoveryCardHeaderMenu({
+    Key? key,
+    required this.items,
+    this.onClose,
+  }) : super(key: key);
+
+  final VoidCallback? onClose;
+  final List<DiscoveryCardHeaderMenuItem> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppMenu.single(
+      top: R.dimen.unit15,
+      start: R.dimen.unit4,
+      width: R.dimen.screenWidth - R.dimen.unit8,
+      borderRadius: R.styles.roundBorder3,
+      onClose: onClose,
+      child: _buildMenuBody(items),
+    );
+  }
+
+  Widget _buildMenuBody(List<DiscoveryCardHeaderMenuItem> items) =>
+      ListView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        padding: EdgeInsets.symmetric(
+            vertical: R.dimen.unit3, horizontal: R.dimen.unit3),
+        itemBuilder: (_, i) {
+          final menuItem = items.elementAt(i);
+          if (i == items.length - 1) {
+            return _buildRow(menuItem);
+          }
+          return _buildRowWithBottomPadding(
+            _buildRow(menuItem),
+          );
+        },
+        itemCount: items.length,
+      );
+
+  Widget _buildRow(DiscoveryCardHeaderMenuItem item) => InkWell(
+        onTap: item.onTap,
+        child: Padding(
+          padding: EdgeInsets.all(R.dimen.unit),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                item.iconPath,
+                width: R.dimen.iconSize,
+                color: R.colors.icon,
+              ),
+              SizedBox(
+                width: R.dimen.unit2,
+              ),
+              Flexible(child: Text(item.title, maxLines: 2)),
+            ],
+          ),
+        ),
+      );
+
+  Widget _buildRowWithBottomPadding(Widget child) => Padding(
+        padding: EdgeInsets.only(
+          bottom: R.dimen.unit,
+        ),
+        child: child,
+      );
+}
+
+class DiscoveryCardHeaderMenuItem {
+  final String iconPath;
+  final String title;
+  final VoidCallback onTap;
+
+  DiscoveryCardHeaderMenuItem({
+    required this.iconPath,
+    required this.title,
+    required this.onTap,
+  });
+
+  DiscoveryCardHeaderMenuItem copyWith({
+    String? iconPath,
+    String? title,
+    VoidCallback? onTap,
+  }) =>
+      DiscoveryCardHeaderMenuItem(
+        iconPath: iconPath ?? this.iconPath,
+        title: title ?? this.title,
+        onTap: onTap ?? this.onTap,
+      );
+}
