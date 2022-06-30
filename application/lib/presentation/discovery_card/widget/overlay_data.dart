f7105690cf11138464ce3c376dc65539deb1f61a
Carmine Toriello <carmine.toriello@xayn.com>
2022-06-30 14:45:10 +0200

    TB-3959 Created tooltips to show when menu item is pressed

--- application/lib/presentation/discovery_card/widget/overlay_data.dart
+++ application/lib/presentation/discovery_card/widget/overlay_data.dart
@@ -74,6 +74,22 @@ class OverlayData {
         ),
       );
 
+  static tooltipSourceExcluded({required VoidCallback onTap}) => _wrapTooltip(
+        design.TooltipData.customized(
+          key: 'sourceExcluded',
+          label: R.strings.sourceExcludedTooltipMessage,
+          highlightText: R.strings.manageSourcesTooltipMessage,
+          onTap: onTap,
+        ),
+      );
+
+  static tooltipSourceIncluded() => _wrapTooltip(
+        design.TooltipData.customized(
+          key: 'sourceIncluded',
+          label: R.strings.sourceAllowedBackTooltipMessage,
+        ),
+      );
+
   static tooltipInvalidSearch() => _wrapTooltip(design.TooltipData.customized(
         key: 'invalidSearch',
         label: R.strings.invalidSearch,
