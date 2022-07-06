37760a67ba492e524b9e65b0949245bcc595c19b
Carmine Toriello <carmine.toriello@xayn.com>
2022-07-06 23:26:48 +0200

    TB-4025 Created Reset AI bottom sheet UI

---
+++ application/lib/presentation/bottom_sheet/reset_ai/widget/reset_ai_bottom_sheet.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:xayn_design/xayn_design.dart';
+import 'package:xayn_discovery_app/presentation/bottom_sheet/model/bottom_sheet_footer/bottom_sheet_footer_button_data.dart';
+import 'package:xayn_discovery_app/presentation/bottom_sheet/model/bottom_sheet_footer/bottom_sheet_footer_data.dart';
+import 'package:xayn_discovery_app/presentation/bottom_sheet/widgets/bottom_sheet_footer.dart';
+import 'package:xayn_discovery_app/presentation/bottom_sheet/widgets/bottom_sheet_header.dart';
+import 'package:xayn_discovery_app/presentation/constants/r.dart';
+
+class ResetAIBottomSheet extends BottomSheetBase {
+  ResetAIBottomSheet({Key? key, VoidCallback? onSystemPop})
+      : super(
+          key: key,
+          body: _ResetAI(
+            onSystemPop: onSystemPop,
+          ),
+        );
+}
+
+class _ResetAI extends StatefulWidget {
+  const _ResetAI({
+    Key? key,
+    this.onSystemPop,
+  }) : super(
+          key: key,
+        );
+
+  final VoidCallback? onSystemPop;
+
+  @override
+  State<_ResetAI> createState() => __ResetAIState();
+}
+
+class __ResetAIState extends State<_ResetAI> with BottomSheetBodyMixin {
+  @override
+  Widget build(BuildContext context) {
+    final header = Padding(
+      padding: EdgeInsets.symmetric(vertical: R.dimen.unit),
+      child: BottomSheetHeader(
+        headerText: R.strings.bottomSheetResetAIHeader,
+      ),
+    );
+
+    final body = Text(R.strings.bottomSheetResetAIBody);
+
+    final footer = BottomSheetFooter(
+      onCancelPressed: () {
+        closeBottomSheet(context);
+        widget.onSystemPop?.call();
+      },
+      setup: _buildFooterSetup,
+    );
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        header,
+        body,
+        footer,
+      ],
+    );
+  }
+
+  BottomSheetFooterSetup get _buildFooterSetup => BottomSheetFooterSetup.column(
+        buttonsData: [
+          BottomSheetFooterButton(
+            text: R.strings.bottomSheetResetAIButton,
+            onPressed: () {
+              closeBottomSheet(context);
+            },
+          )
+        ],
+      );
+}
