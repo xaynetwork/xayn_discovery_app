09c91e148f0b055e75c4828969024c3dcf586688
michaelkmalak <michael.k.malak@gmail.com>
2022-07-07 08:41:46 +0200

    remove injectable dependency from RealTime

--- application/lib/presentation/utils/real_time.dart
+++ application/lib/presentation/utils/real_time.dart
@@ -1,8 +1,12 @@
-import 'package:injectable/injectable.dart';
 import 'package:ntp/ntp.dart';
 
-@singleton
 class RealTime {
+  static final RealTime _instance = RealTime._internal();
+
+  factory RealTime() => _instance;
+
+  RealTime._internal();
+
   DateTime now = DateTime.now();
 
   Future<void> updateTime() async => now = await NTP.now();
