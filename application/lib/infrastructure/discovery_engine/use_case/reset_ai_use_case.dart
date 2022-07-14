ce7fc6602eff414f3e7c5eab61a3aee7f656cccb
Carmine Toriello <carmine.toriello@xayn.com>
2022-07-14 09:54:49 +0200

    TB-4026 Addressed PR comments

--- application/lib/infrastructure/discovery_engine/use_case/reset_ai_use_case.dart
+++ application/lib/infrastructure/discovery_engine/use_case/reset_ai_use_case.dart
@@ -2,14 +2,22 @@ import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/xayn_architecture.dart';
 import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
 
+///Return true if succeeded, false otherwise
+///
 @injectable
-class ResetAIUseCase extends UseCase<None, EngineEvent> {
+class ResetAIUseCase extends UseCase<None, bool> {
   final DiscoveryEngine _engine;
 
   ResetAIUseCase(this._engine);
 
   @override
-  Stream<EngineEvent> transaction(None param) async* {
-    yield await _engine.resetAi();
+  Stream<bool> transaction(None param) async* {
+    final engineEvent = await _engine.resetAi();
+    if (engineEvent is ResetAiSucceeded) {
+      yield true;
+      return;
+    } else {
+      yield false;
+    }
   }
 }
--- application/lib/presentation/bottom_sheet/reset_ai/manager/resetting_ai_manager.dart
+++ application/lib/presentation/bottom_sheet/reset_ai/manager/resetting_ai_manager.dart
@@ -5,7 +5,6 @@ import 'package:xayn_discovery_app/infrastructure/discovery_engine/use_case/rese
 import 'package:xayn_discovery_app/presentation/bottom_sheet/reset_ai/manager/resetting_ai_state.dart';
 import 'package:xayn_discovery_app/presentation/discovery_feed/manager/discovery_feed_manager.dart';
 import 'package:xayn_discovery_app/presentation/utils/overlay/overlay_manager_mixin.dart';
-import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
 
 @injectable
 class ResettingAIManager extends Cubit<ResettingAIState>
@@ -20,27 +19,27 @@ class ResettingAIManager extends Cubit<ResettingAIState>
     this.discoveryFeedManager,
   ) : super(Loading());
 
-  late final _resetAIHandler = consume(resetAIUseCase, initialData: none);
+  late final UseCaseValueStream<bool> _resetAIHandler =
+      consume(resetAIUseCase, initialData: none);
 
   void resetAI() => resetAIUseCase.call(none);
 
   @override
   Future<ResettingAIState?> computeState() async =>
       fold(_resetAIHandler).foldAll(
-        (engineEvent, errorReport) {
+        (
+          resetAISucceeded,
+          errorReport,
+        ) {
           final error = errorReport.of(_resetAIHandler);
-          if (error != null) {
-            return ResetFailed();
-          }
+          if (error != null) return ResetFailed();
 
-          if (engineEvent is EngineExceptionRaised) {
-            return ResetFailed();
-          }
+          if (resetAISucceeded == null) return state;
 
-          if (engineEvent.toString() == 'EngineEvent.resetAiSucceeded()') {
-            discoveryFeedManager.onResetAISucceeded();
-            return ResetSucceeded();
-          }
+          if (!resetAISucceeded) return ResetFailed();
+
+          if (resetAISucceeded) return ResetSucceeded();
+
           return state;
         },
       );
