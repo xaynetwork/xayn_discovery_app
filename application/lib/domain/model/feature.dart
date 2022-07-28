f2086696a2e29689150941251bf6ec81cfdac3c7
Pawel Biernacki <pawel@robotygraficzne.com>
2022-07-28 08:58:15 +0200

    disabled features not needed for PoC

--- application/lib/domain/model/feature.dart
+++ application/lib/domain/model/feature.dart
@@ -3,19 +3,17 @@ import 'package:xayn_discovery_app/presentation/utils/environment_helper.dart';
 enum Feature {
   featuresScreen(Owner.Michael,
       EnvironmentHelper.kIsDebug || EnvironmentHelper.kIsInternalFlavor),
-  payment(
-      Owner.Peter, EnvironmentHelper.kAppId == EnvironmentHelper.kReleaseAppId),
+  payment(Owner.Peter, false),
   discoveryEngineReportOverlay(Owner.Simon, false),
-  ratingDialog(Owner.Simon,
-      EnvironmentHelper.kIsDebug || EnvironmentHelper.kIsInternalFlavor),
+  ratingDialog(Owner.Simon, false),
   tts(Owner.Frank, false, 'Enables text-to-speech function for articles'),
 
   /// Keep flag for remote config
-  promptSurvey(Owner.Carmine, true,
+  promptSurvey(Owner.Carmine, false,
       'When enabled, collects the user interactions in order to prompt the survey card'),
 
   /// Keep flag for remote config
-  altPromoCode(Owner.Simon, true, 'PromoCodes are handled inApp'),
+  altPromoCode(Owner.Simon, false, 'PromoCodes are handled inApp'),
 
   pushNotificationDeepLinks(
     Owner.Peter,
