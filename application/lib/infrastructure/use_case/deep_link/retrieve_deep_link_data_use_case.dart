f3d55238e42c12e87a4a5386b5c76d91aec56036
Carmine Toriello <carmine.toriello@xayn.com>
2022-09-08 17:37:57 +0200

    TB-4190 Implemented changes for handling the new article data

--- application/lib/infrastructure/use_case/deep_link/retrieve_deep_link_data_use_case.dart
+++ application/lib/infrastructure/use_case/deep_link/retrieve_deep_link_data_use_case.dart
@@ -1,37 +1,37 @@
 import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 import 'package:injectable/injectable.dart';
 import 'package:xayn_architecture/concepts/use_case/use_case_base.dart';
+import 'package:xayn_discovery_app/domain/model/article/article_data.dart';
 import 'package:xayn_discovery_app/infrastructure/service/analytics/constants/analytics_constants.dart';
-import 'package:xayn_discovery_app/infrastructure/use_case/document/decode_document_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/document/decode_article_use_case.dart';
 import 'package:xayn_discovery_app/presentation/navigation/deep_link_data.dart';
 import 'package:xayn_discovery_app/presentation/navigation/deep_link_manager.dart';
 import 'package:xayn_discovery_app/presentation/navigation/pages.dart';
-import 'package:xayn_discovery_engine_flutter/discovery_engine.dart';
 
 @injectable
 class RetrieveDeepLinkDataUseCase
     extends UseCase<DeepLinkResult, DeepLinkData> {
-  final DecodeDocumentUseCase _decodeDocumentUseCase;
+  final DecodeArticleUseCase _decodeArticleUseCase;
 
-  RetrieveDeepLinkDataUseCase(this._decodeDocumentUseCase);
+  RetrieveDeepLinkDataUseCase(this._decodeArticleUseCase);
 
   @override
   Stream<DeepLinkData> transaction(DeepLinkResult param) async* {
     final deepLinkName = param.deepLink?.deepLinkValue;
-    Document? document;
+    ArticleData? articleData;
 
     /// Check if the deep link is the one used for sharing a document
     if (deepLinkName == PageName.cardDetails.name) {
       /// If yes, retrieve the encoded document from the deepLink and decode it
       final encodedDocument = param.deepLink!
-          .getStringValue(AnalyticsConstants.documentLinkParamName);
+          .getStringValue(AnalyticsConstants.articleLinkParamName);
 
       if (encodedDocument != null) {
-        document = await _decodeDocumentUseCase.singleOutput(encodedDocument);
+        articleData = await _decodeArticleUseCase.singleOutput(encodedDocument);
       }
 
       yield DeepLinkData.fromValue(
-          DeepLinkValue.cardDetailsFromDocument, document);
+          DeepLinkValue.cardDetailsFromDocument, articleData?.document);
       return;
     }
     final deepLinkValue = DeepLinkValue.values.firstWhere(
--- application/test/test_utils/mocks.dart
+++ application/test/test_utils/mocks.dart
@@ -90,8 +90,8 @@ import 'package:xayn_discovery_app/infrastructure/use_case/develop/extract_log_u
 import 'package:xayn_discovery_app/infrastructure/use_case/develop/handlers.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/discovery_engine/custom_card/survey_card_injection_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/discovery_feed/share_uri_use_case.dart';
-import 'package:xayn_discovery_app/infrastructure/use_case/document/decode_document_use_case.dart';
-import 'package:xayn_discovery_app/infrastructure/use_case/document/encode_document_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/document/decode_article_use_case.dart';
+import 'package:xayn_discovery_app/infrastructure/use_case/document/encode_article_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/feed_settings/get_selected_countries_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/feed_settings/get_supported_countries_use_case.dart';
 import 'package:xayn_discovery_app/infrastructure/use_case/feed_settings/save_selected_countries_use_case.dart';
@@ -176,11 +176,11 @@ import 'package:xayn_discovery_engine/discovery_engine.dart';
   DbEntityMapToOnboardingStatusMapper,
   DbEntityMapToSurveyBannerMapper,
   DiscoveryCardManager,
-  DecodeDocumentUseCase,
+  DecodeArticleUseCase,
   Document,
   DocumentRepository,
   EditReaderModeSettingsManager,
-  EncodeDocumentUseCase,
+  EncodeArticleUseCase,
   EngineEventsUseCase,
   ExtractLogUseCase,
   FeatureManager,
