adecf9050f9691ae64f5af33be72ca9cd6d45715
vadim.popov <vadim.popov@xayn.com>
2022-09-12 11:27:06 +0200

    debugging automation

---
+++ application/automation/src/test/java/android/LaunchTest.java
@@ -0,0 +1,12 @@
+package android;
+
+import base.AndroidTestBase;
+import com.xayn.annotations.TestCase;
+import org.testng.annotations.Test;
+
+public class LaunchTest extends AndroidTestBase {
+    @Test(description = "Simple test to launch the app")
+    @TestCase()
+    public void LaunchingApp() {
+    }
+}
