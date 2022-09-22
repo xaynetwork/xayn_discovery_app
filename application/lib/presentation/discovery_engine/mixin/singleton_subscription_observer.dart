0ced15832c1374ca4854c77f77d507f81bababca
Frank Pepermans <frank@igindo.com>
2022-09-22 13:28:16 +0200

    adds allowSuspension, a flag to keep certain observers open when the manager itself is canceled

--- application/lib/presentation/discovery_engine/mixin/singleton_subscription_observer.dart
+++ application/lib/presentation/discovery_engine/mixin/singleton_subscription_observer.dart
@@ -21,25 +21,25 @@ import 'package:xayn_discovery_app/presentation/utils/environment_helper.dart';
 mixin SingletonSubscriptionObserver<T> on UseCaseBlocHelper<T> {
   /// todo: since managers where not singletons before, we should update our tests
   /// for now, disabling the listeners is sufficient to make them pass.
+
+  /// Indicates that this observer will pause/resume in its [onListen] and [onCancel] handlers.
+  /// If false, then the observer remains open, even if nothing is consuming the parent manager.
+  /// This is useful if a certain observer has operations that may outlast the typical UI lifetime
+  /// of a BlocConsumer.
+  bool get allowSuspension => true;
+
   @override
   Stream<T> get stream => EnvironmentHelper.kIsInTest
       ? super.stream
       : super.stream.doOnListen(onListen).doOnCancel(onCancel);
 
   @mustCallSuper
-  void onListen() => resumeAllSubscriptions();
+  void onListen() {
+    if (allowSuspension) resumeAllSubscriptions();
+  }
 
   @mustCallSuper
   void onCancel() {
-    const timeout = Duration(seconds: 3);
-
-    /// todo: make this less flaky
-    /// The problem: we should pause _after_ any pending use cases are
-    /// done, before we were pausing in the middle of a use cases's job (sometimes)
-    /// This timeout fixes the issue, but is of course not a clean solution...
-    Rx.race([
-      Stream.fromFuture(Future.delayed(timeout * 1.5)),
-      super.stream.debounceTime(const Duration(seconds: 3)).take(1)
-    ]).first.whenComplete(pauseAllSubscriptions);
+    if (allowSuspension) pauseAllSubscriptions();
   }
 }
