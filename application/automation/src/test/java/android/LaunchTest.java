27821a70064374e99c398a2ecee93f008fb90c9e
vadim.popov <vadim.popov@xayn.com>
2022-09-22 13:57:45 +0200

    replacing sanity with launch test

--- application/automation/src/test/java/android/LaunchTest.java
+++ application/automation/src/test/java/android/LaunchTest.java
@@ -7,7 +7,9 @@ import org.testng.annotations.Test;
 public class LaunchTest extends AndroidTestBase {
     @Test(description = "Simple test to launch the app")
     @TestCase()
-    public void LaunchingApp() {
+    public void LaunchingApp() throws InterruptedException {
         System.out.println("LAUNCHING THE APP");
+        Thread.sleep(60000);
     }
+
 }
