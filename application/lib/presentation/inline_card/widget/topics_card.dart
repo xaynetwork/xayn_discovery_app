a27db2a332ced41f280104047b2d6404ac1364fc
michaelkmalak <michael.k.malak@gmail.com>
2022-10-04 17:07:04 +0200

    refactor topics card

--- application/lib/presentation/inline_card/widget/topics_card.dart
+++ application/lib/presentation/inline_card/widget/topics_card.dart
@@ -30,84 +30,94 @@ class _TopicsInLineCardState extends State<TopicsInLineCard>
 
   @override
   Widget build(BuildContext context) {
-    final column = BlocBuilder<TopicsManager, TopicsState>(
-        bloc: manager,
-        builder: (_, state) {
-          final ctaString = manager.customSelectedTopicsCount == 0
-              ? R.strings.topicsCardCTA
-              : R.strings.topicsCardCTACustomTopics
-                  .format(manager.customSelectedTopicsCount.toString());
-          final children = <Widget>[
-            SizedBox(height: R.dimen.unit),
-            Expanded(child: _buildAnimation()),
-            SizedBox(height: R.dimen.unit3),
-            Text(
-              R.strings.addTopicScreenTitle,
-              textAlign: TextAlign.center,
-              style: R.styles.lBoldStyle.copyWith(color: R.colors.brightText),
-            ),
-            SizedBox(height: R.dimen.unit2),
-            Text(
-              R.strings.topicsCardSubtitle,
-              textAlign: TextAlign.center,
-              style: R.styles.mStyle.copyWith(color: R.colors.brightText),
-            ),
-            SizedBox(height: R.dimen.unit2_5),
-            Wrap(
-              spacing: R.dimen.unit,
-              children: state.suggestedTopics
-                  .map(
-                    (it) => TopicChip(
-                      topic: it,
-                      onPressed: manager.onAddOrRemoveTopic,
-                      isSelected: manager.isSelected(it),
-                    ),
-                  )
-                  .toList(),
-            ),
-            SizedBox(height: R.dimen.unit2),
-            AppGhostButton(
-              onPressed: manager.onAddTopicButtonClicked,
-              child: Text(
-                ctaString,
-                textAlign: TextAlign.center,
-                style: R.styles.underlinedCTATextStyle,
-              ),
-            ),
-          ];
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: children,
-          );
-        });
+    final background = Positioned.fill(
+      child: Container(
+          decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            R.colors.inLineCardDarkBackground,
+            R.colors.inLineCardLightBackground,
+            R.colors.inLineCardLightBackground,
+            R.colors.inLineCardDarkBackground,
+            R.colors.inLineCardDarkBackground,
+          ],
+          stops: const <double>[0.0, 0.13, 0.33, 0.44, 1.0],
+        ),
+      )),
+    );
+    final body = Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: R.dimen.unit4,
+        vertical: R.dimen.unit6,
+      ),
+      child: _buildBody(),
+    );
+
     return Stack(
       children: [
-        Positioned.fill(
-          child: Container(
-              decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: <Color>[
-                R.colors.inLineCardDarkBackground,
-                R.colors.inLineCardLightBackground,
-                R.colors.inLineCardLightBackground,
-                R.colors.inLineCardDarkBackground,
-                R.colors.inLineCardDarkBackground,
-              ],
-              stops: const <double>[0.0, 0.13, 0.33, 0.44, 1.0],
-            ),
-          )),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: R.dimen.unit4,
-            vertical: R.dimen.unit6,
-          ),
-          child: column,
-        ),
+        background,
+        body,
       ],
+    );
+  }
+
+  Widget _buildBody() => BlocBuilder<TopicsManager, TopicsState>(
+      bloc: manager,
+      builder: (_, state) {
+        final children = <Widget>[
+          SizedBox(height: R.dimen.unit),
+          Expanded(child: _buildAnimation()),
+          SizedBox(height: R.dimen.unit3),
+          Text(
+            R.strings.addTopicScreenTitle,
+            textAlign: TextAlign.center,
+            style: R.styles.lBoldStyle.copyWith(color: R.colors.brightText),
+          ),
+          SizedBox(height: R.dimen.unit2),
+          Text(
+            R.strings.topicsCardSubtitle,
+            textAlign: TextAlign.center,
+            style: R.styles.mStyle.copyWith(color: R.colors.brightText),
+          ),
+          SizedBox(height: R.dimen.unit2_5),
+          _buildTopicChips(state),
+          SizedBox(height: R.dimen.unit2),
+          _buildTextBtn(),
+        ];
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: children,
+        );
+      });
+
+  Widget _buildTopicChips(TopicsState state) => Wrap(
+        spacing: R.dimen.unit,
+        children: state.suggestedTopics
+            .map(
+              (it) => TopicChip(
+                topic: it,
+                onPressed: manager.onAddOrRemoveTopic,
+                isSelected: manager.isSelected(it),
+              ),
+            )
+            .toList(),
+      );
+
+  Widget _buildTextBtn() {
+    final ctaString = manager.customSelectedTopicsCount == 0
+        ? R.strings.topicsCardCTA
+        : R.strings.topicsCardCTACustomTopics
+            .format(manager.customSelectedTopicsCount.toString());
+    return AppGhostButton(
+      onPressed: manager.onAddTopicButtonClicked,
+      child: Text(
+        ctaString,
+        textAlign: TextAlign.center,
+        style: R.styles.underlinedCTATextStyle,
+      ),
     );
   }
 
